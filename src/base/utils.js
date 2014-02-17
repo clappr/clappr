e02cd911e7af104cae1a074b8ecd93ef780368e6
Bruno Torres <bruno.torres@me.com>
2014-02-17 18:02:35 -0300

    add base objects inspired by components from backbone.js

---
+++ src/base/utils.js
@@ -0,0 +1,28 @@
+var _ = require('underscore');
+
+var extend = function(protoProps, staticProps) {
+  var parent = this;
+  var child;
+
+  if (protoProps && _.has(protoProps, 'constructor')) {
+    child = protoProps.constructor;
+  } else {
+    child = function(){ return parent.apply(this, arguments); };
+  }
+
+  _.extend(child, parent, staticProps);
+
+  var Surrogate = function(){ this.constructor = child; };
+  Surrogate.prototype = parent.prototype;
+  child.prototype = new Surrogate;
+
+  if (protoProps) _.extend(child.prototype, protoProps);
+
+  child.__super__ = parent.prototype;
+
+  return child;
+};
+
+module.exports = {
+  extend: extend
+}
