90bfef0f4a48d3e443d182dfe32cbfd8a2998798
Bruno Torres <bruno.torres@me.com>
2014-02-17 18:47:16 -0300

    add base container implementation

--- src/components/container.js
+++ src/components/container.js
@@ -1,3 +1,36 @@
 /**
  * Container is responsible for the video rendering and state
  */
+
+var Component = require('../base/component');
+
+var Container = Component.extend({
+  events: {
+    'click': 'clicked'
+  },
+  initialize: function() {
+    this.trigger('container:ready');
+  },
+  timeUpdated: function(time) {
+    this.trigger('container:timeupdate', time);
+  },
+  play: function() {
+    this.trigger('container:play');
+  },
+  pause: function() {
+    this.trigger('container:pause');
+  },
+  clicked: function() {
+    this.trigger('container:click', this);
+  },
+  setCurrentTime: function(time) {
+    this.trigger('container:seek', time);
+  },
+  getCurrentTime: function() {
+  },
+  requestFullscreen: function() {
+    this.trigger('container:fullscreen');
+  }
+});
+
+module.exports = Container;
