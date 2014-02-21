3d085c55c6e0af9cf0f65efa6e0fba233b76b901
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2014-02-21 18:34:04 -0300

    main: add Player interface draft

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
-module.exports = WP3 = {};
+var Core = require('./components/core');
+var BaseObject = require('./base/base_object');
+var $ = require('jquery');
+
+var Player = BaseObject.extend({
+  initialize: function(params) {
+    this.params = params;
+  },
+
+  attachTo: function(element) {
+    this.params["el"] = element;
+    this.core = new Core(this.params);
+  }
+});
+
+module.exports = WP3 = { Player: Player };
