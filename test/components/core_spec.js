62075428af1bfdacd5ed2f8af0443e2ed7ca6cb7
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2014-02-21 18:34:04 -0300

    main: add Player interface draft

--- test/components/core_spec.js
+++ test/components/core_spec.js
@@ -2,10 +2,10 @@ var Core = require('../spec_helper').Core;
 
 describe('Core', function() {
   it('should receive embed parameters', function() {
-    var core = new Core({width: 320, height: 240});
-
+    var core = new Core({width: 320, height: 240, el: "div_here"});
     core.embedParams.should.have.property("width");
     core.embedParams.should.have.property("height");
+    core.embedParams.should.have.property("el");
   });
 
   it('should create a playbackhandler', function() {
