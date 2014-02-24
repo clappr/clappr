f329d7b499a4ef36b360d046eba5853760e6c108
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2014-02-24 11:30:02 -0300

    connect the components to instantiate a basic player

--- src/components/core.js
+++ src/components/core.js
@@ -5,12 +5,27 @@
 
 var BaseObject = require('../base/base_object');
 var PlaybackHandler = require('./playback_handler');
-var MediaControl = require('./playback_handler');
+var MediaControl = require('./media_control');
 
 var Core = BaseObject.extend({
   initialize: function(params) {
-    this.embedParams = params;
     this.playbackHandler = new PlaybackHandler(params);
+    this.containers = this.playbackHandler.createContainers();
+    this.createMediaControl(this.getCurrentContainer());
+  },
+
+  createMediaControl: function(container) {
+    this.mediaControl = new MediaControl({container: container, className: "media-control"});
+  },
+
+  getCurrentContainer: function() {
+    return this.containers[0];
+  },
+
+  render: function() {
+    this.$el.append(this.mediaControl.render().el);
+    this.$el.append(this.getCurrentContainer().render().el);
+    return this;
   }
 });
 
--- src/components/playback_handler.js
+++ src/components/playback_handler.js
@@ -3,10 +3,24 @@
  */
 
 var BaseObject = require('../base/base_object');
+var HTML5VideoPlaybackPlugin = require('../plugins/html5_video_playback');
+var Container = require('../components/container');
 
 var PlaybackHandler = BaseObject.extend({
   initialize: function(params) {
+    this.params = params;
+  },
+
+  createContainers: function() {
+    var containers = [];
+    if ('src' in this.params) {
+      var HTML5VideoContainer = new Container({className: 'HTML5VideoContainer'});
+      var HTML5VideoPlayback = new HTML5VideoPlaybackPlugin({container: HTML5VideoContainer, src: this.params['src']});
+      containers.push(HTML5VideoContainer);
+    }
+    return containers;
   }
+
 });
 
 module.exports = PlaybackHandler;
