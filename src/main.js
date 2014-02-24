9008f0a4a459e8645ef9de3a4732a05dec026e43
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2014-02-24 14:01:46 -0300

    media control: define template logic inside plugin settings

--- src/main.js
+++ src/main.js
@@ -6,11 +6,10 @@ var Player = BaseObject.extend({
   initialize: function(params) {
     this.params = params;
   },
-
   attachTo: function(element) {
     this.core = new Core(this.params);
     this.core.render().$el.appendTo(element);
-  }
+  },
 });
 
 module.exports = WP3 = { Player: Player };
