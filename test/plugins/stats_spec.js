70c53dbb005383ee9f137d39884a03e27defdd3f
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2014-02-25 17:45:04 -0300

    stats plugin: calculate rebuffering and startup time (refs #15)

--- test/plugins/stats_spec.js
+++ test/plugins/stats_spec.js
@@ -16,4 +16,32 @@ describe('StatsPlugin', function() {
     expect(stats.startupTime).to.equal(1000);
   });
 
+  it('should calculate rebuffer events', function() {
+    var stats = new StatsPlugin({container: this.container});
+    this.container.buffering();
+    this.container.bufferfull();
+
+    this.container.buffering();
+    this.container.bufferfull();
+
+    expect(stats.rebuffers).to.equal(2);
+  });
+
+  it('should calculate total rebuffer time', function() {
+    var stats = new StatsPlugin({container: this.container});
+    this.container.play();
+    this.container.buffering(); // startup time
+    this.clock.tick(1000);
+    this.container.bufferfull();
+
+    this.container.buffering();
+    this.clock.tick(1000);
+    this.container.bufferfull();
+
+    this.container.buffering();
+    this.clock.tick(500);
+    this.container.bufferfull();
+
+    expect(stats.rebufferingTime).to.equal(1500);
+  });
 });
