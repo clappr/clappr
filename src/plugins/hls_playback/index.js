78a7d0bb1724907d0570f4f5ef56e473d02c6b98
Bruno Torres <bruno.torres@me.com>
2014-02-26 14:36:33 -0300

    license: add missing copyright notice to some files

--- src/plugins/hls_playback/index.js
+++ src/plugins/hls_playback/index.js
@@ -1,3 +1,7 @@
+// Copyright 2014 Globo.com Player authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 //FIXME review this require approach.
 var PlaybackPlugin = require('../../base/playback_plugin');
 var JST = require('../../base/jst');
