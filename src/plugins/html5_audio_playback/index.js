7dec782868cf272b7c9d759ffcde52163b6e1093
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2014-02-27 15:34:06 -0300

    plugins: port html5 audio/video plugins to new-style

--- src/plugins/html5_audio_playback/index.js
+++ src/plugins/html5_audio_playback/index.js
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-var PlaybackPlugin = require('../base/playback_plugin');
+var PlaybackPlugin = require('../../base/playback_plugin');
 
 var HTML5AudioPlayback = PlaybackPlugin.extend({
   events: {
