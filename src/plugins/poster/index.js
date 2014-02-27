5bdbc68c77c87f4a2bca38ac27f6823044b534a7
thiagopnts <thiagopnts@gmail.com>
2014-02-26 22:10:11 -0300

    poster plugin: migrate to new structure and add styles

--- src/plugins/poster/index.js
+++ src/plugins/poster/index.js
@@ -2,9 +2,13 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-var UIObject = require('../base/ui_object');
+var UIObject = require('../../base/ui_object');
+var Styler = require('../../base/styler');
 
 var PosterPlugin = UIObject.extend({
+  attributes: {
+    'data-poster': ''
+  },
   tagName: 'img',
   initialize: function(options) {
     this.listenTo(this.container, 'container:play', this.onPlay);
@@ -19,10 +23,11 @@ var PosterPlugin = UIObject.extend({
     this.$el.show();
   },
   render: function() {
+    var style = Styler.getStyleFor('poster');
+    this.container.$el.append(style);
     this.container.$el.append(this.el);
     return this;
-  },
+  }
 });
 
 module.exports = PosterPlugin;
-
