694e05a94a4c772ab35665dd34c787d06536fb34
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2014-02-27 15:18:21 -0300

    stats plugin: remove unnecessary function

--- src/plugins/stats/index.js
+++ src/plugins/stats/index.js
@@ -13,7 +13,7 @@ var StatsPlugin = BaseObject.extend({
     this.listenTo(this.container, 'container:state:bufferfull', this.onBufferFull);
     this.listenTo(this.container, 'container:stats:add', this.onStatsAdd);
     this.setInitialAttrs();
-    this.intervalId = setInterval(function() { this.announce() }.bind(this), options.announceInterval || 60000);
+    this.intervalId = setInterval(this.announce.bind(this), options.announceInterval || 60000);
   },
   setInitialAttrs: function() {
     this.firstPlay = true;
