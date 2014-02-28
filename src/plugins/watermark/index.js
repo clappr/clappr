95cdf8bac234273b06ab1a3a142c53f25cfb0758
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2014-02-28 12:47:12 -0300

    watermark: add initial support (refs #9)

---
+++ src/plugins/watermark/index.js
@@ -0,0 +1,34 @@
+// Copyright 2014 Globo.com Player authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+var UIObject = require('../../base/ui_object');
+var Styler = require('../../base/styler');
+var JST = require('../../base/jst');
+
+var WaterMarkPlugin = UIObject.extend({
+  pluginName: 'watermark',
+  initialize: function(options) {
+    this.template = JST[this.pluginName];
+    this.listenTo(this.container, 'container:play', this.onPlay);
+    this.listenTo(this.container, 'container:stop', this.onStop);
+    this.render();
+  },
+  onPlay: function() {
+    this.$el.show();
+  },
+  onStop: function() {
+    this.$el.hide();
+  },
+  render: function() {
+    this.$el.hide();
+    this.$el.html(this.template());
+    var style = Styler.getStyleFor(this.pluginName);
+    this.container.$el.append(style);
+    this.container.$el.append(this.$el);
+    return this;
+  }
+});
+
+module.exports = WaterMarkPlugin;
+
