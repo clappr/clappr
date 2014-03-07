f1596982ef0716fd101c360cb42ec455eb72817c
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2014-03-07 14:45:04 -0300

    playback handler: change the way it parses parameters

--- src/components/playback_handler/index.js
+++ src/components/playback_handler/index.js
@@ -6,6 +6,7 @@
  * The PlaybackHandler is responsible for manage playback bootstrap and swapping sources.
  */
 
+var _ = require('underscore');
 var BaseObject = require('../../base/base_object');
 var Container = require('../container');
 var PosterPlugin = require('../../plugins/poster');
@@ -24,21 +25,23 @@ var PlaybackHandler = BaseObject.extend({
 
   createContainers: function(callback) {
     var containers = [];
-    if ('src' in this.params && this.params.src.match(/(.*).mp4/)) {
-      containers.push(this.createHTML5VideoContainer());
-
-    } else if ('src' in this.params && this.params.src.match(/(.*).mp3/)) {
-      containers.push(this.createHTML5AudioContainer());
-
-    } else if ('src' in this.params && this.params.src.match(/(.*).m3u8/)) {
-      containers.push(this.createHLSVideoContainer());
-    }
+    _.each(this.params, function(value, key) {
+      if (key === "src" || key === "pip") {
+        if (value.match(/(.*).mp4/)) {
+          containers.push(this.createHTML5VideoContainer());
+        } else if (value.match(/(.*).mp3/)) {
+          containers.push(this.createHTML5AudioContainer());
+        } else if (value.match(/(.*).m3u8/)) {
+          containers.push(this.createHLSVideoContainer(value));
+        }
+      }
+    }, this);
 
     callback(containers);
   },
   createHTML5VideoContainer: function() {
     var container = new Container();
-    var poster = new PosterPlugin({container: container, src: this.params.poster});
+    var poster = new PosterPlugin({container: container, src: "image2.png"});
     var playback = new HTML5VideoPlaybackPlugin({container: container, src: this.params.src, autoPlay: !!this.params.autoPlay});
     return container;
   },
@@ -47,16 +50,15 @@ var PlaybackHandler = BaseObject.extend({
     var playback = new HTML5AudioPlaybackPlugin({container: container, src: this.params.src, autoPlay: !!this.params.autoPlay});
     return container;
   },
-  createHLSVideoContainer: function() {
+  createHLSVideoContainer: function(src) {
     var container = new Container();
     var poster = new PosterPlugin({container: container, src: this.params.poster});
     var spinner = new SpinnerThreeBouncePlugin({container: container});
 
     // position can be "top-left", "top-right", "bottom-right" or  "bottom-left".
-    var watermark = new WaterMarkPlugin({container: container, imageUrl: "watermark.png", position: "bottom-right"});
-
+    var watermark = new WaterMarkPlugin({container: container, imageUrl: "watermark.png", position: "top-right"});
     var stats = new StatsPlugin({container: container, reportInterval: 10000});
-    var playback = new HLSVideoPlaybackPlugin({container: container, src: this.params.src, autoPlay: !!this.params.autoPlay});
+    var playback = new HLSVideoPlaybackPlugin({container: container, src: src, autoPlay: !!this.params.autoPlay});
     return container;
   }
 });
