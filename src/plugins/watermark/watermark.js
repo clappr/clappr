6458b0faf1ef7ed998f77fec486f5bcf59d7ce7b
thiagopnts <thiagopnts@gmail.com>
2014-03-11 21:02:58 -0300

    only use index.js for loading files :bowtie:

---
+++ src/plugins/watermark/watermark.js
@@ -0,0 +1,37 @@
+// Copyright 2014 Globo.com Player authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+var UIObject = require('../../base/ui_object');
+var Styler = require('../../base/styler');
+var JST = require('../../base/jst');
+
+var WaterMarkPlugin = UIObject.extend({
+  pluginName: 'watermark',
+  initialize: function(options) {
+    this.template = JST[this.pluginName];
+    this.listenTo(this.container, 'container:play', this.onPlay);
+    this.listenTo(this.container, 'container:stop', this.onStop);
+    this.position = options.position? options.position: "bottom-right";
+    this.imageUrl = options.imageUrl || 'assets/watermark.png';
+    this.render();
+  },
+  onPlay: function() {
+    this.$el.show();
+  },
+  onStop: function() {
+    this.$el.hide();
+  },
+  render: function() {
+    this.$el.hide();
+    var templateOptions = {position: this.position, imageUrl: this.imageUrl};
+    this.$el.html(this.template(templateOptions));
+    var style = Styler.getStyleFor(this.pluginName);
+    this.container.$el.append(style);
+    this.container.$el.append(this.$el);
+    return this;
+  }
+});
+
+module.exports = WaterMarkPlugin;
+
