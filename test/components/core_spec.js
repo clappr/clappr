2880c0eb655cc6c82fa2fc2ad28d8187b4d0f8cb
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2014-03-13 18:54:17 -0300

    playback handler: change src to sources (refs #39)

--- test/components/core_spec.js
+++ test/components/core_spec.js
@@ -2,17 +2,17 @@ var Core = require('../spec_helper').Core;
 
 describe('Core', function() {
   it('should create a playbackhandler', function() {
-    var core = new Core({src: 'http://globo.com/video.mp4'});
+    var core = new Core({sources: ['http://globo.com/video.mp4']});
     expect(core.playbackHandler).to.be.a('object');
   });
 
   it('should create a mediacontrol', function() {
-    var core = new Core({src: 'http://globo.com/video.mp4'});
+    var core = new Core({sources: ['http://globo.com/video.mp4']});
     expect(core.mediaControl).to.be.a('object');
   });
 
   it('should have containers', function() {
-    var core = new Core({src: 'http://globo.com/video.mp4'});
+    var core = new Core({sources: ['http://globo.com/video.mp4']});
     expect(core.containers).to.be.a('Array');
   });
 
