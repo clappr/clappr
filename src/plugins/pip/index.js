b489d5a8bbc8194559f1ff921c32f462a4a1cebd
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2014-03-14 17:36:50 -0300

    pip plugin: change files to new-style plugins (refs #7)

--- src/plugins/pip/index.js
+++ src/plugins/pip/index.js
@@ -1,15 +1,2 @@
-// Copyright 2014 Globo.com Player authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+module.exports = require('./pip');
 
-var BaseObject = require('../../base/base_object');
-var $ = require("jquery");
-
-var PipPlugin = BaseObject.extend({
-  initialize: function(mediaControl, containers) {
-    var pipContainer = containers[1];
-    pipContainer.setStyle({width: "25%", height: "25%", zindex: 10, bottom: "7px", right: "7px"});
-  }
-});
-
-module.exports = PipPlugin;
