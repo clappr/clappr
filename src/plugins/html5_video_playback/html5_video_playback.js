a1fdc1dc60c6493d69070eed50badac2c740342b
thiagopnts <thiagopnts@gmail.com>
2014-03-18 17:45:01 -0300

    html5 video playback: make it compatible with the new plugin structure

--- src/plugins/html5_video_playback/html5_video_playback.js
+++ src/plugins/html5_video_playback/html5_video_playback.js
@@ -2,74 +2,94 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-var UIPlugin = require('../../base/ui_object'); //FIXME
+var UIPlugin = require('../../base/ui_plugin');
 var Styler = require('../../base/styler');
 
 var HTML5VideoPlaybackPlugin = UIPlugin.extend({
-  pluginName: 'html5_video_playback',
+  name: 'html5_video_playback',
+  type: 'playback',
+  tagName: 'video',
   attributes: {
     'data-html5-video': ''
   },
+
   events: {
     'timeupdate': 'timeUpdated',
     'ended': 'ended'
   },
-  tagName: 'video',
+
   initialize: function(options) {
+    this.super('initialize');
     this.el.src = options.src;
     this.container.settings = {
       left: ['playpause'],
       right: ['fullscreen', 'volume'],
       default: ['position', 'seekbar', 'duration']
     };
+    this.render();
+    options.autoPlay && this.container.play();
+  },
+
+  bindEvents: function() {
     this.listenTo(this.container, 'container:play', this.play);
     this.listenTo(this.container, 'container:pause', this.pause);
     this.listenTo(this.container, 'container:seek', this.seek);
     this.listenTo(this.container, 'container:volume', this.volume);
     this.listenTo(this.container, 'container:stop', this.stop);
-    this.render();
-    options.autoPlay && this.container.play();
   },
+
   play: function() {
     this.el.play();
   },
+
   pause: function() {
     this.el.pause();
   },
+
   stop: function() {
     this.pause();
     this.el.currentTime = 0;
   },
+  
   volume: function(value) {
     this.el.volume = value / 100;
   },
+
   mute: function() {
     this.el.volume = 0;
   },
+
   unmute: function() {
     this.el.volume = 1;
   },
+
   isMuted: function() {
     return !!this.el.volume;
   },
+
   ended: function() {
     this.trigger('container:timeupdate', 0);
   },
+
   seek: function(seekBarValue) {
     var time = this.el.duration * (seekBarValue / 100);
     this.el.currentTime = time;
   },
+
   getCurrentTime: function() {
     return this.el.currentTime;
   },
+
   getDuration: function() {
     return this.el.duration;
   },
+
   timeUpdated: function() {
     this.container.timeUpdated(this.el.currentTime, this.el.duration);
   },
+
   render: function() {
-    var style = Styler.getStyleFor(this.pluginName);
+    var style = Styler.getStyleFor(this.name);
     this.container.$el.append(style);
     this.container.$el.append(this.el);
     return this;
@@ -80,5 +100,5 @@ HTML5VideoPlaybackPlugin.canPlay = function(resource) {
     return !!resource.match(/(.*).mp4/);
 }
 
-
 module.exports = HTML5VideoPlaybackPlugin;
+
