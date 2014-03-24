1ef9a638f34bdab90ae8fdbc6489c9308ccdfe8b
thiagopnts <thiagopnts@gmail.com>
2014-03-24 16:59:46 -0300

    tests: add a browser runner

--- test/spec_helper.js
+++ test/spec_helper.js
@@ -1,24 +1,23 @@
-module.paths.push(process.cwd() + '/src');
-var jsdom = require('jsdom').jsdom;
+//var jsdom = require('jsdom').jsdom;
 global.chai = require('chai');
 global.sinon = require('sinon');
 
 // bootstrap a "DOM" so jquery can work in the test env.
-global.window = jsdom('<html><body></body></html>').createWindow();
+//global.window = jsdom('<html><body></body></html>').createWindow();
 
 chai.should();
 expect = chai.expect;
 
 module.exports = {
-  UIObject: require('base/ui_object'),
-  BaseObject: require('base/base_object'),
-  MediaControl: require('components/media_control'),
-  Container: require('components/container'),
-  Core: require('components/core'),
-  StatsPlugin: require('plugins/stats'),
-  PipPlugin: require('plugins/pip'),
-  StatsEvents: require('plugins/stats/stats_events'),
-  UIPlugin: require('base/ui_plugin'),
-  WaterMarkPlugin: require('plugins/watermark'),
-  JST: require('base/jst')
+  UIObject: require('../src/base/ui_object'),
+  BaseObject: require('../src/base/base_object'),
+  MediaControl: require('../src/components/media_control'),
+  Container: require('../src/components/container'),
+  Core: require('../src/components/core'),
+  StatsPlugin: require('../src/plugins/stats'),
+  PipPlugin: require('../src/plugins/pip'),
+  StatsEvents: require('../src/plugins/stats/stats_events'),
+  UIPlugin: require('../src/base/ui_plugin'),
+  WaterMarkPlugin: require('../src/plugins/watermark'),
+  JST: require('../src/base/jst')
 };
