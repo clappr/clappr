36aa3b29021197a75026f8b122ca42a94b198f7b
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2014-03-27 18:23:36 -0300

    pip: implement pip feature (closes #7)

--- src/plugins/pip/pip.js
+++ src/plugins/pip/pip.js
@@ -9,26 +9,35 @@ var _ = require('underscore');
 var PipPlugin = BaseObject.extend({
   initialize: function(core) {
     this.core = core;
+    this.pipStyle = {width: "24%", height: "24%", "z-index": 20, bottom: "47px", right: "7px"};
+    this.masterStyle = {width: "100%", height: "100%", "z-index": 20, bottom: "0px", right: "0px"};
     this.masterContainer = core.containers[0];
     if (core.containers.length === 2) {
       this.pipContainer = core.containers[1];
-      this.pipContainer.setStyle({width: "30%", height: "30%", "z-index": 20, bottom: "7px", right: "7px"});
+      this.pipContainer.setStyle(this.pipStyle);
     }
+    this.setupApi();
+  },
+  setupApi: function() {
+    //FIXME we need to figure out how to manage player API (issue #51)
+    window.WP3.addPip = this.addPip.bind(this);
+    window.WP3.discardPip = this.discardPip.bind(this);
+    window.WP3.addMaster = this.addMaster.bind(this);
+    window.WP3.pipToMaster = this.pipToMaster.bind(this);
   },
   addPip: function(source) {
     this.discardPip();
-    this.core.playbackHandler.createContainer(source, this.addPipCallback.bind(this));
+    this.pipContainer = this.core.playbackHandler.createContainer(source, this.addPipCallback.bind(this));
+    this.pipContainer.setStyle({'z-index': -1}); //we need to put this container behind everything until flash dispatch on:ready
+    window.pip = this.pipContainer;
+    this.core.$el.append(this.pipContainer.render().el);
   },
   addPipCallback: function(container) {
-    this.pipContainer = container;
-    this.core.$el.append(this.pipContainer.render().el);
-    console.log("listened to container ready");
     this.pipContainer.setVolume(0);
-    this.pipContainer.play();
     this.pipContainer.getPluginByName('watermark').disable();
-    this.pipContainer.setStyle({width: "30%", height: "30%", "z-index": 20, bottom: "7px", right: "7px"});
+    this.pipContainer.setStyle(this.pipStyle);
+    this.pipContainer.play();
     this.core.containers.push(this.pipContainer);
-    this.core.$el.append(this.pipContainer.render().el);
   },
   discardPip: function() {
     if (this.pipContainer) {
@@ -53,7 +62,11 @@ var PipPlugin = BaseObject.extend({
     delete this.tmpContainer;
     this.pipContainer.setVolume(0);
     this.pipContainer.getPluginByName('watermark').disable();
-    this.pipContainer.animate({width: "30%", height: "30%", "z-index": 20, bottom: "7px", right: "7px"}, 400);
+    if (this.pipContainer.hasPlugin('hls_playback')) { //flash breaks on animate
+      this.pipContainer.setStyle(this.pipStyle);
+    } else {
+      this.pipContainer.animate(this.pipStyle, 400);
+    }
     this.core.containers.splice(0, 0, this.masterContainer);
   },
   discardContainer: function(container) {
@@ -61,18 +74,19 @@ var PipPlugin = BaseObject.extend({
   },
   pipToMaster: function() {
     if (this.pipContainer) {
-      this.pipContainer.animate({width: "100%", height: "100%", "z-index": 20, bottom: "0px", right: "0px"}, 400);
-      setTimeout(function() {
-        this.pipContainer.setVolume(100);
-        this.pipContainer.getPluginByName('watermark').enable();
-        this.masterContainer.destroy();
-        this.discardContainer(this.masterContainer);
-        delete this.masterContainer;
-        this.masterContainer = this.pipContainer;
-        this.masterContainer.setStyle({"z-index": 1});
-        delete this.pipContainer;
-      }.bind(this), 400);
+      this.pipContainer.animate(this.masterStyle, 400);
+      setTimeout(this.afterPipToMaster.bind(this), 400);
     }
+  },
+  afterPipToMaster: function() {
+    this.pipContainer.setVolume(100);
+    this.pipContainer.getPluginByName('watermark').enable();
+    this.masterContainer.destroy();
+    this.discardContainer(this.masterContainer);
+    delete this.masterContainer;
+    this.masterContainer = this.pipContainer;
+    this.masterContainer.setStyle({"z-index": 1});
+    delete this.pipContainer;
   }
 });
 
