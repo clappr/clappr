6cef65788db7480599de60205a7bcc9e110251dc
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2014-03-28 14:50:19 -0300

    pip: fix border when addMaster() and call stop() and set src to "" for html5 playback containers

--- src/plugins/pip/pip.js
+++ src/plugins/pip/pip.js
@@ -12,7 +12,7 @@ var PipPlugin = BaseObject.extend({
     this.pipStyle = {width: "24%", height: "24%", "z-index": 20, bottom: "47px", right: "7px",
                      "border-width": "3px", "border-style": "solid", "border-color": "rgba(255,255,255, .3)",
                      "background-clip": "padding-box", "-webkit-background-clip": "padding-box"};
-    this.masterStyle = {width: "100%", height: "100%", "z-index": 20, bottom: "0px", right: "0px", border: "none"};
+    this.masterStyle = {width: "100%", height: "100%", bottom: "0px", right: "0px", border: "none"};
     this.masterContainer = core.containers[0];
     if (core.containers.length === 2) {
       this.pipContainer = core.containers[1];
@@ -30,21 +30,31 @@ var PipPlugin = BaseObject.extend({
   addPip: function(source) {
     this.discardPip();
     this.pipContainer = this.core.playbackHandler.createContainer(source, this.addPipCallback.bind(this));
-    this.pipContainer.setStyle({'z-index': -1}); //we need to put this container behind everything until flash dispatch on:ready
+    if (this.pipContainer.hasPlugin('hls_playback')) {
+      // html5 players dispatches on:ready instantly, causing this style to be set after addPipCallback.
+      // For flash players, we need to put it behind everything until flash dispatch on:ready.
+      this.pipContainer.setStyle({'z-index': -1});
+    }
     this.core.$el.append(this.pipContainer.render().el);
   },
   addPipCallback: function(container) {
-    this.pipContainer.setVolume(0);
-    this.pipContainer.getPluginByName('watermark').disable();
-    this.pipContainer.setStyle(this.pipStyle);
-    this.pipContainer.play();
-    this.core.containers.push(this.pipContainer);
+    window.pipContainer = container;
+    container.setVolume(0);
+    container.getPluginByName('watermark').disable();
+    container.play();
+    container.setStyle(this.pipStyle);
+    this.core.containers.push(container);
   },
   discardPip: function() {
     if (this.pipContainer) {
-      this.pipContainer.destroy();
       this.discardContainer(this.pipContainer);
       delete this.pipContainer;
+    }
+  },
+  discardMaster: function() {
+    if (this.masterContainer) {
+      this.discardContainer(this.masterContainer);
+      delete this.masterContainer;
     }
   },
   addMaster: function(source) {
@@ -67,10 +77,16 @@ var PipPlugin = BaseObject.extend({
       this.pipContainer.setStyle(this.pipStyle);
     } else {
       this.pipContainer.animate(this.pipStyle, 400);
+      setTimeout(function() { this.pipContainer.setStyle(this.pipStyle); }.bind(this), 400); //workaround to persist borders after the animation
     }
     this.core.containers.splice(0, 0, this.masterContainer);
   },
   discardContainer: function(container) {
+    if (container.hasPlugin('html5_video_playback')) {
+      container.stop();
+      container.getPluginByName('html5_video_playback').el.src = '';
+    }
+    container.destroy();
     this.core.containers = _.without(this.core.containers, _.findWhere(this.core.containers, container));
   },
   pipToMaster: function() {
@@ -82,9 +98,7 @@ var PipPlugin = BaseObject.extend({
   afterPipToMaster: function() {
     this.pipContainer.setVolume(100);
     this.pipContainer.getPluginByName('watermark').enable();
-    this.masterContainer.destroy();
-    this.discardContainer(this.masterContainer);
-    delete this.masterContainer;
+    this.discardMaster();
     this.masterContainer = this.pipContainer;
     this.masterContainer.setStyle({"z-index": 1});
     delete this.pipContainer;
