62534d357925ceef00d76bfeec1efa77d3bdd7db
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2014-04-04 14:38:00 -0300

    update player.js

--- player/player.js
+++ player/player.js
@@ -14311,6 +14311,15 @@ var PipPlugin = BaseObject.extend({
     if (core.containers.length === 2) {
       this.pipContainer = core.containers[1];
       this.pipContainer.setStyle(this.pipStyle);
+      if (!this.pipContainer.isReady) {
+        this.pipContainer.on('container:ready', function() {
+          this.pipContainer.play();
+        }.bind(this));
+      } else {
+        this.pipContainer.play();
+      }
+      this.core.mediaControl.setContainer(this.masterContainer);
+      this.core.mediaControl.render();
     }
     this.listenTo(this.core.mediaControl, 'mediacontrol:show', this.onMediaControlShow);
     this.listenTo(this.core.mediaControl, 'mediacontrol:hide', this.onMediaControlHide);
