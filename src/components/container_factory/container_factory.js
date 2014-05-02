880e946ac5a54777f2b1536d52efeb2ff72b59a0
thiagopnts <thiagopnts@gmail.com>
2014-05-02 13:18:23 -0300

    s/PlaybackHandler/ContainerFactory/g (refs globocom/player #98)

---
+++ src/components/container_factory/container_factory.js
@@ -0,0 +1,45 @@
+// Copyright 2014 Globo.com Player authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/**
+ * The ContainerFactory is responsible for manage playback bootstrap and create containers.
+ */
+
+var _ = require('underscore');
+var BaseObject = require('../../base/base_object');
+var Container = require('../container');
+
+var ContainerFactory = BaseObject.extend({
+  initialize: function(params, loader) {
+    this.params = params;
+    this.loader = loader;
+  },
+  createContainers: function(callback) {
+      var containers = [];
+      _.each(this.params.sources, function(source) {
+        var container = this.createContainer(source);
+        containers.push(container);
+      }, this);
+
+    callback(containers);
+  },
+  findPlaybackPlugin: function(source) {
+    return _.find(this.loader.playbackPlugins, function(p) { return p.canPlay(source) }, this);
+  },
+  createContainer: function(source, callback) {
+    var playbackPlugin = this.findPlaybackPlugin(source);
+    var playback = new playbackPlugin({src: source, autoPlay: !!this.params.autoPlay});
+    var container = new Container({playback: playback});
+    this.addContainerPlugins(container);
+    callback && callback(container);
+    return container;
+  },
+  addContainerPlugins: function(container) {
+    _.each(this.loader.containerPlugins, function(plugin) {
+      container.addPlugin(new plugin(_.extend(this.params, {container: container})));
+    }, this);
+  }
+});
+
+module.exports = ContainerFactory;
