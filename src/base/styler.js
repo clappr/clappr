1b652af048219148bc4c6578cb6c2b747807704c
Bruno Torres <bruno.torres@me.com>
2014-05-09 15:19:15 -0300

    styler: change to use templates for css

--- src/base/styler.js
+++ src/base/styler.js
@@ -3,10 +3,12 @@
 // license that can be found in the LICENSE file.
 
 var $ = require('jquery');
+var _ = require('underscore');
 var JST = require('./jst');
 
 module.exports = Styler = {
-  getStyleFor: function(name) {
-    return $('<style></style>').html(JST.CSS[name]);
+  getStyleFor: function(name, options) {
+    options = options || {};
+    return $('<style></style>').html(_.template(JST.CSS[name])(options));
   }
 }
