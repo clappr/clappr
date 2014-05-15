b7ddd10ef7b1697036be6d44bb98067ba72db136
thiagopnts <thiagopnts@gmail.com>
2014-05-15 15:40:38 -0300

    flash playback: make all calls to external interfaces safe
    Conflicts:
    	src/plugins/flash_playback/flash_playback.js

--- src/plugins/flash_playback/flash_playback.js
+++ src/plugins/flash_playback/flash_playback.js
@@ -2,12 +2,12 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-var UIPlugin = require('../../base/ui_plugin');
+var UIObject = require('../../base/ui_object');
 var Styler = require('../../base/styler');
 var JST = require('../../base/jst');
 var _ = require("underscore");
 
-var FlashVideoPlaybackPlugin = UIPlugin.extend({
+var FlashVideoPlaybackPlugin = UIObject.extend({
   name: 'flash_playback',
   tagName: 'object',
   template: JST.flash_playback,
@@ -26,7 +26,11 @@ var FlashVideoPlaybackPlugin = UIPlugin.extend({
     };
     this.isReady = false;
     this.checkIfFlashIsReady();
-    this.autoPlay && this.play();
+  },
+  safe: function(fn) {
+    if(this.el.getState && this.el.getDuration && this.el.getPosition && this.el.getBytesLoaded && this.el.getBytesTotal) {
+      return fn.apply(this);
+    }
   },
   bootstrap: function() {
     this.el.width = "100%";
@@ -39,10 +43,10 @@ var FlashVideoPlaybackPlugin = UIPlugin.extend({
   },
   checkIfFlashIsReady: function() {
     this.bootstrapId = setInterval(function() {
-      if(this.el.getState && this.el.getState() === "IDLE") {
-           this.bootstrap();
+      if(this.el.getDuration && this.el.getState && this.el.getState() === "IDLE") {
+        this.bootstrap();
       }
-    }.bind(this), 50);
+    }.bind(this), 500);
   },
   setupFirefox: function() {
     var $el = this.$('embed');
@@ -50,80 +54,105 @@ var FlashVideoPlaybackPlugin = UIPlugin.extend({
     this.setElement($el[0]);
   },
   updateTime: function(interval) {
-    return setInterval(function() {
-      this.trigger('playback:timeupdate', this.el.getPosition(), this.el.getDuration(), this.name);
-    }.bind(this), interval);
+    return this.safe(function() {
+      return setInterval(function() {
+        this.trigger('playback:timeupdate', this.el.getPosition(), this.el.getDuration(), this.name);
+      }.bind(this), interval);
+    });
   },
   timedCheckState: function() {
     this.checkStateId = setInterval(this.checkState.bind(this), 250);
     this.progressId = setInterval(this.progress.bind(this), 1000);
   },
   checkState: function() {
-    if (this.currentState !== "PLAYING_BUFFERING" && this.el.getState() === "PLAYING_BUFFERING") {
-      this.trigger('playback:buffering', this.name);
-      this.currentState = "PLAYING_BUFFERING";
-    } else if (this.currentState === "PLAYING_BUFFERING" && this.el.getState() === "PLAYING") {
-      this.trigger('playback:bufferfull', this.name);
-      this.currentState = "PLAYING";
-    } else if (this.el.getState() === "IDLE") {
-      this.currentState = "IDLE";
-    } else if (this.el.getState() === "ENDED") {
-      this.trigger('playback:ended', this.name);
-      this.trigger('playback:timeupdate', 0, this.el.getDuration(), this.name);
-      this.currentState = "ENDED";
-      clearInterval(this.id);
-    }
+    this.safe(function() {
+      if (this.currentState !== "PLAYING_BUFFERING" && this.el.getState() === "PLAYING_BUFFERING") {
+        this.trigger('playback:buffering', this.name);
+        this.currentState = "PLAYING_BUFFERING";
+      } else if (this.currentState === "PLAYING_BUFFERING" && this.el.getState() === "PLAYING") {
+        this.trigger('playback:bufferfull', this.name);
+        this.currentState = "PLAYING";
+      } else if (this.el.getState() === "IDLE") {
+        this.currentState = "IDLE";
+      } else if (this.el.getState() === "ENDED") {
+        this.trigger('playback:ended', this.name);
+        this.trigger('playback:timeupdate', 0, this.el.getDuration(), this.name);
+        this.currentState = "ENDED";
+        clearInterval(this.id);
+      }
+    });
   },
   progress: function() {
-    if (this.currentState !== "IDLE" && this.currentState !== "ENDED") {
-      this.trigger('playback:progress', 0, this.el.getBytesLoaded(), this.el.getBytesTotal(), this.name);
-    }
+    this.safe(function() {
+      if (this.currentState !== "IDLE" && this.currentState !== "ENDED") {
+        this.trigger('playback:progress', 0, this.el.getBytesLoaded(), this.el.getBytesTotal(), this.name);
+      }
+    });
   },
   firstPlay: function() {
-    this.currentState = "PLAYING";
-    this.el.playerPlay(this.src);
+    this.safe(function() {
+      this.currentState = "PLAYING";
+      this.el.playerPlay(this.src);
+    });
   },
   play: function() {
-    if(this.el.getState() === 'IDLE') {
-      this.id = this.updateTime(1000);
-    }
-    if(this.el.getState() === 'PAUSED') {
-      this.currentState = "PLAYING";
-      this.el.playerResume();
-    } else {
-      this.firstPlay();
-    }
-    this.trigger('playback:play', this.name);
+    this.safe(function() {
+      if(this.el.getState() === 'IDLE') {
+        this.id = this.updateTime(1000);
+      }
+      if(this.el.getState() === 'PAUSED') {
+        this.currentState = "PLAYING";
+        this.el.playerResume();
+      } else {
+        this.firstPlay();
+      }
+      this.trigger('playback:play', this.name);
+    });
   },
   volume: function(value) {
-    this.el.playerVolume(value);
+    this.safe(function() {
+      this.el.playerVolume(value);
+    });
   },
   pause: function() {
-    this.currentState = "PAUSED";
-    this.el.playerPause();
+    this.safe(function() {
+      this.currentState = "PAUSED";
+      this.el.playerPause();
+    });
   },
   stop: function() {
-    this.el.playerStop();
-    clearInterval(this.id);
-    this.trigger('playback:timeupdate', 0, this.name);
+    this.safe(function() {
+      this.el.playerStop();
+      clearInterval(this.id);
+      this.trigger('playback:timeupdate', 0, this.name);
+    });
   },
   isPlaying: function() {
     return !!(this.isReady && this.currentState == "PLAYING");
   },
+  getDuration: function() {
+    return this.safe(function() {
+      return this.el.getDuration();
+    })
+  },
   seek: function(time) {
-    clearInterval(this.id);
-    var seekTo = this.el.getDuration()* (time / 100);
-    this.el.playerSeek(seekTo);
-    this.trigger('playback:timeupdate', seekTo, this.el.getDuration(), this.name);
-    this.id = this.updateTime(1000);
-    if (this.currentState == "PAUSED") {
-      this.pause();
-    }
+    this.safe(function() {
+      clearInterval(this.id);
+      var seekTo = this.el.getDuration()* (time / 100);
+      this.el.playerSeek(seekTo);
+      this.trigger('playback:timeupdate', seekTo, this.el.getDuration(), this.name);
+      this.id = this.updateTime(1000);
+      if (this.currentState == "PAUSED") {
+        this.pause();
+      }
+    })
   },
   destroy: function() {
     clearInterval(this.id);
     clearInterval(this.checkStateId);
     clearInterval(this.progressId);
+    this.stopListening();
+    this.$el.remove();
   },
   render: function() {
     var style = Styler.getStyleFor(this.name);
