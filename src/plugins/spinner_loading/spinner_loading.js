f03f2a33bc492def31130f920d8bc7bd6e634beb
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2014-05-23 19:12:01 -0300

    spinner: add spinner_loading

---
+++ src/plugins/spinner_loading/spinner_loading.js
@@ -0,0 +1,42 @@
+// Copyright 2014 Globo.com Player authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+var UIPlugin = require('../../base/ui_plugin');
+var Styler = require('../../base/styler');
+var JST = require('../../base/jst');
+
+var SpinnerLoading = UIPlugin.extend({
+  name: 'spinner_loading',
+  attributes: {
+    "data-spinner":""
+  },
+  initialize: function(options) {
+    SpinnerLoading.super('initialize').call(this, options);
+    this.template = JST[this.name];
+    this.listenTo(this.container, 'container:state:buffering', this.onBuffering);
+    this.listenTo(this.container, 'container:state:bufferfull', this.onBufferFull);
+    this.listenTo(this.container, 'container:stop', this.onStop);
+    this.render();
+  },
+  onBuffering: function() {
+    this.$el.show();
+  },
+  onBufferFull: function() {
+    this.$el.hide();
+  },
+  onStop: function() {
+    this.$el.hide();
+  },
+  render: function() {
+    this.$el.hide();
+    this.$el.html(this.template());
+    var style = Styler.getStyleFor(this.name);
+    this.container.$el.append(style);
+    this.container.$el.append(this.$el);
+    return this;
+  }
+});
+
+module.exports = SpinnerLoading;
+
