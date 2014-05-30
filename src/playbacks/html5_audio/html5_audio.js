539758dca55a8f65632384f66cd05cc23fe8f72c
thiagopnts <thiagopnts@gmail.com>
2014-05-30 19:45:50 -0300

    separe playbacks from plugins
    Conflicts:
    	src/components/loader/loader.js
    	src/playbacks/chromecast/index.js
    	src/playbacks/flash_vod/flash_vod.js
    	src/playbacks/hls/hls.js
    	src/playbacks/html5_video/html5_video.js
    	src/plugins/globo_playback/globo_playback.js
    	src/plugins/lazy_playlist/lazy_playlist.js

---
+++ src/playbacks/html5_audio/html5_audio.js
@@ -0,0 +1,99 @@
+// Copyright 2014 Globo.com Player authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+var UIPlugin = require('../../base/ui_plugin');
+
+var HTML5Audio = UIPlugin.extend({
+  name: 'html5_audio',
+  type: 'playback',
+  tagName: 'audio',
+  events: {
+    'timeupdate': 'timeUpdated',
+    'ended': 'ended'
+  },
+
+  initialize: function(options) {
+    this.el.src = options.src;
+    this.render(); // it should render when the container trigger 'ready'
+  },
+
+  setContainer: function() {
+    this.container.settings = {
+      left: ['playpause'],
+      right: ['volume'],
+      default: ['position', 'seekbar', 'duration']
+    };
+    this.render();
+    this.params.autoPlay && this.play();
+  },
+  bindEvents: function() {
+    this.listenTo(this.container, 'container:play', this.play);
+    this.listenTo(this.container, 'container:pause', this.pause);
+    this.listenTo(this.container, 'container:seek', this.seek);
+    this.listenTo(this.container, 'container:volume', this.volume);
+    this.listenTo(this.container, 'container:stop', this.stop);
+  },
+
+  play: function() {
+    this.el.play();
+  },
+
+  pause: function() {
+    this.el.pause();
+  },
+
+  stop: function() {
+    this.pause();
+    this.el.currentTime = 0;
+  },
+
+  volume: function(value) {
+    this.el.volume = value / 100;
+  },
+
+  mute: function() {
+    this.el.volume = 0;
+  },
+
+  unmute: function() {
+    this.el.volume = 1;
+  },
+
+  isMuted: function() {
+    return !!this.el.volume;
+  },
+
+  ended: function() {
+    this.trigger('container:timeupdate', 0);
+  },
+
+  seek: function(seekBarValue) {
+    var time = this.el.duration * (seekBarValue / 100);
+    this.el.currentTime = time;
+  },
+
+  getCurrentTime: function() {
+    return this.el.currentTime;
+  },
+
+  getDuration: function() {
+    return this.el.duration;
+  },
+
+  timeUpdated: function() {
+    this.container.timeUpdated(this.el.currentTime, this.el.duration);
+  },
+
+  render: function() {
+    this.container.$el.append(this.el);
+    return this;
+  }
+ });
+
+HTML5Audio.canPlay = function(resource) {
+  return !!resource.match(/(.*).mp3/);
+}
+
+
+module.exports = HTML5Audio;
---
+++ src/playbacks/html5_video/html5_video.js
@@ -0,0 +1,143 @@
+// Copyright 2014 Globo.com Player authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+var UIPlugin = require('../../base/ui_plugin');
+var Styler = require('../../base/styler');
+
+var HTML5Video = UIPlugin.extend({
+  name: 'html5_video',
+  type: 'playback',
+  tagName: 'video',
+  attributes: {
+    'data-html5-video': ''
+  },
+
+  events: {
+    'timeupdate': 'timeUpdated',
+    'progress': 'progress',
+    'ended': 'ended',
+    'playing': 'playing',
+    'stalled': 'buffering',
+    'waiting': 'buffering',
+    'canplaythrough': 'bufferFull',
+    'loadedmetadata': 'loadedMetadata'
+  },
+
+  initialize: function(options) {
+    this.options = options;
+    this.src = options.src;
+    this.el.src = options.src;
+    this.el.loop = options.loop;
+    this.settings = {
+      left: ['playpause'],
+      right: ['fullscreen', 'volume'],
+      default: ['position', 'seekbar', 'duration']
+    };
+  },
+
+  loadedMetadata: function(e) {
+    this.trigger('playback:loadedmetadata', e.target.duration);
+  },
+
+  play: function() {
+    this.el.play();
+  },
+
+  pause: function() {
+    this.el.pause();
+  },
+
+  stop: function() {
+    this.pause();
+    if(this.el.readyState !== 0) {
+      this.el.currentTime = 0;
+    }
+  },
+
+  volume: function(value) {
+    this.el.volume = value / 100;
+  },
+
+  mute: function() {
+    this.el.volume = 0;
+  },
+
+  unmute: function() {
+    this.el.volume = 1;
+  },
+
+  isMuted: function() {
+    return !!this.el.volume;
+  },
+
+  isPlaying: function() {
+    return !this.el.paused && !this.el.ended;
+  },
+
+  ended: function() {
+    this.trigger('playback:ended', this.name);
+    this.trigger('playback:timeupdate', 0, this.el.duration, this.name);
+  },
+
+  buffering: function() {
+    this.trigger('playback:buffering', this.name);
+  },
+
+  bufferFull: function() {
+    this.trigger('playback:bufferfull', this.name);
+  },
+
+  destroy: function() {
+    this.stop();
+    this.el.src = '';
+    this.$el.remove();
+  },
+
+  seek: function(seekBarValue) {
+    var time = this.el.duration * (seekBarValue / 100);
+    this.el.currentTime = time;
+  },
+
+  getCurrentTime: function() {
+    return this.el.currentTime;
+  },
+
+  getDuration: function() {
+    return this.el.duration;
+  },
+
+  timeUpdated: function() {
+    this.trigger('playback:timeupdate', this.el.currentTime, this.el.duration, this.name);
+  },
+
+  progress: function() {
+    if (!this.el.buffered.length) return;
+    var bufferedPos = 0;
+    for (var i = 0;  i < this.el.buffered.length; i++) {
+      if (this.el.currentTime >= this.el.buffered.start(i) && this.el.currentTime <= this.el.buffered.end(i)) {
+        bufferedPos = i;
+        break;
+      }
+    }
+    this.trigger('playback:progress', this.el.buffered.start(bufferedPos), this.el.buffered.end(bufferedPos), this.el.duration, this.name);
+  },
+
+  playing: function() {
+    this.trigger('playback:play', this.name);
+  },
+
+  render: function() {
+    var style = Styler.getStyleFor(this.name);
+    this.$el.append(style);
+    this.trigger('playback:ready', this.name);
+    this.options.autoPlay && this.play();
+    return this;
+  }
+});
+
+HTML5Video.canPlay = function(resource) {
+    return !!resource.match(/(.*).mp4/);
+}
+
+module.exports = HTML5Video;
