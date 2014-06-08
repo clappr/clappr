e721997d7fc8dae0c52023d3a827cf40ad182f1b
thiagopnts <thiagopnts@gmail.com>
2014-06-08 17:18:55 -0300

    poster: make it work without env info

--- src/plugins/poster/poster.js
+++ src/plugins/poster/poster.js
@@ -1,4 +1,4 @@
-// Copyright 2014 Globo.com Player authors. All rights reserved.
+//Copyright 2014 Globo.com Player authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
@@ -9,19 +9,21 @@ var $ = require('jquery');
 
 var PosterPlugin = UIPlugin.extend({
   name: 'poster',
-  tagName: 'img',
+  tagName: 'div',
   attributes: {
     'data-poster': ''
   },
   events: {
     'click': 'clicked'
   },
+  template: JST.poster,
   initialize: function(options) {
     PosterPlugin.super('initialize').call(this, options);
+    if (options.disableControlsOnPoster === undefined)
+      options.disableControlsOnPoster = true;
     this.options = options;
     if (this.options.disableControlsOnPoster)
       this.container.disableMediaControl();
-    this.el.src = options.poster || 'assets/default.png';
     this.render();
   },
   bindEvents: function() {
@@ -29,6 +31,7 @@ var PosterPlugin = UIPlugin.extend({
     this.listenTo(this.container, 'container:play', this.onPlay);
     this.listenTo(this.container, 'container:stop', this.onStop);
     this.listenTo(this.container, 'container:ended', this.onStop);
+    this.listenTo(this.container, 'container:pip', this.onPipStateChanged);
   },
   onBuffering: function() {
     this.hidePlayButton();
@@ -61,14 +64,20 @@ var PosterPlugin = UIPlugin.extend({
   showPlayButton: function() {
     this.$playButton.show();
   },
-  render: function() {
-    var style = Styler.getStyleFor(this.name);
-    this.container.$el.append(style);
-    this.container.$el.append(this.el);
-    return this;
-  },
   clicked: function() {
     this.container.play();
+  },
+  render: function() {
+    var style = Styler.getStyleFor(this.name);
+    this.$el.html(this.template());
+    this.$el.append(style);
+    this.container.$el.append(this.el);
+    var imgEl = this.$el.find('img')[0];
+    imgEl.src = this.options.poster || 'assets/default.png';
+    this.$playButton = $(this.$el.find('.play-wrapper'));
+    if (this.options.hidePlayButton)
+      this.$playButton.hide();
+    return this;
   }
 });
 
