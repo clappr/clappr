ae5b487353844868e4a4349491e4a261eff528bc
Bruno Torres <bruno.torres@me.com>
2014-06-27 18:34:55 -0300

    main: reformat code

--- src/main.js
+++ src/main.js
@@ -2,37 +2,37 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-var BaseObject = require('./base/base_object');
-var CoreFactory = require('./components/core_factory');
-var Loader = require('./components/loader');
+var BaseObject = require('./base/base_object')
+var CoreFactory = require('./components/core_factory')
+var Loader = require('./components/loader')
 
 
 class Player extends BaseObject {
   initialize(params) {
-    window.p = this;
-    params.displayType || (params.displayType = 'pip');
-    this.params = params;
-    this.loader = new Loader(this.params);
-    this.coreFactory = new CoreFactory(this, this.loader);
+    window.p = this
+    params.displayType || (params.displayType = 'pip')
+    this.params = params
+    this.loader = new Loader(this.params)
+    this.coreFactory = new CoreFactory(this, this.loader)
   }
 
   attachTo(element) {
-    this.params.parentElement = element;
-    this.core = this.coreFactory.create();
+    this.params.parentElement = element
+    this.core = this.coreFactory.create()
   }
 
   load(params) {
-    this.core.load(params);
+    this.core.load(params)
   }
 
   destroy() {
-    this.core.destroy();
+    this.core.destroy()
   }
 }
 
 
-global.DEBUG = false;
+global.DEBUG = true
 
-window.WP3 = { Player: Player };
+window.WP3 = { Player: Player }
 
-module.exports = window.WP3;
+module.exports = window.WP3
