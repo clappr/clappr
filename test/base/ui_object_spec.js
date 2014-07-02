964681914b85b708bc986cfef19036d8c51363d8
thiagopnts <thiagopnts@gmail.com>
2014-07-02 18:33:22 -0300

    fix tests™

--- test/base/ui_object_spec.js
+++
@@ -1,8 +0,0 @@
-var UIObject = require('../spec_helper').UIObject;
-
-describe('UIObject', function() {
-  it('should have an empty default element', function() {
-    var obj = new UIObject();
-    obj.$el.html().should.be.empty;
-  });
-});
--- test/components/core_spec.js
+++
@@ -1,8 +0,0 @@
-var Core = require('../spec_helper').Core;
-
-describe('Core', function() {
-  it('should create a playbackhandler', function() {
-    var core = new Core({sources: ['http://globo.com/video.mp4'], player: {}});
-    expect(core.playbackHandler).to.be.a('object');
-  });
-});
