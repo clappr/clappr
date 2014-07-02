35c0cfc3a49c01cbb781c3196e3f162108bf402b
thiagopnts <thiagopnts@gmail.com>
2014-07-02 18:23:17 -0300

    add karma.conf.js

---
+++ karma.conf.js
@@ -0,0 +1,82 @@
+// Karma configuration
+// Generated on Wed Jul 02 2014 11:05:24 GMT-0300 (BRT)
+
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['browserify', 'traceur', 'mocha'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      'test/**/*spec.js'
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+      'src/**/*.js': ['traceur'],
+      'test/**/*.js': ['traceur'],
+      'test/**/*.js': ['browserify'] 
+    },
+
+    traceurPreprocessor: {
+      options: {
+        sourceMap: true
+      }
+    },
+
+    browserify: {
+      watch: true,
+      debug: true,
+      transform: ['es6ify'],
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    //browsers: ['Chrome', 'Firefox', 'Safari'],
+    browsers: ['Chrome'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false
+  });
+};
