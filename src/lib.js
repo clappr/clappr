a40da3e99a574c523f621cf5abca32df3384ca62
thiagopnts <thiagopnts@gmail.com>
2014-07-08 11:13:40 -0300

    update lib exports

--- src/lib.js
+++ src/lib.js
@@ -2,20 +2,10 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-var BaseObject = require('./base/base_object')
-var BaseObject = require('./base/ui_object')
 var UIPlugin = require('./base/ui_plugin')
-var Core = require('./components/core')
-var Container = require('./components/container')
-var Loader = require('./components/loader')
 
 global.DEBUG = true
 
 module.exports = {
-  BaseObject,
-  UIPlugin,
-  UIObject,
-  Core,
-  Container,
-  Loader
+  UIPlugin
 }
