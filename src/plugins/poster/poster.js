9f6285cc71a98b012f8599063b09a0578099e105
Bruno Torres <bruno.torres@me.com>
2014-07-10 18:44:52 -0300

    poster plugin: change poster play button behavior when on pip

--- src/plugins/poster/poster.js
+++ src/plugins/poster/poster.js
@@ -2,83 +2,100 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-var UIPlugin = require('../../base/ui_plugin');
-var Styler = require('../../base/styler');
-var JST = require('../../base/jst');
-var $ = require('jquery');
+var UIPlugin = require('../../base/ui_plugin')
+var Styler = require('../../base/styler')
+var JST = require('../../base/jst')
+var $ = require('jquery')
 
-var PosterPlugin = UIPlugin.extend({
-  name: 'poster',
-  tagName: 'div',
-  attributes: {
-    'data-poster': ''
-  },
-  events: {
-    'click': 'clicked'
-  },
-  template: JST.poster,
-  initialize: function(options) {
-    PosterPlugin.super('initialize').call(this, options);
-    if (options.disableControlsOnPoster === undefined)
-      options.disableControlsOnPoster = true;
-    this.options = options;
-    if (this.options.disableControlsOnPoster)
-      this.container.disableMediaControl();
-    this.render();
-  },
-  bindEvents: function() {
-    this.listenTo(this.container, 'container:state:buffering', this.onBuffering);
-    this.listenTo(this.container, 'container:play', this.onPlay);
-    this.listenTo(this.container, 'container:stop', this.onStop);
-    this.listenTo(this.container, 'container:ended', this.onStop);
-    this.listenTo(this.container, 'container:pip', this.onPipStateChanged);
-  },
-  onBuffering: function() {
-    this.hidePlayButton();
-  },
-  onPlay: function() {
-    this.$el.hide();
-    if (this.options.disableControlsOnPoster) {
-      this.container.enableMediaControl();
+class PosterPlugin extends UIPlugin {
+  get name() { return 'poster' }
+  get template() { return JST.poster }
+
+  get attributes() {
+    return {
+      'class': 'player-poster',
+      'data-poster': ''
     }
-  },
-  onStop: function() {
-    this.$el.show();
+  }
+
+  get events() {
+    return {
+      'click': 'clicked'
+    }
+  }
+
+  initialize(options) {
+    super(options)
+    if (options.disableControlsOnPoster === undefined)
+      options.disableControlsOnPoster = true
+    this.options = options
+    if (this.options.disableControlsOnPoster)
+      this.container.disableMediaControl()
+    this.render()
+  }
+
+  bindEvents() {
+    this.listenTo(this.container, 'container:state:buffering', this.onBuffering)
+    this.listenTo(this.container, 'container:play', this.onPlay)
+    this.listenTo(this.container, 'container:stop', this.onStop)
+    this.listenTo(this.container, 'container:ended', this.onStop)
+    this.listenTo(this.container, 'container:pip', this.onPipStateChanged)
+  }
+
+  onBuffering() {
+    this.hidePlayButton()
+  }
+
+  onPlay() {
+    this.$el.hide()
     if (this.options.disableControlsOnPoster) {
-      this.container.disableMediaControl();
+      this.container.enableMediaControl()
+    }
+  }
+
+  onStop() {
+    this.$el.show()
+    if (this.options.disableControlsOnPoster) {
+      this.container.disableMediaControl()
     }
     if (!this.options.hidePlayButton) {
-      this.showPlayButton();
+      this.showPlayButton()
     }
-  },
-  onPipStateChanged: function(isPip) {
-    if (isPip) {
-      this.hidePlayButton();
-    } else if (!this.options.hidePlayButton) {
-      this.showPlayButton();
-    }
-  },
-  hidePlayButton: function() {
-    this.$playButton.hide();
-  },
-  showPlayButton: function() {
-    this.$playButton.show();
-  },
-  clicked: function() {
-    this.container.play();
-  },
-  render: function() {
-    var style = Styler.getStyleFor(this.name);
-    this.$el.html(this.template());
-    this.$el.append(style);
-    this.container.$el.append(this.el);
-    var imgEl = this.$el.find('img')[0];
-    imgEl.src = this.options.poster || 'assets/default.png';
-    this.$playButton = $(this.$el.find('.play-wrapper'));
-    if (this.options.hidePlayButton)
-      this.$playButton.hide();
-    return this;
   }
-});
 
-module.exports = PosterPlugin;
+  onPipStateChanged(isPip) {
+    this.$el.css({ fontSize: this.$el.height() })
+    if (!this.options.hidePlayButton) {
+      this.showPlayButton()
+    }
+  }
+
+  hidePlayButton() {
+    this.$playButton.hide()
+  }
+
+  showPlayButton() {
+    this.$el.css({ fontSize: this.$el.height() })
+    this.$playButton.show()
+  }
+
+  clicked() {
+    this.container.play()
+  }
+
+  render() {
+    var style = Styler.getStyleFor(this.name)
+    this.$el.html(this.template())
+    this.$el.append(style)
+    this.container.$el.append(this.el)
+    this.$el.css({ fontSize: this.options.height })
+    var imgEl = this.$el.find('img')[0]
+    imgEl.src = this.options.poster || 'assets/default.png'
+    this.$playButton = $(this.$el.find('.play-wrapper'))
+    if (this.options.hidePlayButton)
+      this.$playButton.hide()
+    return this
+  }
+}
+
+module.exports = PosterPlugin
