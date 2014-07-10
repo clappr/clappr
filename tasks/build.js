0cd77c5df10dace415f0336c904bb696ff95b7e4
thiagopnts <thiagopnts@gmail.com>
2014-07-10 18:25:10 -0300

    tasks: fix deps

--- tasks/build.js
+++ tasks/build.js
@@ -1,12 +1,15 @@
 var browserify = require('browserify');
 var es6ify = require('es6ify');
+var uglify = require('gulp-uglify');
+var streamify = require('gulp-streamify')
 var args = require('yargs').argv;
 var gulp = require('gulp');
 var rename = require('gulp-rename');
+var source = require('vinyl-source-stream');
 
 
 gulp.task('build', ['pre-build'], function() {
-  var isProd = ['prod', 'production'].indexOf(args.env) !== -1 ? false : true;
+  var isProd = ['prod', 'production'].indexOf(args.env) !== -1 ? true : false;
   var bundle = browserify()
     .transform(es6ify.configure(/^(?!.*node_modules)+.+\.js$/))
     .add(es6ify.runtime)
@@ -16,10 +19,11 @@ gulp.task('build', ['pre-build'], function() {
     .require('./src/base/ui_object', { expose: 'ui_object' })
     .bundle();
 
-  return bundle.pipe(source('main.js'))
-    .pipe(rename(isProd ? 'player.min.js' : 'player.js'))
-    .pipe(gulp.dest('./dist'))
-    .on("error", function(err) {
-      throw err;
-    });
+  var stream = bundle.pipe(source('main.js'))
+    .pipe(rename(isProd ? 'player.min.js' : 'player.js'));
+
+    if(isProd) {
+      stream.pipe(streamify(uglify()));
+    }
+    stream.pipe(gulp.dest('./dist'))
 });
--- tasks/pre-build.js
+++ tasks/pre-build.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
 var exec = require('child_process').exec;
 
-gulp.task('pre-build', ['sass', 'copy-html', 'copy-css'], function() {
-  exec('node bin/hook.js', noop);
+gulp.task('pre-build', ['sass', 'copy-html', 'copy-css'], function(done) {
+  exec('node bin/hook.js', done);
 });
