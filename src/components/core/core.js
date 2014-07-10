fe05a71645fbcada8924da2030c67b89a1e5e94b
Bruno Torres <bruno.torres@me.com>
2014-07-10 16:58:27 -0300

    core: change to es6 syntax

--- src/components/core/core.js
+++ src/components/core/core.js
@@ -7,189 +7,204 @@
  * and the player state.
  */
 
-var _ = require('underscore');
-var $ = require('jquery');
+var _ = require('underscore')
+var $ = require('jquery')
 
-var UIObject = require('../../base/ui_object');
-var ContainerFactory = require('../container_factory');
-var Fullscreen = require('../../base/utils').Fullscreen;
-var Loader = require('../loader');
-var Styler = require('../../base/styler');
-var MediaControl = require('../media_control');
+var UIObject = require('../../base/ui_object')
+var ContainerFactory = require('../container_factory')
+var Fullscreen = require('../../base/utils').Fullscreen
+var Loader = require('../loader')
+var Styler = require('../../base/styler')
+var MediaControl = require('../media_control')
 
-var Core = UIObject.extend({
-  events: {
-    'webkitfullscreenchange': 'exit',
-    'mousemove': 'showMediaControl',
-    'mouseleave': 'hideMediaControl'
-  },
-  attributes: {
-    'data-player': ''
-  },
-  initialize: function(params) {
-    window.$ = $;
-    this.defer = $.Deferred();
-    this.defer.promise(this);
-    this.plugins = [];
-    this.containers = [];
-    this.params = params;
-    //this.setupExternalInterface();
-    this.params.displayType || (this.params.displayType = 'pip');
-    this.parentElement = params.parentElement;
-    this.loader = new Loader(params);
-    this.containerFactory = new ContainerFactory(params, this.loader);
+class Core extends UIObject {
+  get events() {
+    return {
+      'webkitfullscreenchange': 'exit',
+      'mousemove': 'showMediaControl',
+      'mouseleave': 'hideMediaControl'
+    }
+  }
+
+  get attributes() {
+    return {
+      'data-player': ''
+    }
+  }
+
+  initialize(params) {
+    this.defer = $.Deferred()
+    this.defer.promise(this)
+    this.plugins = []
+    this.containers = []
+    this.params = params
+    this.params.displayType || (this.params.displayType = 'pip')
+    this.parentElement = params.parentElement
+    this.loader = new Loader(params)
+    this.containerFactory = new ContainerFactory(params, this.loader)
     this.containerFactory
       .createContainers()
-      .then(this.setupContainers.bind(this))
-      .then(this.resolveOnContainersReady.bind(this));
-    this.updateSize();
+      .then((containers) => this.setupContainers(containers))
+      .then((containers) => this.resolveOnContainersReady(containers))
+    this.updateSize()
     //FIXME fullscreen api sucks
-    document.addEventListener('mozfullscreenchange', this.exit.bind(this));
-    $(window).resize(this.updateSize.bind(this));
-  },
-  updateSize: function() {
-   if(Fullscreen.isFullscreen()) {
-      this.$el.addClass('fullscreen');
-      this.$el.removeAttr('style');
+    document.addEventListener('mozfullscreenchange', () => this.exit())
+    $(window).resize(() => this.updateSize())
+  }
+
+  updateSize() {
+   if (Fullscreen.isFullscreen()) {
+      this.$el.addClass('fullscreen')
+      this.$el.removeAttr('style')
     } else {
-      var width = 0;
-      var height = 0;
+      var width = 0
+      var height = 0
       if (this.params.stretchWidth && this.params.stretchHeight && this.params.stretchWidth <= window.innerWidth && this.params.stretchHeight <= (window.innerHeight * 0.73)) {
-        width = this.params.stretchWidth;
-        height = this.params.stretchHeight;
+        width = this.params.stretchWidth
+        height = this.params.stretchHeight
       } else {
         if (this.params.width) {
-          width = this.params.width;
+          width = this.params.width
         }
         if (this.params.height) {
-          height = this.params.height;
+          height = this.params.height
         }
       }
-
       if (width > 0) {
-        this.$el.css({ width: width });
+        this.$el.css({ width: width })
       }
-
       if (height > 0) {
-        this.$el.css({ height: height });
+        this.$el.css({ height: height })
       }
-
-      this.$el.removeClass('fullscreen');
+      this.$el.removeClass('fullscreen')
     }
-  },
-  resolveOnContainersReady: function(containers) {
-    $.when.apply($, containers).done(function() {
-      this.defer.resolve(this);
-    }.bind(this));
-  },
-  addPlugin: function(plugin) {
-    this.plugins.push(plugin);
-  },
-  hasPlugin: function(name) {
-    return !!this.getPlugin(name);
-  },
-  getPlugin: function(name) {
-    return _(this.plugins).find(function(plugin) { return plugin.name === name });
-  },
-  load: function(params) {
-    _(this.containers).each(function(container) {
-      container.destroy();
-    });
-    this.containerFactory.params = _(this.params).extend(params);
-    this.containerFactory.createContainers()
-      .then(this.setupContainers.bind(this));
-  },
-  destroy: function() {
-    _(this.containers).each(function(container) {
-      container.destroy();
-    });
-    this.$el.remove();
-  },
-  exit: function() {
-    this.updateSize();
-    this.mediaControl.show();
-  },
-  setMediaControlContainer: function(container) {
-    this.mediaControl.setContainer(container);
-    this.mediaControl.render();
-  },
-  disableMediaControl: function() {
-    this.mediaControl.disable();
-    this.$el.removeClass('nocursor');
-  },
-  enableMediaControl: function() {
-    this.mediaControl.enable();
-  },
-  removeContainer: function(container) {
-    console.log('container being removed');
-    this.stopListening(container);
-    this.containers = _.without(this.containers, container);
-  },
-  appendContainer: function(container) {
-    this.listenTo(container, 'container:destroyed', this.removeContainer);
-    this.el.appendChild(container.render().el);
-    this.containers.push(container);
-  },
-  prependContainer: function(container) {
-    this.listenTo(container, 'container:destroyed', this.removeContainer);
-    this.$el.append(container.render().el);
-    this.containers.unshift(container);
-  },
-  setupContainers: function(containers) {
-    console.log('setupContainers');
-    _.map(containers, this.appendContainer, this);
-    this.createMediaControl(this.getCurrentContainer());
-    this.render();
-    this.$el.appendTo(this.parentElement);
-    return containers;
-  },
-  createContainer: function(source) {
-    var container = this.containerFactory.createContainer(source);
-    this.appendContainer(container);
-    return container;
-  },
-  createMediaControl: function(container) {
-    var params = _.extend({container: container}, this.params);
-    this.mediaControl = new MediaControl(_.extend({container: container}, this.params));
-    this.listenTo(this.mediaControl, 'mediacontrol:fullscreen', this.toggleFullscreen);
-    this.listenTo(this.mediaControl, 'mediacontrol:show', this.onMediaControlShow.bind(this, true));
-    this.listenTo(this.mediaControl, 'mediacontrol:hide', this.onMediaControlShow.bind(this, false));
-  },
-  getCurrentContainer: function() {
-    return this.containers[0];
-  },
-  toggleFullscreen: function() {
-    if (!Fullscreen.isFullscreen()) {
-      Fullscreen.requestFullscreen(this.el);
-      this.$el.addClass('fullscreen');
-    } else {
-      Fullscreen.cancelFullscreen();
-
-      this.$el.removeClass('fullscreen nocursor');
-    }
-    this.mediaControl.show();
-  },
-  showMediaControl: function(event) {
-    this.mediaControl.show(event);
-  },
-  hideMediaControl: function(event) {
-    this.mediaControl.hide(event);
-  },
-  onMediaControlShow: function(showing) {
-    if (showing)
-      this.$el.removeClass('nocursor');
-    else if (Fullscreen.isFullscreen())
-      this.$el.addClass('nocursor');
-  },
-  render: function() {
-    var style = Styler.getStyleFor('core');
-    //FIXME
-    //this.$el.empty();
-    this.$el.append(style);
-
-    this.$el.append(this.mediaControl.render().el);
-    return this;
   }
-});
 
-module.exports = Core;
+  resolveOnContainersReady(containers) {
+    $.when.apply($, containers).done(() =>this.defer.resolve(this))
+  }
+
+  addPlugin(plugin) {
+    this.plugins.push(plugin)
+  }
+
+  hasPlugin(name) {
+    return !!this.getPlugin(name)
+  }
+
+  getPlugin(name) {
+    return _(this.plugins).find((plugin) => plugin.name === name)
+  }
+
+  load(params) {
+    _(this.containers).each((container) => container.destroy())
+    this.containerFactory.params = _(this.params).extend(params)
+    this.containerFactory.createContainers().then(() => this.setupContainers())
+  }
+
+  destroy() {
+    _(this.containers).each((container) => container.destroy())
+    this.$el.remove()
+  }
+
+  exit() {
+    this.updateSize()
+    this.mediaControl.show()
+  }
+
+  setMediaControlContainer(container) {
+    this.mediaControl.setContainer(container)
+    this.mediaControl.render()
+  }
+
+  disableMediaControl() {
+    this.mediaControl.disable()
+    this.$el.removeClass('nocursor')
+  }
+
+  enableMediaControl() {
+    this.mediaControl.enable()
+  }
+
+  removeContainer(container) {
+    console.log('container being removed')
+    this.stopListening(container)
+    this.containers = _.without(this.containers, container)
+  }
+
+  appendContainer(container) {
+    this.listenTo(container, 'container:destroyed', this.removeContainer)
+    this.el.appendChild(container.render().el)
+    this.containers.push(container)
+  }
+
+  prependContainer(container) {
+    this.listenTo(container, 'container:destroyed', this.removeContainer)
+    this.$el.append(container.render().el)
+    this.containers.unshift(container)
+  }
+
+  setupContainers(containers) {
+    console.log('setupContainers')
+    _.map(containers, this.appendContainer, this)
+    this.createMediaControl(this.getCurrentContainer())
+    this.render()
+    this.$el.appendTo(this.parentElement)
+    return containers
+  }
+
+  createContainer(source) {
+    var container = this.containerFactory.createContainer(source)
+    this.appendContainer(container)
+    return container
+  }
+
+  createMediaControl(container) {
+    var params = _.extend({container: container}, this.params)
+    this.mediaControl = new MediaControl(_.extend({container: container}, this.params))
+    this.listenTo(this.mediaControl, 'mediacontrol:fullscreen', this.toggleFullscreen)
+    this.listenTo(this.mediaControl, 'mediacontrol:show', this.onMediaControlShow.bind(this, true))
+    this.listenTo(this.mediaControl, 'mediacontrol:hide', this.onMediaControlShow.bind(this, false))
+  }
+
+  getCurrentContainer() {
+    return this.containers[0]
+  }
+
+  toggleFullscreen() {
+    if (!Fullscreen.isFullscreen()) {
+      Fullscreen.requestFullscreen(this.el)
+      this.$el.addClass('fullscreen')
+    } else {
+      Fullscreen.cancelFullscreen()
+      this.$el.removeClass('fullscreen nocursor')
+    }
+    this.mediaControl.show()
+  }
+
+  showMediaControl(event) {
+    this.mediaControl.show(event)
+  }
+
+  hideMediaControl(event) {
+    this.mediaControl.hide(event)
+  }
+
+  onMediaControlShow(showing) {
+    if (showing)
+      this.$el.removeClass('nocursor')
+    else if (Fullscreen.isFullscreen())
+      this.$el.addClass('nocursor')
+  }
+
+  render() {
+    var style = Styler.getStyleFor('core')
+    //FIXME
+    //this.$el.empty()
+    this.$el.append(style)
+    this.$el.append(this.mediaControl.render().el)
+    return this
+  }
+}
+
+module.exports = Core
