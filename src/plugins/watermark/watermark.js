2f33f118a75c6036e2fe618f32166e2c5bb386e8
Bruno Torres <bruno.torres@me.com>
2014-07-10 18:05:41 -0300

    watermark plugin: fix visibility issues on pip

--- src/plugins/watermark/watermark.js
+++ src/plugins/watermark/watermark.js
@@ -2,48 +2,55 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-var UIPlugin = require('../../base/ui_plugin');
-var Styler = require('../../base/styler');
-var JST = require('../../base/jst');
+var UIPlugin = require('../../base/ui_plugin')
+var Styler = require('../../base/styler')
+var JST = require('../../base/jst')
 
-var WaterMarkPlugin = UIPlugin.extend({
-  name: 'watermark',
-  type: 'ui',
-  initialize: function(options) {
-    WaterMarkPlugin.super('initialize').call(this, options);
-    this.template = JST[this.name];
-    this.position = options.position || "bottom-right";
-    this.imageUrl = options.imageUrl || 'assets/watermark.png';
-    this.render();
-  },
-  bindEvents: function() {
-    this.listenTo(this.container, 'container:play', this.onPlay);
-    this.listenTo(this.container, 'container:stop', this.onStop);
-    this.listenTo(this.container, 'container:pip', this.onPip);
-  },
-  onPlay: function() {
-    this.$el.show();
-  },
-  onStop: function() {
-    this.$el.hide();
-  },
-  onPip: function(isPip) {
-    if (isPip) {
-      this.$el.hide();
-    } else {
-      this.$el.show();
-    }
-  },
-  render: function() {
-    this.$el.hide();
-    var templateOptions = {position: this.position, imageUrl: this.imageUrl};
-    this.$el.html(this.template(templateOptions));
-    var style = Styler.getStyleFor(this.name);
-    this.container.$el.append(style);
-    this.container.$el.append(this.$el);
-    return this;
+class WaterMarkPlugin extends UIPlugin {
+  get name() { return 'watermark' }
+  get type() { return 'ui' }
+
+  initialize(options) {
+    super(options)
+    this.template = JST[this.name]
+    this.position = options.position || "bottom-right"
+    this.imageUrl = options.imageUrl || 'assets/watermark.png'
+    this.render()
   }
-});
 
-module.exports = WaterMarkPlugin;
+  bindEvents() {
+    this.listenTo(this.container, 'container:play', this.onPlay)
+    this.listenTo(this.container, 'container:stop', this.onStop)
+    this.listenTo(this.container, 'container:pip', this.onPip)
+  }
 
+  onPlay() {
+    if (!this.hidden)
+      this.$el.show()
+  }
+
+  onStop() {
+    this.$el.hide()
+  }
+
+  onPip(isPip) {
+    this.hidden = !!isPip
+    if (isPip) {
+      this.$el.hide()
+    } else {
+      this.$el.show()
+    }
+  }
+
+  render() {
+    this.$el.hide()
+    var templateOptions = {position: this.position, imageUrl: this.imageUrl}
+    this.$el.html(this.template(templateOptions))
+    var style = Styler.getStyleFor(this.name)
+    this.container.$el.append(style)
+    this.container.$el.append(this.$el)
+    return this
+  }
+}
+
+module.exports = WaterMarkPlugin
