647b449d79561aee145d8ff69f7d344da1f2b951
Bruno Torres <bruno.torres@me.com>
2014-07-10 18:45:34 -0300

    html5 audio playback: change to es6 syntax

--- src/playbacks/html5_audio/html5_audio.js
+++ src/playbacks/html5_audio/html5_audio.js
@@ -2,98 +2,100 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-var UIPlugin = require('../../base/ui_plugin');
+var UIPlugin = require('../../base/ui_plugin')
 
-var HTML5Audio = UIPlugin.extend({
-  name: 'html5_audio',
-  type: 'playback',
-  tagName: 'audio',
-  events: {
-    'timeupdate': 'timeUpdated',
-    'ended': 'ended'
-  },
+class HTML5Audio extends UIPlugin {
+  get name() { return 'html5_audio' }
+  get type() { return 'playback' }
+  get tagName() { return 'audio' }
+  get events() {
+    return {
+      'timeupdate': 'timeUpdated',
+      'ended': 'ended'
+    }
+  }
 
-  initialize: function(options) {
-    this.el.src = options.src;
-    this.render(); // it should render when the container trigger 'ready'
-  },
+  initialize(options) {
+    this.el.src = options.src
+    this.render() // it should render when the container trigger 'ready'
+  }
 
-  setContainer: function() {
+  setContainer() {
     this.container.settings = {
       left: ['playpause'],
       right: ['volume'],
       default: ['position', 'seekbar', 'duration']
-    };
-    this.render();
-    this.params.autoPlay && this.play();
-  },
-  bindEvents: function() {
-    this.listenTo(this.container, 'container:play', this.play);
-    this.listenTo(this.container, 'container:pause', this.pause);
-    this.listenTo(this.container, 'container:seek', this.seek);
-    this.listenTo(this.container, 'container:volume', this.volume);
-    this.listenTo(this.container, 'container:stop', this.stop);
-  },
-
-  play: function() {
-    this.el.play();
-  },
-
-  pause: function() {
-    this.el.pause();
-  },
-
-  stop: function() {
-    this.pause();
-    this.el.currentTime = 0;
-  },
-
-  volume: function(value) {
-    this.el.volume = value / 100;
-  },
-
-  mute: function() {
-    this.el.volume = 0;
-  },
-
-  unmute: function() {
-    this.el.volume = 1;
-  },
-
-  isMuted: function() {
-    return !!this.el.volume;
-  },
-
-  ended: function() {
-    this.trigger('container:timeupdate', 0);
-  },
-
-  seek: function(seekBarValue) {
-    var time = this.el.duration * (seekBarValue / 100);
-    this.el.currentTime = time;
-  },
-
-  getCurrentTime: function() {
-    return this.el.currentTime;
-  },
-
-  getDuration: function() {
-    return this.el.duration;
-  },
-
-  timeUpdated: function() {
-    this.container.timeUpdated(this.el.currentTime, this.el.duration);
-  },
-
-  render: function() {
-    this.container.$el.append(this.el);
-    return this;
+    }
+    this.render()
+    this.params.autoPlay && this.play()
   }
- });
+  bindEvents() {
+    this.listenTo(this.container, 'container:play', this.play)
+    this.listenTo(this.container, 'container:pause', this.pause)
+    this.listenTo(this.container, 'container:seek', this.seek)
+    this.listenTo(this.container, 'container:volume', this.volume)
+    this.listenTo(this.container, 'container:stop', this.stop)
+  }
+
+  play() {
+    this.el.play()
+  }
+
+  pause() {
+    this.el.pause()
+  }
+
+  stop() {
+    this.pause()
+    this.el.currentTime = 0
+  }
+
+  volume(value) {
+    this.el.volume = value / 100
+  }
+
+  mute() {
+    this.el.volume = 0
+  }
+
+  unmute() {
+    this.el.volume = 1
+  }
+
+  isMuted() {
+    return !!this.el.volume
+  }
+
+  ended() {
+    this.trigger('container:timeupdate', 0)
+  }
+
+  seek(seekBarValue) {
+    var time = this.el.duration * (seekBarValue / 100)
+    this.el.currentTime = time
+  }
+
+  getCurrentTime() {
+    return this.el.currentTime
+  }
+
+  getDuration() {
+    return this.el.duration
+  }
+
+  timeUpdated() {
+    this.container.timeUpdated(this.el.currentTime, this.el.duration)
+  }
+
+  render() {
+    this.container.$el.append(this.el)
+    return this
+  }
+ }
 
 HTML5Audio.canPlay = function(resource) {
-  return !!resource.match(/(.*).mp3/);
+  return !!resource.match(/(.*).mp3/)
 }
 
 
-module.exports = HTML5Audio;
+module.exports = HTML5Audio
