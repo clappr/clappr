00e8917b3112eb30842dbe98423ef38ac0108b17
thiagopnts <thiagopnts@gmail.com>
2014-07-14 11:59:59 -0300

    add common.js helper for tasks

---
+++ tasks/common.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var es6ify = require('es6ify');
+var rename = require('gulp-rename');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+
+module.exports.js = 'src/**/*.js';
+
+module.exports.browserify = function() {
+  return browserify()
+    .transform(es6ify.configure(/^(?!.*node_modules)+.+\.js$/))
+    .add(es6ify.runtime)
+    .require('./src/main.js', { entry: true })
+    .require('./src/base/ui_plugin', { expose: 'ui_plugin' })
+    .require('./src/base/base_object', { expose: 'base_object' })
+    .require('./src/base/ui_object', { expose: 'ui_object' });
+};
+
+gulp.task('compile-js', function() {
+  return module.exports.browserify()
+  .bundle()
+  .pipe(source('player.js'))
+  .pipe(rename('player.js'))
+  .pipe(gulp.dest('./dist'))
+});
+
