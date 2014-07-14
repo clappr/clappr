ae2d88090cf08fdd0d6872ad9af344a8b5699c9d
thiagopnts <thiagopnts@gmail.com>
2014-07-14 12:00:17 -0300

    add watch task

---
+++ tasks/watch.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp');
+var util = require('gulp-util');
+var livereload = require('gulp-livereload');
+var watchify = require('gulp-watchify');
+
+gulp.task('watch', function() {
+  var reloadServer = livereload();
+
+  var js = gulp.watch('./src/**/*.js');
+  js.on('change', function(event) {
+    gulp.start('compile-js', function() {
+      reloadServer.changed(event.path);
+    });
+  });
+
+  var assets = gulp.watch('./src/**/*.{html,scss,css}');
+  assets.on('change', function(event) {
+    gulp.start(['sass', 'copy-html', 'copy-css'], function() {
+      reloadServer.changed(event.path);
+    });
+  });
+
+  util.log(util.colors.bgGreen('Watching for changes...'));
+});
