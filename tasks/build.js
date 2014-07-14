53d1079cdc38bfd89b21fec97516107d94abd2a7
thiagopnts <thiagopnts@gmail.com>
2014-07-14 12:03:19 -0300

    use common browserify task in build task

--- tasks/build.js
+++ tasks/build.js
@@ -1,4 +1,3 @@
-var browserify = require('browserify');
 var es6ify = require('es6ify');
 var uglify = require('gulp-uglify');
 var streamify = require('gulp-streamify')
@@ -6,24 +5,19 @@ var args = require('yargs').argv;
 var gulp = require('gulp');
 var rename = require('gulp-rename');
 var source = require('vinyl-source-stream');
+var browserify = require('./common').browserify;
 
 
-gulp.task('build', ['pre-build'], function() {
+gulp.task('build', ['pre-build'], function(b) {
   var isProd = ['prod', 'production'].indexOf(args.env) !== -1 ? true : false;
-  var bundle = browserify()
-    .transform(es6ify.configure(/^(?!.*node_modules)+.+\.js$/))
-    .add(es6ify.runtime)
-    .require('./src/main.js', { entry: true })
-    .require('./src/base/ui_plugin', { expose: 'ui_plugin' })
-    .require('./src/base/base_object', { expose: 'base_object' })
-    .require('./src/base/ui_object', { expose: 'ui_object' })
-    .bundle();
 
-  var stream = bundle.pipe(source('main.js'))
-    .pipe(rename(isProd ? 'player.min.js' : 'player.js'));
+  var stream = browserify()
+  .bundle()
+  .pipe(source('main.js'))
+  .pipe(rename(isProd ? 'player.min.js' : 'player.js'));
 
-    if(isProd) {
-      stream.pipe(streamify(uglify()));
-    }
-    stream.pipe(gulp.dest('./dist'))
+  if(isProd) {
+    stream.pipe(streamify(uglify()));
+  }
+  stream.pipe(gulp.dest('./dist'))
 });
