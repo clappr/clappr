fec436259d30a1cd1dc356483487ab748dc38f46
thiagopnts <thiagopnts@gmail.com>
2014-07-14 14:26:30 -0300

    gulpfile: cleanup requires

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,29 +1,6 @@
-var gulp   = require('gulp');
-var uglify = require('gulp-uglify');
-var utils = require('gulp-util');
-var rename = require('gulp-rename');
-var browserify = require('browserify');
-var path = require('path');
-var express = require('express');
+var gulp  = require('gulp');
+var path  = require('path');
 var karma = require('karma').server;
-var streamify = require('gulp-streamify')
-var source = require('vinyl-source-stream');
-
-var server = express();
-server.use(express.static('./dist'));
-server.use(express.static('./test'));
-
-var paths = {
-  files: ['src/**/*.js', 'src/**/*.css', 'src/**/*.html', 'src/**/*.scss'],
-  main: ['src/main.js'],
-  tests: ['test/**/*.js'],
-  dest: 'dist'
-};
-
-var port = 3000;
-
-var distFile = 'player.js';
-var distTestFile = 'tests_bundle.js';
 
 require('./tasks/assets');
 require('./tasks/pre-build');
