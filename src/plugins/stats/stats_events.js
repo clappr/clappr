bd4ee6761488b8207141cddf7f8a1c8e8903b3b6
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2014-08-15 15:50:19 -0300

    stats: port stats_events to es6

--- src/plugins/stats/stats_events.js
+++ src/plugins/stats/stats_events.js
@@ -2,13 +2,14 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-var StatsEvents = {
-  statsAdd: function(metric) {
-    this.trigger('container:stats:add', metric);
-  },
-  statsReport: function(metrics) {
-    this.trigger('container:stats:report', metrics);
+
+class StatsEvents {
+  statsAdd(metric) {
+    this.trigger('container:stats:add', metric)
   }
-};
+  statsReport(metrics) {
+    this.trigger('container:stats:report', metrics)
+  }
+}
 
 module.exports = StatsEvents;
