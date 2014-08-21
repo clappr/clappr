6e6b9c158d6e47b3953f8a206f40d3e6f30c4ecf
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2014-08-21 16:14:00 -0300

    spinner loading, spinner three bounce: port to es6

--- src/plugins/spinner_loading/spinner_loading.js
+++ src/plugins/spinner_loading/spinner_loading.js
@@ -6,37 +6,42 @@ var UIPlugin = require('../../base/ui_plugin');
 var Styler = require('../../base/styler');
 var JST = require('../../base/jst');
 
-var SpinnerLoading = UIPlugin.extend({
-  name: 'spinner_loading',
-  attributes: {
-    "data-spinner":""
-  },
-  initialize: function(options) {
-    SpinnerLoading.super('initialize').call(this, options);
-    this.template = JST[this.name];
-    this.listenTo(this.container, 'container:state:buffering', this.onBuffering);
-    this.listenTo(this.container, 'container:state:bufferfull', this.onBufferFull);
-    this.listenTo(this.container, 'container:stop', this.onStop);
-    this.render();
-  },
-  onBuffering: function() {
-    this.$el.show();
-  },
-  onBufferFull: function() {
-    this.$el.hide();
-  },
-  onStop: function() {
-    this.$el.hide();
-  },
-  render: function() {
-    this.$el.hide();
-    this.$el.html(this.template());
-    var style = Styler.getStyleFor(this.name);
-    this.container.$el.append(style);
-    this.container.$el.append(this.$el);
-    return this;
+class SpinnerLoading extends UIPlugin {
+  get name() { return 'spinner_loading' }
+  get attributes() {
+    return { 'data-spinner':'' }
   }
-});
+
+  initialize(options) {
+    super(options)
+    this.template = JST[this.name]
+    this.listenTo(this.container, 'container:state:buffering', this.onBuffering)
+    this.listenTo(this.container, 'container:state:bufferfull', this.onBufferFull)
+    this.listenTo(this.container, 'container:stop', this.onStop)
+    this.render()
+  }
+
+  onBuffering() {
+    this.$el.show()
+  }
+
+  onBufferFull() {
+    this.$el.hide()
+  }
+
+  onStop() {
+    this.$el.hide()
+  }
+
+  render() {
+    this.$el.hide()
+    this.$el.html(this.template())
+    var style = Styler.getStyleFor(this.name)
+    this.container.$el.append(style)
+    this.container.$el.append(this.$el)
+    return this
+  }
+}
 
 module.exports = SpinnerLoading;
 
--- src/plugins/spinner_three_bounce/spinner_three_bounce.js
+++ src/plugins/spinner_three_bounce/spinner_three_bounce.js
@@ -6,37 +6,44 @@ var UIPlugin = require('../../base/ui_plugin');
 var Styler = require('../../base/styler');
 var JST = require('../../base/jst');
 
-var SpinnerThreeBouncePlugin = UIPlugin.extend({
-  name: 'spinner_three_bounce',
-  attributes: {
-    'data-spinner':'',
-    'class': 'spinner-three-bounce'
-  },
-  initialize: function(options) {
-    SpinnerThreeBouncePlugin.super('initialize').call(this, options);
-    this.template = JST[this.name];
-    this.listenTo(this.container, 'container:state:buffering', this.onBuffering);
-    this.listenTo(this.container, 'container:state:bufferfull', this.onBufferFull);
-    this.listenTo(this.container, 'container:stop', this.onStop);
-    this.render();
-  },
-  onBuffering: function() {
-    this.$el.show();
-  },
-  onBufferFull: function() {
-    this.$el.hide();
-  },
-  onStop: function() {
-    this.$el.hide();
-  },
-  render: function() {
-    this.$el.hide();
-    this.$el.html(this.template());
-    var style = Styler.getStyleFor(this.name);
-    this.container.$el.append(style);
-    this.container.$el.append(this.$el);
-    return this;
+class SpinnerThreeBouncePlugin extends UIPlugin {
+  get name() { return 'spinner_three_bounce' }
+  get attributes() {
+    return {
+      'data-spinner':'',
+      'class': 'spinner-three-bounce'
+    }
   }
-});
+
+  initialize(options) {
+    super(options)
+    this.template = JST[this.name]
+    this.listenTo(this.container, 'container:state:buffering', this.onBuffering)
+    this.listenTo(this.container, 'container:state:bufferfull', this.onBufferFull)
+    this.listenTo(this.container, 'container:stop', this.onStop)
+    this.render()
+  }
+
+  onBuffering() {
+    this.$el.show()
+  }
+
+  onBufferFull() {
+    this.$el.hide()
+  }
+
+  onStop() {
+    this.$el.hide()
+  }
+
+  render() {
+    this.$el.hide()
+    this.$el.html(this.template())
+    var style = Styler.getStyleFor(this.name)
+    this.container.$el.append(style)
+    this.container.$el.append(this.$el)
+    return this
+  }
+}
 
 module.exports = SpinnerThreeBouncePlugin;
