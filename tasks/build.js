fc5a169bc450fbc37cda83f9c9db3e08ac2d8f6d
thiagopnts <thiagopnts@gmail.com>
2014-08-27 18:01:14 -0300

    gulpfile: remove environment concept from build

--- tasks/build.js
+++ tasks/build.js
@@ -1,23 +1,12 @@
-var es6ify = require('es6ify');
-var uglify = require('gulp-uglify');
-var streamify = require('gulp-streamify')
-var args = require('yargs').argv;
 var gulp = require('gulp');
 var rename = require('gulp-rename');
 var source = require('vinyl-source-stream');
 var browserify = require('./common').browserify;
 
-
 gulp.task('build', ['pre-build'], function(b) {
-  var isProd = ['prod', 'production'].indexOf(args.env) !== -1 ? true : false;
-
-  var stream = browserify()
+  return browserify()
     .bundle()
     .pipe(source('main.js'))
-    .pipe(rename(isProd ? 'clappr.min.js' : 'clappr.js'));
-
-  if(isProd) {
-    stream.pipe(streamify(uglify()));
-  }
-  return stream.pipe(gulp.dest('./dist'));
+    .pipe(rename('clappr.js'))
+    .pipe(gulp.dest('./dist'));
 });
