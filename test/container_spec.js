bfba635a4cc025210c0cac32c6c9866fc95465ab
thiagopnts <thiagopnts@gmail.com>
2014-08-29 17:43:10 -0300

    inject chai and sinon to avoid need to require it in all tests

--- test/container_spec.js
+++ test/container_spec.js
@@ -1,8 +1,5 @@
 var Container = require('../src/components/container');
 var FakePlayback = require('./fakes/fake_playback');
-var expect = require('chai').expect;
-var assert = require('chai').assert;
-var sinon = require('sinon');
 
 describe('Container', () => {
   beforeEach(() => {
