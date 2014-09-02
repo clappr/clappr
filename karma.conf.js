b38e1b7a1dd164a4cee99a4b0bcb90bab6766e64
thiagopnts <thiagopnts@gmail.com>
2014-09-02 11:51:19 -0300

    poster: add tests :clap:

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['browserify', 'traceur', 'mocha', 'chai', 'sinon'],
+    frameworks: ['browserify', 'traceur', 'mocha', 'sinon-chai', 'jquery-2.1.0'],
 
 
     // list of files / patterns to load in the browser
