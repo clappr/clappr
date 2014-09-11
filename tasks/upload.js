17e783097109ef9c8c8661e7a00b017905808684
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2014-09-11 11:33:35 -0300

    tasks: add --tag to upload task (close #116)

--- tasks/upload.js
+++ tasks/upload.js
@@ -1,12 +1,26 @@
 var gulp = require('gulp');
 var s3 = require('s3');
 var fs = require('fs');
+var args = require('yargs').argv;
+var exec = require('child_process').exec;
 
 gulp.task('upload', ['release'], function(b) {
+  var tag = args.tag || undefined;
+  upload('latest/');
+  if (tag) {
+    exec('git tag ' + tag)
+    exec('git push origin master --tags')
+    upload(tag + '/');
+  }
+  return;
+});
+
+var upload = function(prefix) {
   var awsOptions = JSON.parse(fs.readFileSync('./aws.json'));
   var client = s3.createClient({s3Options: awsOptions});
-  var params = {localDir: "./dist/", deleteRemoved: true, s3Params: {Bucket: "cdn.clappr.io", Prefix: "latest/"}};
+  var params = {localDir: "./dist/", deleteRemoved: true, s3Params: {Bucket: "cdn.clappr.io", Prefix: prefix}};
   var uploader = client.uploadDir(params);
   uploader.on('error', function(err) { console.error("unable to sync:", err.stack); });
-  uploader.on('end', function() { console.log("done uploading"); });
-});
+  uploader.on('end', function() { console.log("done uploading for " + prefix); });
+}
+
