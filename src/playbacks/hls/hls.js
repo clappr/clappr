2e73b6ade35f9b74aed006ce7795c54c714e1906
thiagopnts <thiagopnts@gmail.com>
2014-10-08 11:33:06 -0300

    hls: use playback base class

--- src/playbacks/hls/hls.js
+++ src/playbacks/hls/hls.js
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-var UIPlugin = require('../../base/ui_plugin')
+var Playback = require('../../base/playback')
 var Styler = require('../../base/styler')
 var JST = require('../../base/jst')
 var _ = require("underscore")
@@ -11,7 +11,7 @@ var Browser = require('../../components/browser')
 
 var objectIE = '<object type="application/x-shockwave-flash" id="<%= cid %>" class="hls-playback" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" data-hls="" width="100%" height="100%"><param name="movie" value="<%= swfPath %>"> <param name="quality" value="autohigh"> <param name="swliveconnect" value="true"> <param name="allowScriptAccess" value="always"> <param name="bgcolor" value="#001122"> <param name="allowFullScreen" value="false"> <param name="wmode" value="transparent"> <param name="tabindex" value="1"> <param name=FlashVars value="playbackId=<%= playbackId %>" /> </object>'
 
-class HLS extends UIPlugin {
+class HLS extends Playback {
   get name() { return 'hls' }
   get tagName() { return 'object' }
   get template() { return JST.hls }
