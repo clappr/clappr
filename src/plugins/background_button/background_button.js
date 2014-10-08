4a9d275637800093580cb49c3137350e3c165705
Bruno Torres <bruno.torres@me.com>
2014-10-08 16:57:55 -0300

    background button: add player option to toggle displaying the button without disabling the plugin

--- src/plugins/background_button/background_button.js
+++ src/plugins/background_button/background_button.js
@@ -1,9 +1,9 @@
-var UIContainerPlugin = require('../../base/ui_container_plugin');
-var JST = require('../../base/jst');
-var Styler = require('../../base/styler');
+var UICorePlugin = require('../../base/ui_core_plugin')
+var JST = require('../../base/jst')
+var Styler = require('../../base/styler')
 
-class BackgroundButton extends UIContainerPlugin {
-  get template() { return JST.background_button; }
+class BackgroundButton extends UICorePlugin {
+  get template() { return JST.background_button }
   get name() { return 'background_button' }
   get events() {
     return {
@@ -17,84 +17,98 @@ class BackgroundButton extends UIContainerPlugin {
     }
   }
   constructor(core) {
-    super(core);
-    this.core = core;
-    this.listenTo(this.core.mediaControl.container, 'container:state:buffering', this.hide);
-    this.listenTo(this.core.mediaControl.container, 'container:state:bufferfull', this.show);
-    this.listenTo(this.core.mediaControl.container, 'container:settingsupdate', this.settingsUpdate);
-    this.listenTo(this.core.mediaControl.container, 'container:dvr', this.settingsUpdate);
-    this.listenTo(this.core.mediaControl, 'mediacontrol:show', this.show);
-    this.listenTo(this.core.mediaControl, 'mediacontrol:hide', this.hide);
-    this.listenTo(this.core.mediaControl, 'mediacontrol:playing', this.playing);
-    this.listenTo(this.core.mediaControl, 'mediacontrol:notplaying', this.notplaying);
-    this.settingsUpdate();
+    super(core)
+    this.core = core
+    this.settingsUpdate()
+  }
+
+  bindEvents() {
+    this.listenTo(this.core.mediaControl.container, 'container:state:buffering', this.hide)
+    this.listenTo(this.core.mediaControl.container, 'container:state:bufferfull', this.show)
+    this.listenTo(this.core.mediaControl.container, 'container:settingsupdate', this.settingsUpdate)
+    this.listenTo(this.core.mediaControl.container, 'container:dvr', this.settingsUpdate)
+    this.listenTo(this.core.mediaControl, 'mediacontrol:show', this.show)
+    this.listenTo(this.core.mediaControl, 'mediacontrol:hide', this.hide)
+    this.listenTo(this.core.mediaControl, 'mediacontrol:playing', this.playing)
+    this.listenTo(this.core.mediaControl, 'mediacontrol:notplaying', this.notplaying)
   }
 
   settingsUpdate() {
     if(this.shouldRender()) {
-      this.render();
+      this.render()
+      this.bindEvents()
       if (this.core.mediaControl.container.isPlaying()) {
         this.playing()
       } else {
         this.notplaying()
       }
+    } else {
+      this.hide()
+      this.stopListening()
+      this.listenTo(this.core.mediaControl.container, 'container:settingsupdate', this.settingsUpdate)
     }
   }
 
   shouldRender() {
     //this plugin should render only if there is a playpause icon in media control
-    var settings = this.core.mediaControl.settings;
-    return settings.default.indexOf('playpause') >= 0 ||
+    var settings = this.core.mediaControl.settings
+    var useBackgroundButton = this.core.options.useBackgroundButton === undefined || !!this.core.options.useBackgroundButton
+    return useBackgroundButton && (settings.default.indexOf('playpause') >= 0 ||
       settings.left.indexOf('playpause') >= 0 ||
-      settings.right.indexOf('playpause') >= 0;
+      settings.right.indexOf('playpause') >= 0)
 
   }
 
   click() {
-    this.core.mediaControl.togglePlayPause();
+    this.core.mediaControl.togglePlayPause()
   }
 
   show() {
-    this.$el.removeClass('hide');
+    this.$el.removeClass('hide')
   }
 
   hide() {
-    this.$el.addClass('hide');
+    this.$el.addClass('hide')
   }
 
   enable() {
+    this.stopListening()
     super()
-    this.core.mediaControl.$el.find('[data-playpause]').hide();
+    this.core.mediaControl.$el.find('[data-playpause]').hide()
   }
 
   disable() {
     super()
-    this.core.mediaControl.$el.find('[data-playpause]').show();
+    this.core.mediaControl.$el.find('[data-playpause]').show()
+    this.listenTo(this.core.mediaControl.container, 'container:settingsupdate', this.settingsUpdate)
   }
 
   playing() {
     this.$el.find('.playpause-icon[data-background-button]')
       .removeClass('paused')
-      .addClass('playing');
+      .addClass('playing')
   }
 
   notplaying() {
     this.$el.find('.playpause-icon[data-background-button]')
       .removeClass('playing')
-      .addClass('paused');
+      .addClass('paused')
   }
 
   getExternalInterface() {}
 
   render() {
-    var style = Styler.getStyleFor(this.name);
-    this.$el.html(this.template());
-    this.$el.append(style);
+    var style = Styler.getStyleFor(this.name)
+    this.$el.html(this.template())
+    this.$el.append(style)
     if (this.enabled) {
-      this.core.mediaControl.$el.find('[data-playpause]').hide();
+      this.core.mediaControl.$el.find('[data-playpause]').hide()
     }
-    this.core.$el.append(this.$el);
-    return this;
+    this.core.$el.append(this.$el)
+    if (this.core.mediaControl.isVisible()) {
+      this.show()
+    }
+    return this
   }
 }
 
