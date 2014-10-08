e91bac0823cc1c30ca1ec0b77512ddbbf7f8098e
thiagopnts <thiagopnts@gmail.com>
2014-10-08 10:55:42 -0300

    add container plugin base classes

---
+++ src/base/ui_container_plugin.js
@@ -0,0 +1,27 @@
+// Copyright 2014 Globo.com Player authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+var UIObject = require('./ui_object')
+var _ = require('underscore')
+
+class UIContainerPlugin extends UIObject {
+  constructor(options) {
+    super(options)
+    this.bindEvents()
+  }
+
+  enable() {
+    this.bindEvents()
+    this.$el.show()
+  }
+
+  disable() {
+    this.stopListening()
+    this.$el.hide()
+  }
+
+  bindEvents() {}
+}
+
+module.exports = UIPlugin
