2b0a5bb0483334d35c6469b56a0984924c116981
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2014-10-09 14:38:55 -0300

    seek_time: add license header

--- src/components/seek_time/seek_time.js
+++ src/components/seek_time/seek_time.js
@@ -1,3 +1,7 @@
+// Copyright 2014 Globo.com Player authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 var UIObject = require('../../base/ui_object')
 var Styler = require('../../base/styler')
 var JST = require('../../base/jst')
