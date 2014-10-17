b29946896d8151cad0fc63861f74e14dd091e3b6
thiagopnts <thiagopnts@gmail.com>
2014-10-17 13:58:42 -0300

    player info: refactor to be a literal object

---
+++ src/components/player_info.js
@@ -0,0 +1,12 @@
+// Copyright 2014 Globo.com Player authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+var PlayerInfo ={
+  options: {},
+  playbackPlugins: [],
+  currentSize: { width: 0, height: 0 }
+}
+
+module.exports = PlayerInfo
+
