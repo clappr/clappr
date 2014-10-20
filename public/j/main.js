7ac18bd33c9714c5c4481c54905f899a21c751c4
Bruno Torres <bruno.torres@me.com>
2014-10-20 16:50:40 -0200

    add initial test page version

---
+++ public/j/main.js
@@ -0,0 +1,26 @@
+var Parser = function(output) {
+  this.output = output;
+  this.console = $("#console");
+  this.context = document;
+};
+
+Parser.prototype = {
+  parse: function(code) {
+    try {
+      var old = player;
+      eval(code);
+      old.destroy();
+      window.player = player;
+      this.console.empty();
+    } catch(err) {
+      this.console.html(err.message);
+    }
+  }
+};
+$(document).ready(function() {
+  var parser = new Parser($('#output'));
+  $('.run').click(function() {
+    var code = ace.edit('editor').getSession().getValue();
+    parser.parse(code);
+  });
+})
