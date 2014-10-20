c1068b526aed351d7c5b02a4db3c28ff639b1875
Bruno Torres <bruno.torres@me.com>
2014-10-20 17:03:57 -0200

    change upload task to upload the test page as well as dist

--- tasks/upload.js
+++ tasks/upload.js
@@ -6,7 +6,8 @@ var exec = require('child_process').exec;
 
 gulp.task('upload', ['release'], function(b) {
   var tag = args.tag || undefined;
-  upload('latest/');
+  upload('./dist/', 'latest/', true);
+  upload('./public/', '', false);
   if (tag) {
     exec('git tag ' + tag)
     exec('git push origin master --tags')
@@ -15,12 +16,11 @@ gulp.task('upload', ['release'], function(b) {
   return;
 });
 
-var upload = function(prefix) {
+var upload = function(localDir, prefix, deleteRemoved) {
   var awsOptions = JSON.parse(fs.readFileSync('./aws.json'));
   var client = s3.createClient({s3Options: awsOptions});
-  var params = {localDir: "./dist/", deleteRemoved: true, s3Params: {Bucket: "cdn.clappr.io", Prefix: prefix}};
+  var params = {localDir: localDir, deleteRemoved: !!deleteRemoved, s3Params: {Bucket: "cdn.clappr.io", Prefix: prefix}};
   var uploader = client.uploadDir(params);
   uploader.on('error', function(err) { console.error("unable to sync:", err.stack); });
   uploader.on('end', function() { console.log("done uploading for " + prefix); });
 }
-
