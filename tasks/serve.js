b8f6789d40304315fb5b0c9e9c7fc8e77f3abfe0
Bruno Torres <bruno.torres@me.com>
2014-10-23 16:43:59 -0200

    change gulp serve task to avoid having to create a symbolic link for the /latest path

--- tasks/serve.js
+++ tasks/serve.js
@@ -2,10 +2,12 @@ var gulp = require('gulp');
 var util = require('gulp-util');
 var express = require('express');
 
+require('express-alias');
+
 gulp.task('serve', ['watch'], function() {
   express()
     .use(express.static('./public'))
     .use(express.static('./dist'))
+    .alias('/latest/clappr.min.js', '/clappr.js', 301)
     .listen(3000);
 });
-
