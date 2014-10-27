f9de543bfeea00de5d8e2271810efc283d84461e
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2014-10-27 18:58:25 -0200

    log: use underscore instead of jquery to match blacklist messages

--- src/plugins/log/log.js
+++ src/plugins/log/log.js
@@ -3,13 +3,13 @@
 // license that can be found in the LICENSE file.
 
 var Mousetrap = require('mousetrap')
-var $ = require('jquery')
+var _ = require('underscore')
 
 class Log {
   constructor() {
       Mousetrap.bind(['ctrl+shift+d'], () => this.onOff())
       this.BLACKLIST = ['playback:timeupdate', 'playback:progress', 'container:hover', 'container:timeupdate', 'container:progress'];
-    }
+  }
 
   info(klass, message) {this.log(klass, 'info', message)}
   warn(klass, message) {this.log(klass, 'warn', message)}
@@ -22,7 +22,7 @@ class Log {
     }
 
   log(klass, level, message) {
-      if (!window.DEBUG || $.inArray(message, this.BLACKLIST)) return
+      if (!window.DEBUG || _.contains(this.BLACKLIST, message)) return
       var color
       if (level === 'warn') { color = '#FF8000' }
       else if (level === 'info') { color = '#006600' }
