9536ee97e8bc050a7a0f474803c021043999ffc4
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2014-10-31 15:00:47 -0200

    hls: expose capLevelToStage, false by default (close #229)

--- src/playbacks/hls/hls.js
+++ src/playbacks/hls/hls.js
@@ -28,6 +28,7 @@ class HLS extends Playback {
     this.src = options.src
     this.swfPath = options.swfPath || "http://cdn.clappr.io/latest/assets/HLSPlayer.swf"
     this.flushLiveURLCache = options.flushLiveURLCache || true
+    this.capLevelToStage = options.capLevelToStage || false
     this.highDefinition = false
     this.autoPlay = options.autoPlay
     this.defaultSettings = {
@@ -64,9 +65,14 @@ class HLS extends Playback {
     this.isReady = true
     this.trigger('playback:ready', this.name)
     this.currentState = "IDLE"
-    this.el.globoPlayerSetflushLiveURLCache(this.flushLiveURLCache)
-    this.el.globoPlayerSetmaxBufferLength(0)
+    this.setFlashSettings()
     this.autoPlay && this.play()
+  }
+
+  setFlashSettings() {
+    this.el.globoPlayerSetflushLiveURLCache(this.flushLiveURLCache)
+    this.el.globoPlayerCapLeveltoStage(this.capLevelToStage)
+    this.el.globoPlayerSetmaxBufferLength(0)
   }
 
   updateHighDefinition(isHD) {
