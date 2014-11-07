f424e8cfad7cd58b64462ff5de3aeb35fc311fc1
thiagopnts <thiagopnts@gmail.com>
2014-11-07 11:36:02 -0200

    generate source maps in separated file(closes #247)

--- tasks/build.js
+++ tasks/build.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var rename = require('gulp-rename');
 var source = require('vinyl-source-stream');
+var transform = require('vinyl-transform');
+var exorcist = require('exorcist');
 var browserify = require('./common').browserify;
 
 gulp.task('build', ['pre-build'], function(b) {
@@ -8,5 +10,6 @@ gulp.task('build', ['pre-build'], function(b) {
     .bundle()
     .pipe(source('main.js'))
     .pipe(rename('clappr.js'))
+    .pipe(transform(function() { return exorcist('./dist/clappr.map') }))
     .pipe(gulp.dest('./dist'));
 });
