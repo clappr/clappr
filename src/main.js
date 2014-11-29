8d0851561e6df2332b2a125105c84f470abdfdbc
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2014-11-29 19:52:03 -0200

    main: add version attribute to Clappr (close #240)

--- src/main.js
+++ src/main.js
@@ -5,9 +5,11 @@
 var Player = require('./components/player')
 var IframePlayer = require('./components/iframe_player')
 var Mediator = require('mediator')
+var version = require('../package.json').version
 
 global.DEBUG = false
 
 window.Clappr = { Player: Player, Mediator: Mediator, IframePlayer: IframePlayer }
+window.Clappr.version = version
 
 module.exports = window.Clappr
