5e385425ac1dfb80f7c6bca213b69b2337d33ddd
Thiago Pontes <email@thiago.me>
2014-12-03 10:51:57 -0200

    update browserify shim configs

--- shim.js
+++
@@ -1,4 +0,0 @@
-module.exports = process.argv[2].match(/release|start|test/) ? {
-  "jquery": { exports: "node_modules/jquery/dist/jquery.min.js" },
-  "underscore": { exports: "global:_" },
-} : {};
