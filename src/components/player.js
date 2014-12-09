2b75354865b9dd9ec926a2eca23066dcf21abceb
Alvyn McQuitty <alvynmcq@gmail.com>
2014-12-09 23:39:05 +0000

    player: add parentid option

--- src/components/player.js
+++ src/components/player.js
@@ -20,6 +20,12 @@ class Player extends BaseObject {
     options.height || (options.height = 360)
     options.width || (options.width = 640)
     PlayerInfo.currentSize = {width: options.width, height: options.height}
+    if (this.options.parentId) {
+      var el = document.querySelector(this.options.parentId)
+      if (el) {
+        this.attachTo(el)
+      }
+    }
   }
 
   attachTo(element) {
