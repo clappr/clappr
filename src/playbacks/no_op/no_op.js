b632f9965ff9b28f567d39a89b4059ddadb22fb2
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2015-01-18 19:42:15 -0200

    no_op: improve error screen (close #186)

--- src/playbacks/no_op/no_op.js
+++ src/playbacks/no_op/no_op.js
@@ -17,7 +17,39 @@ class NoOp extends Playback {
     var style = Styler.getStyleFor(this.name);
     this.$el.html(this.template())
     this.$el.append(style);
+    this.animate()
     return this
+  }
+
+  noise() {
+    var idata = this.context.createImageData(this.context.canvas.width, this.context.canvas.height)
+    var buffer32 = new Uint32Array(idata.data.buffer)
+    var len = buffer32.length
+    var run = 0
+    var color = 0
+    var m = Math.random() * 6 + 4
+
+    for (var i = 0; i < len;) {
+      if (run < 0) {
+        run = m * Math.random();
+        var p = Math.pow(Math.random(), 0.4);
+        color = (255 * p) << 24;
+      }
+      run -= 1;
+      buffer32[i++] = color;
+    }
+    this.context.putImageData(idata, 0, 0);
+  }
+
+  loop() {
+    this.noise()
+    requestAnimationFrame(() => this.loop())
+  }
+
+  animate() {
+    this.canvas = this.$el.find('canvas[data-no-op-canvas]')[0]
+    this.context = this.canvas.getContext('2d')
+    this.loop()
   }
 }
 
