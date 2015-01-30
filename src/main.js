899bfff7c49204b51f91328dd5763137dc6b4beb
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2015-01-30 19:10:54 -0200

    remove code related to iframe support (close #352)

--- src/main.js
+++ src/main.js
@@ -3,12 +3,11 @@
 // license that can be found in the LICENSE file.
 
 var Player = require('./components/player')
-var IframePlayer = require('./components/iframe_player')
 var Mediator = require('mediator')
 
 window.DEBUG = false
 
-window.Clappr = { Player: Player, Mediator: Mediator, IframePlayer: IframePlayer }
+window.Clappr = { Player: Player, Mediator: Mediator }
 window.Clappr.version = "__VERSION__"
 
 module.exports = window.Clappr
