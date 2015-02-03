03354dd6ad211fa884a5799f555dfbde6b415d77
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2015-02-03 14:03:29 -0200

    tasks: force sync tasks on gulp bump

--- tasks/bump.js
+++ tasks/bump.js
@@ -4,32 +4,32 @@ var git = require('gulp-git');
 var run = require('gulp-run');
 var bump = require('gulp-bump');
 var nodemailer = require('nodemailer');
-var runSequence = require('run-sequence').use(gulp);
+var gulpSync = require('gulp-sync')(gulp);
+var gcallback = require('gulp-callback');
 var version = require("../package.json").version;
 
 gulp.task('increase-version', function(cb) {
-  console.log("1");
-  return gulp.src('./package.json')
+  gulp.src('./package.json')
     .pipe(bump({version: args.tag}))
     .pipe(gulp.dest('./'))
+    .pipe(gcallback(cb))
 });
 
-gulp.task('git-commit',['build', 'release'], function() {
-  console.log("2");
-  return gulp.src('.')
+gulp.task('git-commit', function(cb) {
+  gulp.src('.')
     .pipe(git.add())
     .pipe(git.commit("bump to " + args.tag))
+    .pipe(gcallback(cb))
 });
 
-gulp.task('git-tag', ['git-commit'], function(cb) {
-  console.log("3");
+gulp.task('git-tag', function(cb) {
   git.tag(args.tag, "bump to " + args.tag, function(err) {
     if (err) throw err;
     cb();
   })
 });
 
-gulp.task('git-push', ['git-tag'], function(cb) {
+gulp.task('git-push', function(cb) {
   console.log("4");
   git.push('origin', 'master', {args: " --tags"}, function (err) {
     if (err) throw err;
@@ -38,12 +38,10 @@ gulp.task('git-push', ['git-tag'], function(cb) {
 });
 
 gulp.task('npm-publish', function() {
-  console.log("5");
-//  return run('npm publish --tag=' + args.tag).exec()
+  return run('npm publish --tag=' + args.tag).exec()
 })
 
 gulp.task('sendmail', function(cb) {
-  console.log("6");
   git.exec({args : "log --pretty='format:%h %s (%an)' " + version + "..master"}, function (err, stdout) {
     if (err) throw err;
     var transporter = nodemailer.createTransport();
@@ -59,6 +57,4 @@ gulp.task('sendmail', function(cb) {
   cb();
 })
 
-gulp.task('bump', function() {
-  runSequence('increase-version', 'git-push', 'npm-publish', 'sendmail', 'upload')
-});
+gulp.task('bump', gulpSync.sync(['increase-version', 'build', 'release', 'git-commit', 'git-tag', 'git-push', 'npm-publish', 'sendmail', 'upload']))
