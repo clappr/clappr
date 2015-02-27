dea9e03699624eff20044e4f3e733b12af7ca2c2
Thiago Pontes <email@thiago.me>
2015-02-27 16:20:16 -0300

    hls: add missing dep

--- src/playbacks/hls/hls.js
+++ src/playbacks/hls/hls.js
@@ -11,6 +11,7 @@ var Mediator = require('mediator')
 var Browser = require('browser')
 var Events = require('events')
 var Styler = require('../../base/styler')
+var $ = require('zepto')
 
 var objectIE = '<object type="application/x-shockwave-flash" id="<%= cid %>" class="hls-playback" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" data-hls="" width="100%" height="100%"><param name="movie" value="<%= swfPath %>"> <param name="quality" value="autohigh"> <param name="swliveconnect" value="true"> <param name="allowScriptAccess" value="always"> <param name="bgcolor" value="#001122"> <param name="allowFullScreen" value="false"> <param name="wmode" value="transparent"> <param name="tabindex" value="1"> <param name=FlashVars value="playbackId=<%= playbackId %>" /> </object>'
 
