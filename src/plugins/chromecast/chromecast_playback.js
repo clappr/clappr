b298ead5c2fd0a306e1ec4cf94e5aeade7e494a4
Bruno Torres <bruno.torres@me.com>
2015-03-04 15:42:37 -0300

    chromecast playback: fix formatting

--- src/plugins/chromecast/chromecast_playback.js
+++ src/plugins/chromecast/chromecast_playback.js
@@ -6,62 +6,62 @@ var TICK_INTERVAL = 100
 
 class ChromecastPlayback extends Playback {
 
-    get name() { return 'chromecast_playback' }
-    get template() { return JST.chromecast }
+  get name() { return 'chromecast_playback' }
+  get template() { return JST.chromecast }
 
-    constructor(options) {
-      super(options)
-      this.options = options
-      this.src = options.src
-      this.currentMedia = options.currentMedia
-      this.mediaControl = options.mediaControl
-      this.currentMedia.addUpdateListener(this.onMediaStatusUpdate.bind(this))
-    }
+  constructor(options) {
+    super(options)
+    this.options = options
+    this.src = options.src
+    this.currentMedia = options.currentMedia
+    this.mediaControl = options.mediaControl
+    this.currentMedia.addUpdateListener(this.onMediaStatusUpdate.bind(this))
+  }
 
-    render() {
-      var template = this.template()
-      this.$el = $(template)
-      this.$el.css({'background-image': 'url(' + this.options.poster + ')'})
-    }
+  render() {
+    var template = this.template()
+    this.$el = $(template)
+    this.$el.css({'background-image': 'url(' + this.options.poster + ')'})
+  }
 
-    play() {
-      this.currentMedia.play()
-    }
+  play() {
+    this.currentMedia.play()
+  }
 
-    pause() {
-      this.currentMedia.pause()
-      clearInterval(this.timer)
-    }
+  pause() {
+    this.currentMedia.pause()
+    clearInterval(this.timer)
+  }
 
-    stop() {
-      this.currentMedia.stop()
-      clearInterval(this.timer)
-    }
+  stop() {
+    this.currentMedia.stop()
+    clearInterval(this.timer)
+  }
 
-    isPlaying() {
-      return this.currentMedia.playerState === 'PLAYING'
-    }
+  isPlaying() {
+    return this.currentMedia.playerState === 'PLAYING'
+  }
 
-    onMediaStatusUpdate() {
-      this.mediaControl.changeTogglePlay()
-      if (this.isPlaying() && !this.timer) {
-        this.timer = setInterval(() => this.updateMediaControl(), TICK_INTERVAL)
-      }
+  onMediaStatusUpdate() {
+    this.mediaControl.changeTogglePlay()
+    if (this.isPlaying() && !this.timer) {
+      this.timer = setInterval(() => this.updateMediaControl(), TICK_INTERVAL)
     }
+  }
 
-    updateMediaControl() {
-      var position = this.currentMedia.getEstimatedTime()
-      var duration = this.currentMedia.media.duration
-      this.trigger(Events.PLAYBACK_TIMEUPDATE, position, duration, this.name)
-    }
+  updateMediaControl() {
+    var position = this.currentMedia.getEstimatedTime()
+    var duration = this.currentMedia.media.duration
+    this.trigger(Events.PLAYBACK_TIMEUPDATE, position, duration, this.name)
+  }
 
-    show() {
-      this.$el.show()
-    }
+  show() {
+    this.$el.show()
+  }
 
-    hide() {
-      this.$el.hide()
-    }
+  hide() {
+    this.$el.hide()
+  }
 }
 
 module.exports = ChromecastPlayback
