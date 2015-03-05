9e72771ea72eef72307db74978b0afb5231dde2f
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2015-03-05 16:15:11 -0300

    html5 audio: improve canPlay() method, avoid some crashes

---
+++ test/html5_audio_spec.js
@@ -0,0 +1,11 @@
+var HTML5Audio = require('../src/playbacks/html5_audio');
+
+describe('HTML5Audio playback', () => {
+  it('should check if canPlay resource', () => {
+    expect(HTML5Audio.canPlay("")).to.be.false;
+    expect(HTML5Audio.canPlay("resource_without_dots")).to.be.false;
+    expect(HTML5Audio.canPlay("http://domain.com/Audio.oga")).to.be.true;
+    expect(HTML5Audio.canPlay("http://domain.com/Audio.oga?query_string=here")).to.be.true;
+    expect(HTML5Audio.canPlay("/relative/Audio.mp3")).to.be.true;
+  });
+})
