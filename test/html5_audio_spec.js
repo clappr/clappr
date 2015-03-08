1d8495758cf71b5a79511839f820417adcdfc445
Flávio Ribeiro <flavio.ribeiro@corp.globo.com>
2015-03-08 19:04:05 -0300

    tests: change source containers in order to pass tests on Firefox@Linux (travis)

--- test/html5_audio_spec.js
+++ test/html5_audio_spec.js
@@ -6,6 +6,6 @@ describe('HTML5Audio playback', () => {
     expect(HTML5Audio.canPlay("resource_without_dots")).to.be.false;
     expect(HTML5Audio.canPlay("http://domain.com/Audio.oga")).to.be.true;
     expect(HTML5Audio.canPlay("http://domain.com/Audio.oga?query_string=here")).to.be.true;
-    expect(HTML5Audio.canPlay("/relative/Audio.mp3")).to.be.true;
+    expect(HTML5Audio.canPlay("/relative/Audio.oga")).to.be.true;
   });
 })
--- test/html5_video_spec.js
+++ test/html5_video_spec.js
@@ -4,8 +4,8 @@ describe('HTML5Video playback', () => {
   it('should check if canPlay resource', () => {
     expect(HTML5Video.canPlay("")).to.be.false;
     expect(HTML5Video.canPlay("resource_without_dots")).to.be.false;
-    expect(HTML5Video.canPlay("http://domain.com/video.mp4")).to.be.true;
-    expect(HTML5Video.canPlay("http://domain.com/video.mp4?query_string=here")).to.be.true;
-    expect(HTML5Video.canPlay("/relative/video.mp4")).to.be.true;
+    expect(HTML5Video.canPlay("http://domain.com/video.ogv")).to.be.true;
+    expect(HTML5Video.canPlay("http://domain.com/video.ogv?query_string=here")).to.be.true;
+    expect(HTML5Video.canPlay("/relative/video.ogv")).to.be.true;
   });
 })
