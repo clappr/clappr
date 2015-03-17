9d71e64874c3fdfd94de07b06f672002f57462bd
Thiago Pontes & Leandro Moreira <thiago+leandro.ribeiro.moreira@gmail.com>
2015-03-17 11:32:08 -0300

    adds test for base object

---
+++ test/base/base_object_spec.js
@@ -0,0 +1,17 @@
+var BaseObject = require('base_object');
+
+describe('BaseObject', function() {
+  beforeEach(function(){
+    this.baseObject = new BaseObject();
+    this.baseObject2 = new BaseObject();
+  });
+
+  it('has unique id', function(){
+    expect(this.baseObject.uniqueId).to.be.equal('o1');
+    expect(this.baseObject2.uniqueId).to.be.equal('o2');
+  });
+
+  it('has a container', function(){
+    expect(this.baseObject.container).to.be.equal(undefined);
+  });
+});
