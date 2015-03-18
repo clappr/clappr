fbcf08b6dd6ebc6abc926a561602a3df348355e1
Bruno Torres <bruno.torres@me.com>
2015-03-18 20:57:31 -0300

    google analytics: add tests for domain name

--- test/google_analytics_spec.js
+++ test/google_analytics_spec.js
@@ -26,20 +26,29 @@ describe('GoogleAnalytics', () => {
     });
   });
 
-  describe('constructor with gaAccount and gaTrackerName', () => {
+  describe('constructor with gaAccount, gaDomainName and gaTrackerName', () => {
     beforeEach(() => {
+      window._gat = {};
       window._gaq = [];
-      gaControl = new GoogleAnalytics({gaAccount: "UA-XXXXX-X", gaTrackerName: "MyPlayerInstance"});
+      gaControl = new GoogleAnalytics({gaAccount: "UA-XXXXX-X", gaTrackerName: "MyPlayerInstance", gaDomainName: "some.domain.com"});
     });
     it('trackerName equals to gaTrackerName parameter', () => {
       expect(gaControl.trackerName).to.equal("MyPlayerInstance.");
     });
+    it('sets the account to gaAccount value', () => {
+      expect(window._gaq[0][0]).to.equal("MyPlayerInstance._setAccount");
+      expect(window._gaq[0][1]).to.equal("UA-XXXXX-X");
+    });
+    it('sets the domain name to gaDomainName value', () => {
+      expect(window._gaq[1][0]).to.equal("MyPlayerInstance._setDomainName");
+      expect(window._gaq[1][1]).to.equal("some.domain.com");
+    });
     it('tracks data with gaTrackerName parameter as trackerName', () => {
       gaControl.push(["Video", "Play", "video.mp4"]);
-      expect(window._gaq[0][0]).to.equal("MyPlayerInstance._trackEvent");
-      expect(window._gaq[0][1]).to.equal("Video");
-      expect(window._gaq[0][2]).to.equal("Play");
-      expect(window._gaq[0][3]).to.equal("video.mp4");
+      expect(window._gaq[2][0]).to.equal("MyPlayerInstance._trackEvent");
+      expect(window._gaq[2][1]).to.equal("Video");
+      expect(window._gaq[2][2]).to.equal("Play");
+      expect(window._gaq[2][3]).to.equal("video.mp4");
     });
   });
 });
