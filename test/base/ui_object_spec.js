b90f49e77d54ebd1e45e2ef694d64479783f0ad9
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2015-03-19 10:46:49 -0300

    adds tests for UIObject

---
+++ test/base/ui_object_spec.js
@@ -0,0 +1,17 @@
+var UIObject = require('ui_object');
+
+describe('UIObject', function() {
+  beforeEach(function(){
+    this.uiObject = new UIObject();
+  });
+
+  it('is a div', function(){
+    expect(this.uiObject.tagName).to.be.equal("div");
+  });
+
+  it('has an unique id', function(){
+    var uiObjectA = new UIObject();
+    var uiObjectB = new UIObject();
+    expect(uiObjectA.cid).to.be.not.equal(uiObjectB.cid);
+  });
+});
