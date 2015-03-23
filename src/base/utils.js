dc4de343c115584333c80faefb9ea6cdbbf4df82
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2015-03-23 14:39:56 -0300

    keeps test common code style

--- src/base/utils.js
+++ src/base/utils.js
@@ -3,38 +3,38 @@
 // license that can be found in the LICENSE file.
 
 var assign = require('lodash.assign')
-var Browser = require('../components/browser');
+var Browser = require('../components/browser')
 
 var extend = function(protoProps, staticProps) {
-  var parent = this;
-  var child;
+  var parent = this
+  var child
 
   if (protoProps && protoProps.constructor !== undefined) {
-    child = protoProps.constructor;
+    child = protoProps.constructor
   } else {
-    child = function(){ return parent.apply(this, arguments); };
+    child = function(){ return parent.apply(this, arguments); }
   }
 
-  assign(child, parent, staticProps);
+  assign(child, parent, staticProps)
 
-  var Surrogate = function(){ this.constructor = child; };
-  Surrogate.prototype = parent.prototype;
-  child.prototype = new Surrogate();
+  var Surrogate = function(){ this.constructor = child; }
+  Surrogate.prototype = parent.prototype
+  child.prototype = new Surrogate()
 
-  if (protoProps) assign(child.prototype, protoProps);
+  if (protoProps) assign(child.prototype, protoProps)
 
-  child.__super__ = parent.prototype;
+  child.__super__ = parent.prototype
 
   child.super = function(name) {
-    return parent.prototype[name];
-  };
-
-  child.prototype.getClass = function() {
-    return child;
+    return parent.prototype[name]
   }
 
-  return child;
-};
+  child.prototype.getClass = function() {
+    return child
+  }
+
+  return child
+}
 
 var formatTime = function(time) {
     time = time * 1000
@@ -58,35 +58,35 @@ var Fullscreen = {
       document.webkitIsFullScreen ||
       document.mozFullScreen ||
       !!document.msFullscreenElement
-    );
+    )
   },
   requestFullscreen: function(el) {
     if(el.requestFullscreen) {
-      el.requestFullscreen();
+      el.requestFullscreen()
     } else if(el.webkitRequestFullscreen) {
-      el.webkitRequestFullscreen();
+      el.webkitRequestFullscreen()
     } else if(el.mozRequestFullScreen) {
-      el.mozRequestFullScreen();
+      el.mozRequestFullScreen()
     } else if(el.msRequestFullscreen) {
-      el.msRequestFullscreen();
+      el.msRequestFullscreen()
     } else if (el.querySelector && el.querySelector("video").webkitEnterFullScreen) {
-      el.querySelector("video").webkitEnterFullScreen();
+      el.querySelector("video").webkitEnterFullScreen()
     }
   },
   cancelFullscreen: function() {
     if(document.exitFullscreen) {
-      document.exitFullscreen();
+      document.exitFullscreen()
     } else if(document.webkitCancelFullScreen) {
-      document.webkitCancelFullScreen();
+      document.webkitCancelFullScreen()
     } else if(document.webkitExitFullscreen) {
-      document.webkitExitFullscreen();
+      document.webkitExitFullscreen()
     } else if(document.mozCancelFullScreen) {
-      document.mozCancelFullScreen();
+      document.mozCancelFullScreen()
     } else if(document.msExitFullscreen) {
-      document.msExitFullscreen();
+      document.msExitFullscreen()
     }
   }
-};
+}
 
 class Config {
 
@@ -101,7 +101,7 @@ class Config {
 
   static _defaultValueFor(key) {
     try {
-      return this._defaultConfig()[key]['parse'](this._defaultConfig()[key]['value']);
+      return this._defaultConfig()[key]['parse'](this._defaultConfig()[key]['value'])
     } catch(e) {
       return undefined
     }
@@ -131,18 +131,18 @@ class Config {
 }
 
 var seekStringToSeconds = function(url) {
-  var elements = (url.match(/t=([0-9]*h)?([0-9]*m)?([0-9]*s)?/) || []).splice(1);
+  var elements = (url.match(/t=([0-9]*h)?([0-9]*m)?([0-9]*s)?/) || []).splice(1)
   return (!!elements.length)? elements.map(function (el) {
     if (el) {
-      var value = parseInt(el.slice(0,2)) || 0;
+      var value = parseInt(el.slice(0,2)) || 0
       switch (el[el.length-1]) {
-        case 'h': value = value * 3600; break;
-        case 'm': value = value * 60; break;
-      };
-      return value;
+        case 'h': value = value * 3600; break
+        case 'm': value = value * 60; break
+      }
+      return value
     }
-    return 0;
-  }).reduce(function (a,b) { return a+b; }): 0;
+    return 0
+  }).reduce(function (a,b) { return a+b; }): 0
 }
 
 var idCounter = 0;
@@ -159,4 +159,4 @@ module.exports = {
   Config: Config,
   seekStringToSeconds: seekStringToSeconds,
   uniqueId: uniqueId
-};
+}
