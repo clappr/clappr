47b6e245d7e9dffbeec6f3c722495d5ffca54720
derrod <xlnedder@gmail.com>
2015-07-28 17:45:33 +0200

    Fixed days when hours = 0

--- src/base/utils.js
+++ src/base/utils.js
@@ -30,7 +30,10 @@ var formatTime = function(time) {
     var hours = time % 24
     var days = parseInt(time/24)
     var out = ""
-    if (days && days > 0) out += days + ":"
+    if (days && days > 0) {
+      out += days + ":"
+      if (hours && hours < 1) out += "00:"
+    }
     if (hours && hours > 0) out += ("0" + hours).slice(-2) + ":"
     out += ("0" + minutes).slice(-2) + ":"
     out += ("0" + seconds).slice(-2)
