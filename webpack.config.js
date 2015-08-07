49e477207351a2817b8159d8662721aaec53f324
Thiago Pontes <email+github@thiago.me>
2015-08-07 10:34:44 -0400

    use umd as target library

--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/latest/',
-    filename: 'clappr.js'
+    filename: 'clappr.js',
+    libraryTarget: 'umd',
   },
 };
