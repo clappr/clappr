f40213f43280e1c6a54550f47bdb89033ccb72d5
Bruno Torres <me@brunotorr.es>
2015-08-17 23:00:31 -0300

    flash: add test for bootstrap errors

--- test/playbacks/flash_spec.js
+++ test/playbacks/flash_spec.js
@@ -3,14 +3,29 @@ import Events from '../../src/base/events.js'
 import Mediator from '../../src/components/mediator.js'
 
 describe('Flash playback', function() {
+  before(function() {
+    this.clock = sinon.useFakeTimers()
+  })
+  after(function() {
+    this.clock.restore()
+  })
   beforeEach(function() {
     this.flash = new Flash({})
-    this.flash.el = { getPosition: () => 0, playerPlay: () => {} }
+    this.flash.el = { getDuration: () => NaN, getPosition: () => 0, playerPlay: () => {} }
+  })
+  it('should trigger error when swf is not ready after max retries', function() {
+    this.flash.el.playerPlay = null
+    var spy = sinon.spy()
+    this.flash.on(Events.PLAYBACK_ERROR, spy)
+    Mediator.trigger(`${this.flash.uniqueId}:flashready`)
+    this.clock.tick(2950)
+    expect(spy).not.called
+    this.clock.tick(50)
+    expect(spy).called.once
   })
   it('should trigger ready only when metadata is loaded', function() {
     var spy = sinon.spy()
     this.flash.on(Events.PLAYBACK_READY, spy)
-    this.flash.el.getDuration = () => NaN
     Mediator.trigger(`${this.flash.uniqueId}:flashready`)
     expect(spy).not.called
     expect(this.flash.isReady).to.be.false
