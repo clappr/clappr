f39712aafa1a3d60964528393dfb7bf69eb1e2cb
Bruno Torres <me@brunotorr.es>
2015-08-17 21:10:45 -0300

    flash: update swf build script

--- src/playbacks/flash/build_player.sh
+++ src/playbacks/flash/build_player.sh
@@ -1,1 +1,1 @@
-$(find $HOME/airsdk/ -name mxmlc | head -n1) -default-background-color=0x000000 -default-size=640,360 -language=as3 -output=public/Player.swf -optimize=true -compress=true -use-gpu=true -target-player=11.1 -use-network=false Player.as
+$FLEXPATH/bin/mxmlc -default-background-color=0x000000 -default-size=640,360 -language=as3 -output=public/Player.swf -optimize=true -compress=true -use-gpu=true -target-player=11.1 -use-network=false Player.as
