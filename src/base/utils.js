886e9e5b1f26dba8b9ca588ebb95090b4e957dcc
Bruno Torres <me@brunotorr.es>
2015-08-18 23:36:24 -0300

    utils: add method to extract current script url

--- src/base/utils.js
+++ src/base/utils.js
@@ -150,6 +150,11 @@ export function isNumber(value) {
   return value - parseFloat(value) + 1 >= 0
 }
 
+export function currentScriptUrl() {
+  var scripts = document.getElementsByTagName('script')
+  return scripts[scripts.length - 1].src
+}
+
 export var requestAnimationFrame = (window.requestAnimationFrame ||
                             window.mozRequestAnimationFrame ||
                             window.webkitRequestAnimationFrame ||
@@ -167,6 +172,7 @@ export default {
   formatTime,
   seekStringToSeconds,
   uniqueId,
+  currentScriptUrl,
   isNumber,
   requestAnimationFrame,
   cancelAnimationFrame
