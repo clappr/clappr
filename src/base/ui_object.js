0d78325f0e25705e7420aa60df04c67fda2f5017
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2015-08-22 22:59:04 -0300

    documents ui object

--- src/base/ui_object.js
+++ src/base/ui_object.js
@@ -10,10 +10,58 @@ import BaseObject from './base_object'
 
 var delegateEventSplitter = /^(\S+)\s*(.*)$/
 
+/**
+ * A base class to create ui object.
+ * @class UIObject
+ * @constructor
+ * @extends BaseObject
+ * @module base
+ */
 export default class UIObject extends BaseObject {
 
+  /**
+   * gets the tag name for the ui component
+   * @method tagName
+   * @return {String} tag's name
+   */
   get tagName() { return 'div' }
 
+  /**
+   * it builds an ui component by:
+   *  * creating an id for the component `cid`
+   *  * making sure the element is created `$el`
+   *  * delegating all `events` to the element
+   * @method constructor
+   * @param {Object} options the options object
+   */
+  /**
+   * a unique id prefixed with `'c'`, `c1, c232`
+   *
+   * @property cid
+   * @type String
+   */
+  /**
+   * the dom element itself
+   *
+   * @property el
+   * @type HTMLElement
+   */
+  /**
+   * the dom element wrapped by $zepto
+   *
+   * @property $el
+   * @type HTMLElement
+   */
+  /**
+   * an implicit property which consists of an key/value, being key the
+   * element's event and the value the callback for that element
+   *```javascript
+   * html5VideoPlayback.events = {'pause', 'pause'} //will map pause event from video tag to our function pause
+   *```
+   *
+   * @property events
+   * @type Object
+   */
   constructor(options) {
     super(options)
     this.cid = uniqueId('c');
@@ -21,20 +69,46 @@ export default class UIObject extends BaseObject {
     this.delegateEvents();
   }
 
+  /**
+   * selects within the component.
+   * @method $
+   * @param {String} selector a selector to find within the component.
+   * @return {HTMLElement} an element, if it exists.
+   * @example
+   * ```javascript
+   * fullScreenBarUIComponent.$('.button-full') //will return only `.button-full` within the component
+   * ```
+   */
   $(selector) {
     return this.$el.find(selector)
   }
 
+  /**
+   * render the component, usually attach it to a real existent `element`
+   * @method render
+   * @return {UIObject} itself
+   */
   render() {
     return this
   }
 
+  /**
+   * removes the ui component from DOM
+   * @method remove
+   * @return {UIObject} itself
+   */
   remove() {
     this.$el.remove()
     this.stopListening()
     return this
   }
 
+  /**
+   * set element to `el` and `$el`
+   * @method setElement
+   * @param {HTMLElement} element
+   * @param {Boolean} delegate whether is delegate or not
+   */
   setElement(element, delegate) {
     if (this.$el) this.undelegateEvents()
     this.$el = element instanceof $ ? element : $(element)
@@ -43,6 +117,11 @@ export default class UIObject extends BaseObject {
     return this
   }
 
+  /**
+   * delegats all the original `events` on `element` to its callbacks
+   * @method delegateEvents
+   * @param {Object} events
+   */
   delegateEvents(events) {
     if (!(events || (events = result(this, 'events')))) return this
     this.undelegateEvents()
@@ -64,11 +143,20 @@ export default class UIObject extends BaseObject {
     return this
   }
 
+  /**
+   * undelegats all the `events`
+   * @method undelegateEvents
+   */
   undelegateEvents() {
     this.$el.off('.delegateEvents' + this.cid)
     return this
   }
 
+  /**
+   * ensures the creation of this ui component
+   * @method _ensureElement
+   * @private
+   */
   _ensureElement() {
     if (!this.el) {
       var attrs = assign({}, result(this, 'attributes'))
