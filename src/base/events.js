9307b75d1093308ef955bd85940a4b325900193e
Bruno Torres <me@brunotorr.es>
2015-08-27 21:43:36 -0300

    organize imports for base elements

--- src/base/events.js
+++ src/base/events.js
@@ -2,9 +2,9 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+import Log from 'plugins/log'
 import {uniqueId} from './utils'
 import execOnce from 'lodash.once'
-import Log from '../plugins/log'
 
 var slice = Array.prototype.slice
 
