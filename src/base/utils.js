6892e8db25b78cd3161863960ae2e11e6d4e7988
Bruno Torres <me@brunotorr.es>
2015-08-27 21:43:36 -0300

    organize imports for base elements

--- src/base/utils.js
+++ src/base/utils.js
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-import Browser from '../components/browser'
+import Browser from 'components/browser'
 
 var assign = function(obj, source) {
   if (source) {
