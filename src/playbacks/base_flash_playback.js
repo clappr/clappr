7696147e1faa323923d9b3595b7a4f1f18095353
Bruno Torres <me@brunotorr.es>
2015-09-08 23:42:58 -0300

    base flash: rename playback

--- src/playbacks/base_flash_playback.js
+++ src/playbacks/base_flash_playback.js
@@ -14,7 +14,7 @@ var IE_CLASSID = "clsid:d27cdb6e-ae6d-11cf-96b8-444553540000"
 
 var objectIE = '<object type="application/x-shockwave-flash" id="<%= cid %>" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" data-hls="" width="100%" height="100%"><param name="movie" value="<%= swfPath %>"> <param name="quality" value="autohigh"> <param name="swliveconnect" value="true"> <param name="allowScriptAccess" value="always"> <param name="bgcolor" value="#001122"> <param name="allowFullScreen" value="false"> <param name="wmode" value="transparent"> <param name="tabindex" value="1"> <param name=FlashVars value="playbackId=<%= playbackId %>&callback=<%= callbackName %>" /> </object>'
 
-export default class FlashBasedPlayback extends Playback {
+export default class BaseFlashPlayback extends Playback {
   get tagName() { return 'object' }
   get template() { return template(flashHTML) }
   get attributes() {
