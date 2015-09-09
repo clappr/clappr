b662bbb81e7e5c2af16e93e27e7dffabce2e40ac
Bruno Torres <me@brunotorr.es>
2015-09-09 20:45:55 -0300

    remove gulpfile and included tasks

--- tasks/lint.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp')
-var jshint = require('gulp-jshint')
-
-gulp.task('lint', function() {
-  return gulp.src('./src/*/**.js')
-    .pipe(jshint())
-    .pipe(jshint.reporter('default', {verbose: true}));
-});
--- tasks/pre-build.js
+++
@@ -1,6 +0,0 @@
-var gulp = require('gulp');
-var exec = require('child_process').exec;
-
-gulp.task('pre-build', ['sass', 'copy-html', 'copy-css'], function(done) {
-  exec('node bin/hook.js', done);
-});
