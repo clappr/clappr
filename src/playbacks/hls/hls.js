57edc758f249f99b22d7d25845c9f36d79516b0e
Flávio Ribeiro <flavio.ribeiro@nytimes.com>
2015-09-23 12:19:27 -0300

    hls: support live videos without dvr

--- src/playbacks/hls/hls.js
+++ src/playbacks/hls/hls.js
@@ -5,18 +5,31 @@ export default class HLS extends HTML5VideoPlayback {
   get name() { return 'hls' }
   get attributes() { return {'width': '100%', 'height': '100%'} }
   render() { return this }
-  durationChange() {}
 
   constructor(options) {
     super(options)
     this.hls = new HLSJS()
+    this.playbackType = 'vod'
     this.hls.on(HLSJS.Events.MSE_ATTACHED, () => this.hls.loadSource(this.options.source))
+    this.hls.on(HLSJS.Events.LEVEL_LOADED, (evt, data) => this.updatePlaybackType(evt, data))
     this.hls.attachVideo(this.el)
+  }
+
+  updatePlaybackType(evt, data) {
+    this.playbackType = data.details.live ? 'live' : 'vod'
+  }
+
+  getPlaybackType() {
+    return this.playbackType
+  }
+
+  isSeekEnabled() {
+    return (this.playbackType === 'vod')
   }
 }
 
 HLS.canPlay = function(resource) {
   var resourceParts = resource.split('?')[0].match(/.*\.(.*)$/) || []
   var isHls = !!(resourceParts.length > 1 && resourceParts[1] === 'm3u8')
-  return !!(window.MediaSource && MediaSource.isTypeSupported('video/mp4; codecs="avc1.42E01E,mp4a.40.2"') && isHls)
+  return !!(HLSJS.isSupported() && isHls)
 }
