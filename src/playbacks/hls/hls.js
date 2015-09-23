5751f953a43dec7ef6ffa88739f5432d917f08ef
Flávio Ribeiro <flavio.ribeiro@nytimes.com>
2015-09-23 12:20:58 -0300

    hls: add globo.com header

--- src/playbacks/hls/hls.js
+++ src/playbacks/hls/hls.js
@@ -1,3 +1,7 @@
+// Copyright 2014 Globo.com Player authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 import HTML5VideoPlayback from 'playbacks/html5_video'
 import HLSJS from 'hls.js'
 
