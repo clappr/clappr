ef94644ec6d0348cc16f6692df7e7a93dbec96c5
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2015-11-03 13:45:59 -0200

    mostly refactorings

--- index.js
+++ index.js
@@ -5,32 +5,28 @@ const SEND_STATS_AT = 10 * 1000
 
 export default class ClapprDashShaka extends HTML5Video {
 
-  //where is this enforced? maybe we should take it to base_object or each individual
+  //where is this enforced???
   name() {return 'clappr_dash_shaka'}
 
   constructor(options) {
     super(options)
     this._readyToPlay = false
     this._setup()
-    //var checkIsReady = (fn) => { () => { this._readyToPlay && fn() } }
+
+    var checkIsReady = (fn) => {
+      return (arg) => {
+        if (this._readyToPlay) return fn(arg)
+      }
+    }
+
+    this.enableTextTrack = checkIsReady((enable) => this._player.enableTextTrack(enable))
+    this.textTracks = checkIsReady(() => this._player.getTextTracks())
+    this.selectTextTrack = checkIsReady((id) => this._player.selectTextTrack(id))
+    this.audioTracks = checkIsReady(() => this._player.getAudioTracks())
+    this.selectAudioTrack = checkIsReady((id) => this._player.selectAudioTrack(id))
+    this.videoTracks = checkIsReady(() => this._player.getVideoTracks())
+    this.selectVideoTrack = checkIsReady((id) => this._player.selectVideoTrack(id))
   }
-
-  enableTextTrack(enable=true){
-   this._readyToPlay && this._player.enableTextTrack(enable)
-  }
-
-  //maybe we should use this in Playback or AudioSubVideoPlayback ...
-  audioTracks() {return this._readyToPlay && this._player.getAudioTracks()}
-
-  selectAudioTrack(id) {this._readyToPlay && this._player.selectAudioTrack(id)}
-
-  textTracks() {return this._readyToPlay && this._player.getTextTracks()}
-
-  selectTextTrack(id) {this._readyToPlay && this._player.selectTextTrack(id)}
-
-  videoTracks() {return this._readyToPlay && this._player.getVideoTracks()}
-
-  selectVideoTrack(id) {this._readyToPlay && this._player.selectVideoTrack(id)}
 
   // skipping ready event on video tag in favor of ready on shaka
   ready() {}
@@ -41,10 +37,8 @@ export default class ClapprDashShaka extends HTML5Video {
   // skipping error handling on video tag in favor of error on shaka
   error(event) { Log.error('an error was raised by the video tag', event, this.el.error)}
 
-  // shouldn't we be open to 4k 8k ... something like isDefinitionInUse("HD" || VideoDefinition.V4K)
   isHighDefinitionInUse() { return !!this.highDefinition }
 
-  // I think we should enforce PlaybackType.LIVE and VOD (maybe) I documented it wront as it was for name
   getPlaybackType() {
     return this._player && (this._player.isLive() ? 'live' : 'vod')
   }
@@ -60,21 +54,27 @@ export default class ClapprDashShaka extends HTML5Video {
   version() {return shaka.player.Player.version}
 
   _setup() {
+    this._player = this._createPlayer()
 
-    this._player = new shaka.player.Player(this.el)
-    this._player.addEventListener('bufferingStart', this._bufferingHandler.bind(this))
-    this._player.addEventListener('bufferingEnd', this._bufferingFullHandler.bind(this))
-    this._player.addEventListener('error', this._error.bind(this))
-    this._player.addEventListener('adaptation', this._onAdaptation.bind(this));
+    var playerLoaded = this._player.load(new shaka.player.DashVideoSource(this.options.src))
+    playerLoaded.then(this._loaded.bind(this)).catch(this._setupError.bind(this))
+  }
 
-    var shakaLoaded = this._player.load(new shaka.player.DashVideoSource(this.options.src))
-
-    shakaLoaded.then(this._loaded.bind(this)).catch(this._setupError.bind(this))
+  _createPlayer() {
+    var player = new shaka.player.Player(this.el)
+    player.addEventListener('bufferingStart', this._bufferingHandler.bind(this))
+    player.addEventListener('bufferingEnd', this._bufferingFullHandler.bind(this))
+    player.addEventListener('error', this._error.bind(this))
+    player.addEventListener('adaptation', this._onAdaptation.bind(this));
+    return player
   }
 
   _loaded() {
     this._ready()
+    this._startToSendStats()
+  }
 
+  _startToSendStats() {
     this.sendStats = setInterval(() => {
       console.log(this._player.getStats())
       this.trigger(Events.PLAYBACK_STATS_ADD, this._player.getStats())
@@ -90,7 +90,7 @@ export default class ClapprDashShaka extends HTML5Video {
   _bufferingFullHandler() { console.log('buf full'); this.trigger(Events.PLAYBACK_BUFFERFULL, this.name) }
 
   _error(error) {
-    console.log('error')
+    console.log('error:', error.detail)
     Log.error('an error was raised by shaka player', error.detail)
     this.trigger(Events.PLAYBACK_ERROR, error.detail, this.name)
   }
@@ -101,9 +101,7 @@ export default class ClapprDashShaka extends HTML5Video {
 
       Log.debug('an adaptation happened: ', event)
       this.highDefinition = (event.size.height >= 720)
-      // define expected object from this event
       this.trigger(Events.PLAYBACK_HIGHDEFINITIONUPDATE, this.highDefinition)
-      // define expected object from this event
       this.trigger(Events.PLAYBACK_BITRATE, {bitrate: event.size.height})
   }
 
