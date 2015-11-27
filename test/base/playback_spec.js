322c5baeb6514fc5e8f567c1b5180f5ab2ba0707
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2015-11-26 22:37:17 -0200

    test: add playback

---
+++ test/base/playback_spec.js
@@ -0,0 +1,32 @@
+import Playback from 'base/playback'
+
+describe('Playback', function() {
+  beforeEach(() => {
+    this.basePlayback = new Playback({})
+  })
+
+  it('has default duration', () => {
+    expect(this.basePlayback.getDuration()).to.be.equal(0)
+  })
+
+  it('is not playing', () => {
+    expect(this.basePlayback.isPlaying()).to.be.equal(false)
+  })
+
+  it('has NO_OP as playback type', () => {
+    expect(this.basePlayback.getPlaybackType()).to.be.equal(Playback.NO_OP)
+  })
+
+  it('is not on highdefintion', () => {
+    expect(this.basePlayback.isHighDefinitionInUse()).to.be.equal(false)
+  })
+
+  it('destroys by removing element from DOM', () => {
+    var spy = sinon.spy()
+    this.basePlayback.$el = {remove: spy}
+
+    this.basePlayback.destroy()
+
+    expect(spy).called.once
+  })
+})
