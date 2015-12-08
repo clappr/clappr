1cfbd966f03dc8bdb067e876ffd7f5d39669ab0d
Tom Jenkinson <tjcode@outlook.com>
2015-12-08 18:25:02 +0000

    Added 'autoSeekFromUrl' config option

--- src/playbacks/html5_video/html5_video.js
+++ src/playbacks/html5_video/html5_video.js
@@ -88,7 +88,8 @@ export default class HTML5Video extends Playback {
   loadedMetadata(e) {
     this.durationChange()
     this.trigger(Events.PLAYBACK_LOADEDMETADATA, {duration: e.target.duration, data: e})
-    if (this.getPlaybackType() !== Playback.LIVE) {
+    var autoSeekFromUrl = typeof(this.options.autoSeekFromUrl) === "undefined" || this.options.autoSeekFromUrl
+    if (this.getPlaybackType() !== Playback.LIVE && autoSeekFromUrl) {
       this.checkInitialSeek()
     }
   }
