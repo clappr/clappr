b2edc9d8080ea84f2dfa7b50767ab0ac120d0e62
Michael Cunningham <michael.cunningham@live.ca>
2016-02-01 10:29:06 -0400

    Removed dist and switched to lodash.find

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-import 'babel-polyfill'
 import Player from 'components/player'
 import Utils from 'base/utils'
 import Events from 'base/events'
