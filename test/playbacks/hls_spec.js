e4e2d25edbf1ab41f97b11c7c282113094dde948
Mike Griffith <bikegriffith@gmail.com>
2016-02-03 15:59:52 -0500

    fixed broken test

--- test/playbacks/hls_spec.js
+++ test/playbacks/hls_spec.js
@@ -7,20 +7,24 @@ describe('HLS playback', () => {
     var options = {src: 'http://example.com/foo.m3u8'}
     playback = new HLS(options);
     playback.setupHls();
-    // XXX: Monkeypatching deep into hls.js to support our test. Demeter would be sad :(
-    playback.hls.levelController._levels = [];
+    // NOTE: rather than trying to call playback.setupHls, we'll punch a new one in place
+    playback.hls = {
+      levels: []
+    };
     playback.fillLevels();
   });
 
   it('supports specifying the level', () => {
-
     // AUTO by default (-1)
     expect(playback.currentLevel).to.equal(-1);
 
-    // supports other level specification
+    // Supports other level specification. Should keep track of it
+    // on itself and by proxy on the HLS.js object.
     playback.currentLevel = 0;
     expect(playback.currentLevel).to.equal(0);
+    expect(playback.hls.currentLevel).to.equal(0);
     playback.currentLevel = 1;
     expect(playback.currentLevel).to.equal(1);
+    expect(playback.hls.currentLevel).to.equal(1);
   })
 })
