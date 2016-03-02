600aeb9a42da04b808df91eaaee56114024c8bc8
Mike Griffith <bikegriffith@gmail.com>
2016-03-02 13:48:20 -0500

    remove dist from pull request

--- dist/clappr.js
+++ dist/clappr.js
@@ -21006,59 +21006,50 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	'use strict';
 
-	var __webpack_require__ = arguments[2];
-	var sources = __webpack_require__.m;
+	var bundleFn = arguments[3];
+	var sources = arguments[4];
+	var cache = arguments[5];
 
-	var webpackBootstrapFunc = function webpackBootstrapFunc(modules) {
-	    var installedModules = {};
-	    function __webpack_require__(moduleId) {
-	        if (installedModules[moduleId]) return installedModules[moduleId].exports;
-	        var module = installedModules[moduleId] = {
-	            exports: {},
-	            id: moduleId,
-	            loaded: false
-	        };
-	        modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-	        module.loaded = true;
-	        return module.exports;
-	    }
-	    __webpack_require__.m = modules;
-	    __webpack_require__.c = installedModules;
-	    __webpack_require__.oe = function (err) {
-	        throw err;
-	    };
-	    __webpack_require__.p = "";
-	    var f = __webpack_require__(__webpack_require__.s = entryModule);
-	    return f.default || f; // try to call default if defined to also support babel esmodule exports
-	};
+	var stringify = JSON.stringify;
 
 	module.exports = function (fn) {
-	    var key;
-	    for (var i = 0, l = sources.length; i < l; i++) {
-	        if (!sources[i]) {
-	            continue;
-	        }
-	        var wrapperFuncString = sources[i].toString();
-	        var fnString = fn.toString();
-	        var exp = __webpack_require__(i);
+	    var keys = [];
+	    var wkey;
+	    var cacheKeys = Object.keys(cache);
+
+	    for (var i = 0, l = cacheKeys.length; i < l; i++) {
+	        var key = cacheKeys[i];
+	        var exp = cache[key].exports;
 	        // Using babel as a transpiler to use esmodule, the export will always
 	        // be an object with the default export as a property of it. To ensure
 	        // the existing api and babel esmodule exports are both supported we
 	        // check for both
 	        if (exp === fn || exp.default === fn) {
-	            key = i;
-	            break;
-	        } else if (wrapperFuncString.indexOf(fnString) > -1) {
-	            sources[i] = wrapperFuncString.replace(fnString, '(' + fnString + ')();');
-	            key = i;
+	            wkey = key;
 	            break;
 	        }
 	    }
 
-	    // window = {}; => https://github.com/borisirota/webworkify-webpack/issues/1
-	    var src = 'window = {};\n' + 'var fn = (' + webpackBootstrapFunc.toString().replace('entryModule', key) + ')([' + sources.map(function (func) {
-	        return func.toString();
-	    }).join(',') + ']);\n' + '(typeof fn === "function") && fn(self);'; // not a function when calling a function from the current scope
+	    if (!wkey) {
+	        wkey = Math.floor(Math.pow(16, 8) * Math.random()).toString(16);
+	        var wcache = {};
+	        for (var i = 0, l = cacheKeys.length; i < l; i++) {
+	            var key = cacheKeys[i];
+	            wcache[key] = key;
+	        }
+	        sources[wkey] = [Function(['require', 'module', 'exports'], '(' + fn + ')(self)'), wcache];
+	    }
+	    var skey = Math.floor(Math.pow(16, 8) * Math.random()).toString(16);
+
+	    var scache = {};scache[wkey] = wkey;
+	    sources[skey] = [Function(['require'],
+	    // try to call default if defined to also support babel esmodule
+	    // exports
+	    'var f = require(' + stringify(wkey) + ');' + '(f.default ? f.default : f)(self);'), scache];
+
+	    var src = '(' + bundleFn + ')({' + Object.keys(sources).map(function (key) {
+	        return stringify(key) + ':[' + sources[key][0] + ',' + stringify(sources[key][1]) + ']';
+	    }).join(',') + '},{},[' + stringify(skey) + '])';
 
 	    var URL = window.URL || window.webkitURL || window.mozURL || window.msURL;
 
