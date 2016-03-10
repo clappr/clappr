25cd74e5f72894269a5be5f2ad35285d4035b7fb
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2016-03-10 14:16:41 -0300

    adds missing denominator

--- src/playbacks/hls/hls.js
+++ src/playbacks/hls/hls.js
@@ -153,7 +153,7 @@ export default class HLS extends HTML5VideoPlayback {
 
   fillLevels() {
     this._levels = this.hls.levels.map((level, index) => {
-      return {id: index , label: `${level.bitrate/}Kbps`
+      return {id: index , label: `${level.bitrate/1000}Kbps`
     }})
     this.trigger(Events.PLAYBACK_LEVELS_AVAILABLE, this._levels)
   }
