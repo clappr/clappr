cd8c0f6e15b25da1d6521ab3923214a5443e730b
Bruno Torres <me@brunotorr.es>
2016-03-30 12:52:36 -0300

    utils: fix broken tests

--- test/base/utils_spec.js
+++ test/base/utils_spec.js
@@ -1,5 +1,9 @@
 import * as utils from '../../src/base/utils'
 
+var pushUrl = function(path) {
+  window.history.pushState({"html":document.body.html,"pageTitle":document.head.title},"", path)
+}
+
 describe('Utils', function() {
   it('creates unique id for a given prefix', function() {
     expect(utils.uniqueId("a")).to.not.be.equal(utils.uniqueId("a"))
@@ -16,77 +20,80 @@ describe('Utils', function() {
 
   it('should convert querystring seek regex in seconds', function() {
 
-    var url = 'http://globotv.globo.com/rede-globo/globo-esporte/v/brasil-usa-col/3735973/?t=1h10m30s'
-    expect(utils.seekStringToSeconds(url)).to.equal(4230)
+    pushUrl("/some/path/?t=1h10m30s")
+    expect(utils.seekStringToSeconds()).to.equal(4230)
 
-    url = 'http://globotv.globo.com/rede-globo/globo-esporte/v/brasil-usa-col/3735973/?t=40s'
-    expect(utils.seekStringToSeconds(url)).to.equal(40)
+    pushUrl("/some/path/?t=40s")
+    expect(utils.seekStringToSeconds()).to.equal(40)
 
-    url = 'http://globotv.globo.com/rede-globo/globo-esporte/v/brasil-usa-col/3735973/?t=40s&stretch=false'
-    expect(utils.seekStringToSeconds(url)).to.equal(40)
+    pushUrl("/some/path/?t=40s&stretch=false")
+    expect(utils.seekStringToSeconds()).to.equal(40)
 
-    url = 'http://globotv.globo.com/rede-globo/globo-esporte/v/brasil-usa-col/3735973/?t=30m5s'
-    expect(utils.seekStringToSeconds(url)).to.equal(1805)
+    pushUrl("/some/path/?t=30m5s")
+    expect(utils.seekStringToSeconds()).to.equal(1805)
 
-    url = 'http://globotv.globo.com/rede-globo/globo-esporte/v/brasil-usa-col/3735973/?t=1m'
-    expect(utils.seekStringToSeconds(url)).to.equal(60)
+    pushUrl("/some/path/?t=1m")
+    expect(utils.seekStringToSeconds()).to.equal(60)
 
-    url = 'http://globotv.globo.com/rede-globo/globo-esporte/v/brasil-usa-col/3735973/?t=1h10s'
-    expect(utils.seekStringToSeconds(url)).to.equal(3610)
+    pushUrl("/some/path/?t=1h10s")
+    expect(utils.seekStringToSeconds()).to.equal(3610)
 
-    url = 'http://globo.com/rede-globo/globo-esporte/v/brasil-usa-col/3735973/?autoPlay=true&t=5m5s'
-    expect(utils.seekStringToSeconds(url)).to.equal(305)
+    pushUrl("/some/path/?autoPlay=true&t=5m5s")
+    expect(utils.seekStringToSeconds()).to.equal(305)
 
-    url = 'http://globo.com/rede-globo/globo-esporte/v/brasil-usa-col/3735973/'
-    expect(utils.seekStringToSeconds(url)).to.equal(0)
+    pushUrl("/some/path/")
+    expect(utils.seekStringToSeconds()).to.equal(0)
 
-    url = "http://www.nytimes.com/video/business/media/100000003661916/destroying.html?playlistId=1194811622182#t=6000&cview=true";
-    expect(utils.seekStringToSeconds(url)).to.equal(6000);
+    pushUrl("/some/path/videos-1h/")
+    expect(utils.seekStringToSeconds()).to.equal(0)
 
-    url = "http://www.nytimes.com/video/business/media/100000003661916/destroying.html?playlistId=1194811622182#t=6000s&cview=true";
-    expect(utils.seekStringToSeconds(url)).to.equal(6000);
+    pushUrl("/video/business/media/100000003661916/destroying.html?playlistId=1194811622182#t=6000&cview=true")
+    expect(utils.seekStringToSeconds()).to.equal(6000);
 
-    url = "http://www.nytimes.com/video/business/media/100000003661916/destroying.html?playlistId=1194811622182#t=10m10s&cview=true";
-    expect(utils.seekStringToSeconds(url)).to.equal(610);
+    pushUrl("/video/business/media/100000003661916/destroying.html?playlistId=1194811622182#t=6000s&cview=true")
+    expect(utils.seekStringToSeconds()).to.equal(6000);
 
-    url = "http://www.nytimes.com/video/business/media/100000003661916/destroying.html?playlistId=1194811622182#t=1h20m10s&cview=true";
-    expect(utils.seekStringToSeconds(url)).to.equal(4810);
+    pushUrl("/video/business/media/100000003661916/destroying.html?playlistId=1194811622182#t=10m10s&cview=true")
+    expect(utils.seekStringToSeconds()).to.equal(610);
 
-    url = "http://www.nytimes.com/video/business/media/100000003661916/destroying.html?playlistId=1194811622182#t=6000s";
-    expect(utils.seekStringToSeconds(url)).to.equal(6000);
+    pushUrl("/video/business/media/100000003661916/destroying.html?playlistId=1194811622182#t=1h20m10s&cview=true")
+    expect(utils.seekStringToSeconds()).to.equal(4810);
 
-    url = "http://www.nytimes.com/video/business/media/100000003661916/destroying.html?playlistId=1194811622182#t=6000";
-    expect(utils.seekStringToSeconds(url)).to.equal(6000);
+    pushUrl("/video/business/media/100000003661916/destroying.html?playlistId=1194811622182#t=6000s")
+    expect(utils.seekStringToSeconds()).to.equal(6000);
 
-    url = "http://www.nytimes.com/video/business/media/100000003661916/destroying.html?playlistId=1194811622182&t=5m5s";
-    expect(utils.seekStringToSeconds(url)).to.equal(305);
+    pushUrl("/video/business/media/100000003661916/destroying.html?playlistId=1194811622182#t=6000")
+    expect(utils.seekStringToSeconds()).to.equal(6000);
 
-    url = "http://www.nytimes.com/video/business/media/100000003661916/destroying.html?t=5m5s";
-    expect(utils.seekStringToSeconds(url)).to.equal(305);
+    pushUrl("/video/business/media/100000003661916/destroying.html?playlistId=1194811622182&t=5m5s")
+    expect(utils.seekStringToSeconds()).to.equal(305);
 
-    url = "http://www.nytimes.com/video/business/media/100000003661916/destroying.html?playlistId=1194811622182#t=5m5s";
-    expect(utils.seekStringToSeconds(url)).to.equal(305);
+    pushUrl("/video/business/media/100000003661916/destroying.html?t=5m5s")
+    expect(utils.seekStringToSeconds()).to.equal(305);
 
-    var url = "http://www.nytimes.com/video/business/media/100000003661916/destroying.html?playlistId=1194811622182&t=1h10m30s";
-    expect(utils.seekStringToSeconds(url)).to.equal(4230);
+    pushUrl("/video/business/media/100000003661916/destroying.html?playlistId=1194811622182#t=5m5s")
+    expect(utils.seekStringToSeconds()).to.equal(305);
 
-    url = "http://www.nytimes.com/video/business/media/100000003661916/destroying.html?playlistId=1194811622182&t=1m";
-    expect(utils.seekStringToSeconds(url)).to.equal(60);
+    pushUrl("/video/business/media/100000003661916/destroying.html?playlistId=1194811622182&t=1h10m30s")
+    expect(utils.seekStringToSeconds()).to.equal(4230);
 
-    url = "http://www.nytimes.com/video/business/media/100000003661916/destroying.html?playlistId=1194811622182&t=40s";
-    expect(utils.seekStringToSeconds(url)).to.equal(40);
+    pushUrl("/video/business/media/100000003661916/destroying.html?playlistId=1194811622182&t=1m")
+    expect(utils.seekStringToSeconds()).to.equal(60);
 
-    url = "http://www.nytimes.com/video/business/media/100000003661916/destroying.html?playlistId=1194811622182&t=40s&more=here";
-    expect(utils.seekStringToSeconds(url)).to.equal(40);
+    pushUrl("/video/business/media/100000003661916/destroying.html?playlistId=1194811622182&t=40s")
+    expect(utils.seekStringToSeconds()).to.equal(40);
 
-    url = "http://www.nytimes.com/video/business/media/100000003661916/destroying.html?playlistId=1194811622182&t=30m5s";
-    expect(utils.seekStringToSeconds(url)).to.equal(1805);
+    pushUrl("/video/business/media/100000003661916/destroying.html?playlistId=1194811622182&t=40s&more=here")
+    expect(utils.seekStringToSeconds()).to.equal(40);
 
-    url = "http://www.nytimes.com/video/business/media/100000003661916/destroying.html?playlistId=1194811622182&t=5m5s";
-    expect(utils.seekStringToSeconds(url)).to.equal(305);
+    pushUrl("/video/business/media/100000003661916/destroying.html?playlistId=1194811622182&t=30m5s")
+    expect(utils.seekStringToSeconds()).to.equal(1805);
 
-    url = "http://www.nytimes.com/video/business/media/100000003661916/destroying.html?playlistId=1194811622182";
-    expect(utils.seekStringToSeconds(url)).to.equal(0);
+    pushUrl("/video/business/media/100000003661916/destroying.html?playlistId=1194811622182&t=5m5s")
+    expect(utils.seekStringToSeconds()).to.equal(305);
+
+    pushUrl("/video/business/media/100000003661916/destroying.html?playlistId=1194811622182")
+    expect(utils.seekStringToSeconds()).to.equal(0);
   })
 
   describe('Config', function() {
@@ -108,5 +115,3 @@ describe('Utils', function() {
     })
   })
 })
-
-
