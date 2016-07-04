8c03239252a216289563f4d71471b6bd48366f06
Bruno Torres <me@brunotorr.es>
2016-07-04 17:19:32 -0300

    html5 video: add support for working with either audio and video (closes #1057)

--- test/playbacks/html5_audio_spec.js
+++ test/playbacks/html5_audio_spec.js
@@ -2,10 +2,10 @@ import HTML5Audio from '../../src/playbacks/html5_audio'
 
 describe('HTML5Audio playback', function() {
   it('should check if canPlay resource', function() {
-    expect(HTML5Audio.canPlay("")).to.be.false
-    expect(HTML5Audio.canPlay("resource_without_dots")).to.be.false
-    expect(HTML5Audio.canPlay("http://domain.com/Audio.oga")).to.be.true
-    expect(HTML5Audio.canPlay("http://domain.com/Audio.oga?query_string=here")).to.be.true
-    expect(HTML5Audio.canPlay("/relative/Audio.oga")).to.be.true
+    expect(HTML5Audio.canPlay('')).to.be.false
+    expect(HTML5Audio.canPlay('resource_without_dots')).to.be.false
+    expect(HTML5Audio.canPlay('http://domain.com/Audio.oga')).to.be.true
+    expect(HTML5Audio.canPlay('http://domain.com/Audio.oga?query_string=here')).to.be.true
+    expect(HTML5Audio.canPlay('/relative/Audio.oga')).to.be.true
   })
 })
