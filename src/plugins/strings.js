298f8dfe3a48b9c04ec23eaa2c245341725d893b
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2016-07-04 15:08:37 -0300

    strings: uses a better key for unsupported playback

--- src/plugins/strings.js
+++ src/plugins/strings.js
@@ -34,17 +34,17 @@ export default class Strings extends CorePlugin {
       'en': {
         'live': 'live',
         'back_to_live': 'back to live',
-        'doesnt_support_playback': 'Your browser does not support the playback of this video. Please try using a different browser.'
+        'playback_not_supported': 'Your browser does not support the playback of this video. Please try using a different browser.'
       },
       'pt': {
         'live': 'ao vivo',
         'back_to_live': 'voltar para o ao vivo',
-        'doesnt_support_playback': 'Seu navegador não supporta a reprodução deste video. Por favor, tente usar um navegador diferente.'
+        'playback_not_supported': 'Seu navegador não supporta a reprodução deste video. Por favor, tente usar um navegador diferente.'
       },
       'es': {
         'live': 'vivo',
         'back_to_live': 'volver en vivo',
-        'doesnt_support_playback': 'Su navegador no soporta la reproducción de un video. Por favor, trate de usar un navegador diferente.'
+        'playback_not_supported': 'Su navegador no soporta la reproducción de un video. Por favor, trate de usar un navegador diferente.'
       }
     }, this.core.options.strings || {})
 
