88c87836da24a4af7f378cd9dd736ef20f358bb5
Bruno Torres <me@brunotorr.es>
2016-07-04 17:56:06 -0300

    hls.js: rename options.hlsjsConfig to options.playback

--- test/playbacks/hls_spec.js
+++ test/playbacks/hls_spec.js
@@ -1,30 +1,30 @@
 import HLS from 'playbacks/hls'
 
 xit('HLS playback', () => {
-  var playback;
+  var playback
 
   beforeEach(() => {
     var options = {src: 'http://example.com/foo.m3u8'}
-    playback = new HLS(options);
-    playback.setupHls();
+    playback = new HLS(options)
+    playback.setupHls()
     // NOTE: rather than trying to call playback.setupHls, we'll punch a new one in place
     playback.hls = {
       levels: []
-    };
-    playback.fillLevels();
-  });
+    }
+    playback.fillLevels()
+  })
 
   it('supports specifying the level', () => {
     // AUTO by default (-1)
-    expect(playback.currentLevel).to.equal(-1);
+    expect(playback.currentLevel).to.equal(-1)
 
     // Supports other level specification. Should keep track of it
     // on itself and by proxy on the HLS.js object.
-    playback.currentLevel = 0;
-    expect(playback.currentLevel).to.equal(0);
-    expect(playback.hls.currentLevel).to.equal(0);
-    playback.currentLevel = 1;
-    expect(playback.currentLevel).to.equal(1);
-    expect(playback.hls.currentLevel).to.equal(1);
+    playback.currentLevel = 0
+    expect(playback.currentLevel).to.equal(0)
+    expect(playback.hls.currentLevel).to.equal(0)
+    playback.currentLevel = 1
+    expect(playback.currentLevel).to.equal(1)
+    expect(playback.hls.currentLevel).to.equal(1)
   })
 })
