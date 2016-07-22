13c7ae70c073efff04490c3b3c0b1c596dd44856
Bruno Torres <me@brunotorr.es>
2016-07-22 19:43:37 -0300

    uploader: fix eslint complaints

--- upload.js
+++ upload.js
@@ -1,20 +1,20 @@
-var s3 = require('s3');
-var fs = require('fs');
-var args = require('yargs').argv;
-var exec = require('child_process').exec;
+/* eslint-disable no-var, no-console */
+var s3 = require('s3')
+var fs = require('fs')
+var args = require('yargs').argv
 
-var upload = function(localDir, prefix, deleteRemoved, cb) {
-  var awsOptions = JSON.parse(fs.readFileSync('./aws.json'));
-  var client = s3.createClient({s3Options: awsOptions});
-  var params = {localDir: localDir, deleteRemoved: !!deleteRemoved, s3Params: {Bucket: "cdn.clappr.io", Prefix: prefix}};
-  var uploader = client.uploadDir(params);
-  uploader.on('error', function(err) { console.error("unable to sync:", err.stack); });
-  uploader.on('end', function() { console.log("done uploading for " + prefix); });
+var upload = function(localDir, prefix, deleteRemoved, cb) { // eslint-disable-line no-unused-vars
+  var awsOptions = JSON.parse(fs.readFileSync('./aws.json'))
+  var client = s3.createClient({s3Options: awsOptions})
+  var params = {localDir: localDir, deleteRemoved: !!deleteRemoved, s3Params: {Bucket: 'cdn.clappr.io', Prefix: prefix}}
+  var uploader = client.uploadDir(params)
+  uploader.on('error', function(err) { console.error('unable to sync:', err.stack) })
+  uploader.on('end', function() { console.log('done uploading for ' + prefix) })
 }
 
-var tag = args.tag || undefined;
+var tag = args.tag || undefined
 if (tag) {
-  upload('./dist/', tag + '/', true);
+  upload('./dist/', tag + '/', true)
 } else {
-  console.warn('no tag supplied for uploading');
+  console.warn('no tag supplied for uploading')
 }
