2bf3d0aa7279088cf048062ad938b982eb915352
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2016-11-03 09:10:57 -0200

    adds support for hls.6.x

--- webpack-base-config.js
+++ webpack-base-config.js
@@ -14,7 +14,8 @@ module.exports = {
     loaders: [
       {
         test: /\.js$/,
-        loader: 'babel-loader'
+        loader: 'babel-loader',
+        exclude: [path.resolve(__dirname, './node_modules')]
         // config in .babelrc
       },
       {
