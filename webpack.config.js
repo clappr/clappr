fccdcebc4a92f0a9b4b8b3cd008ea8e1aa0d953a
tchakabam <tchakabam@gmail.com>
2017-03-17 17:39:35 +0100

    create a minified build and a non-minified one

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,57 @@
 var path = require('path');
 var webpack = require('webpack');
 
-module.exports = {
-  entry: path.resolve(__dirname, 'index.js'),
-  externals: {
-    clappr: 'Clappr'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        loader: 'babel',
-        query: {
-            compact: true,
+module.exports = [
+  {
+    entry: path.resolve(__dirname, 'index.js'),
+    externals: {
+      clappr: 'Clappr'
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          loader: 'babel'
         }
-      }
-    ],
+      ],
+    },
+    resolve: {
+      extensions: ['', '.js'],
+    },
+    output: {
+      filename: 'dash-shaka-playback.js',
+      library: 'DashShakaPlayback',
+      libraryTarget: 'umd',
+    },
   },
-  resolve: {
-    extensions: ['', '.js'],
-  },
-  output: {
-    filename: 'dash-shaka-playback.js',
-    library: 'DashShakaPlayback',
-    libraryTarget: 'umd',
-  },
-};
+
+  {
+    entry: path.resolve(__dirname, 'index.js'),
+    externals: {
+      clappr: 'Clappr'
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.js$/,
+          loader: 'babel'
+        }
+      ],
+    },
+    resolve: {
+      extensions: ['', '.js'],
+    },
+    output: {
+      filename: 'dash-shaka-playback.min.js',
+      library: 'DashShakaPlayback',
+      libraryTarget: 'umd',
+    },
+    plugins: [
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        }
+      }),
+    ]
+  }
+];
