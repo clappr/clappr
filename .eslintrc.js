77872740807d764f7bd096997d9690c332b40603
tchakabam <tchakabam@gmail.com>
2017-05-17 14:32:19 +0200

    add eslint and plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,7 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "sourceType": "module"
-    },
-    "rules": {
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "never"
-        ]
-    }
+    "extends": "standard",
+    "plugins": [
+        "standard",
+        "promise"
+    ]
 };
