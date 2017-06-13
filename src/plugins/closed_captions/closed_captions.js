c0b265d894e39bccffbd02bf97d1bd406fe37b99
Karim Slimani <karim@playmedia.fr>
2017-06-13 19:21:38 +0200

    feat(plugin): adds 'closed_captions' internal plugin
    BREAKING CHANGE:
    
    PLAYER_TEXTTRACKLOADED event renamed to PLAYER_SUBTITLE_LOADED
    CONTAINER_LOADEDTEXTTRACK event renamed to CONTAINER_SUBTITLE_LOADED
    Player api event option onTextTrackLoaded renamed to onSubtitleLoaded

---
+++ src/plugins/closed_captions/closed_captions.js
@@ -0,0 +1,129 @@
+import UICorePlugin from '../../base/ui_core_plugin'
+import template from '../../base/template'
+import Styler from '../../base/styler'
+import Events from '../../base/events'
+import ccIcon from '../../icons/09-cc.svg'
+import ccStyle from './public/closed_captions.scss'
+import ccHTML from './public/closed_captions.html'
+
+export default class ClosedCaptions extends UICorePlugin {
+  get name() { return 'closed_captions' }
+
+  get template() { return template(ccHTML) }
+
+  get events() {
+    return {
+      'click [data-cc-button]': 'toggleContextMenu',
+      'click [data-cc-select]': 'onTrackSelect',
+    }
+  }
+
+  get attributes() {
+    return {
+      'class': 'cc-controls',
+      'data-cc-controls': ''
+    }
+  }
+
+  constructor(core) {
+    super(core)
+    const config = core.options.closedCaptionsConfig
+    this._title = config && config.title ? config.title : null
+    this._ariaLabel = config && config.ariaLabel ? config.ariaLabel : 'cc-button'
+    this._labelCb = config && config.labelCallback && typeof config.labelCallback === 'function'
+      ? config.labelCallback
+      : function (track) { return track.name }
+  }
+
+  bindEvents() {
+    this.listenTo(this.core.mediaControl, Events.MEDIACONTROL_CONTAINERCHANGED, this.containerChanged)
+    this.listenTo(this.core.mediaControl, Events.MEDIACONTROL_RENDERED, this.render)
+    this.listenTo(this.core.mediaControl, Events.MEDIACONTROL_HIDE, this.hideContextMenu)
+    this.container = this.core.getCurrentContainer()
+    if (this.container) {
+      this.listenTo(this.container, Events.CONTAINER_SUBTITLE_LOADED, this.onSubtitleLoaded)
+    }
+  }
+
+  containerChanged() {
+    this.ccAvailable(false)
+    this.stopListening()
+    this.bindEvents()
+  }
+
+  onSubtitleLoaded() {
+    const trackId = this.container.getClosedCaptionsTrack()
+    this.renderCcButton()
+    this.setCurrentContextMenuElement(trackId)
+    this.ccAvailable(true)
+  }
+
+  onTrackSelect(event) {
+    const trackId = parseInt(event.target.dataset.ccSelect, 10)
+    if (this.container.setClosedCaptionsTrack(trackId)) {
+      this.setCurrentContextMenuElement(trackId)
+    }
+    this.hideContextMenu()
+    event.stopPropagation()
+    return false
+  }
+
+  ccAvailable(hasCC) {
+    const method = hasCC ? 'addClass' : 'removeClass'
+    this.$el[method]('available')
+  }
+
+  toggleContextMenu() {
+    this.$el.find('ul').toggle()
+  }
+
+  hideContextMenu() {
+    this.$el.find('ul').hide()
+  }
+
+  contextMenuElement(id) {
+    return this.$el.find('ul a'+(!isNaN(id) ? '[data-cc-select="'+id+'"]' : '')).parent()
+  }
+
+  setCurrentContextMenuElement(trackId) {
+    this.contextMenuElement().removeClass('current')
+    this.contextMenuElement(trackId).addClass('current')
+    const method = trackId > -1 ? 'addClass' : 'removeClass'
+    this.$ccButton[method]('enabled')
+  }
+
+  renderCcButton() {
+    let tracks = this.container ? this.container.closedCaptionsTracks : []
+    for (let i = 0; i < tracks.length; i++) {
+      tracks[i].label = this._labelCb(tracks[i])
+    }
+
+    this.$el.html(this.template({
+      ariaLabel: this._ariaLabel,
+      disabledLabel: this.core.i18n.t('disabled'),
+      title: this._title,
+      tracks: tracks
+    }))
+
+    this.$ccButton = this.$el.find('button.cc-button[data-cc-button]')
+    this.$ccButton.append(ccIcon)
+    this.$el.append(this.style)
+  }
+
+  render() {
+    if (!this.style) {
+      this.style = Styler.getStyleFor(ccStyle, { baseUrl: this.core.options.baseUrl })
+    }
+
+    this.renderCcButton()
+
+    const $fullscreen = this.core.mediaControl.$el.find('button[data-fullscreen]')
+    if ($fullscreen[0]) {
+      this.$el.insertAfter($fullscreen)
+    } else {
+      this.core.mediaControl.$el.find('.media-control-right-panel[data-media-control]').prepend(this.$el)
+    }
+
+    return this
+  }
+}
