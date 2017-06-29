bc4f746fdb4ba3f88a9016f20569d3fcf491c58f
Diego Péres <diego.peres@corp.globo.com>
2017-06-29 16:07:46 -0300

    test(flash_spec): reduce the complexity of some tests

--- test/playbacks/flash_spec.js
+++ test/playbacks/flash_spec.js
@@ -6,32 +6,38 @@ describe('Flash playback', function() {
   before(function() {
     this.clock = sinon.useFakeTimers()
   })
+
   after(function() {
     this.clock.restore()
   })
+
   beforeEach(function() {
     this.flash = new Flash({})
     this.flash.el = { getDuration: () => NaN, getPosition: () => 0, playerPlay: () => {} }
+    this.callback = sinon.spy()
   })
+
   it('should trigger error when swf is not ready after max retries', function() {
     this.flash.el.playerPlay = null
-    const spy = sinon.spy()
-    this.flash.on(Events.PLAYBACK_ERROR, spy)
+    this.flash.on(Events.PLAYBACK_ERROR, this.callback)
     Mediator.trigger(`${this.flash.uniqueId}:flashready`)
+
     this.clock.tick(2950)
-    expect(spy).not.called
+    this.callback.should.have.not.been.called
+
     this.clock.tick(50)
-    expect(spy).called.once
+    this.callback.should.have.been.calledOnce
   })
+
   it('should trigger ready only when metadata is loaded', function() {
-    const spy = sinon.spy()
-    this.flash.on(Events.PLAYBACK_READY, spy)
+    this.flash.on(Events.PLAYBACK_READY, this.callback)
     Mediator.trigger(`${this.flash.uniqueId}:flashready`)
-    expect(spy).not.called
+    this.callback.should.have.not.been.called
     expect(this.flash.isReady).to.be.false
+
     this.flash.el.getDuration = () => 100
     Mediator.trigger(`${this.flash.uniqueId}:timeupdate`)
-    expect(spy).called.once
+    this.callback.should.have.been.calledOnce
     expect(this.flash.isReady).to.be.true
   })
 })
