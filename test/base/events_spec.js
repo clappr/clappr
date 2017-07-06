b263be0cb9c0ec5fc96affa802888bc29209476b
Diego.Péres <speres.diego@gmail.com>
2017-07-05 21:57:34 -0300

    test(events): reduce the complexity of some tests

--- test/base/events_spec.js
+++ test/base/events_spec.js
@@ -3,42 +3,31 @@ import Events from '../../src/base/events'
 describe('Events', function(){
   beforeEach(function(){
     this.events = new Events()
+    this.callback = sinon.spy()
   })
 
   it('subcribes to a given event', function(){
-    let counter = 0
-    const eventsCounter = function() {
-      counter += 1
-    }
-
-    this.events.on('clappr.any.event', eventsCounter)
-    expect(counter).to.be.equal(0)
+    this.events.on('clappr.any.event', this.callback)
+    this.callback.should.not.have.been.called
 
     this.events.trigger('clappr.any.event')
-    expect(counter).to.be.equal(1)
+    this.callback.should.have.been.calledOnce
   })
 
   it('subscribes to a list of events separated by spaces', function(){
-    let counter = 0
-    const eventsCounter = function() {
-      counter += 1
-    }
-
-    this.events.on('clappr.any.event clappr.my.event', eventsCounter)
-    expect(counter).to.be.equal(0)
+    this.events.on('clappr.any.event clappr.my.event', this.callback)
+    this.callback.should.not.have.been.called
 
     this.events.trigger('clappr.my.event')
-    expect(counter).to.be.equal(1)
+    this.callback.should.have.been.calledOnce
   })
 
   it('subscribes to a given event using an object', function(){
-    let counter = 0
-
-    this.events.on({'clappr.any.event': function(){ counter += 1  }})
-    expect(counter).to.be.equal(0)
+    this.events.on({'clappr.any.event': this.callback})
+    this.callback.should.not.have.been.called
 
     this.events.trigger('clappr.any.event')
-    expect(counter).to.be.equal(1)
+    this.callback.should.have.been.calledOnce
   })
 
   it('subscribes to a given event with context', function(){
@@ -55,105 +44,68 @@ describe('Events', function(){
   })
 
   it('can unsub an event', function(){
-    let counter = 0
-    const eventsCounter = function() {
-      counter += 1
-    }
-
-    this.events.on('clappr.any.event', eventsCounter)
-    expect(counter).to.be.equal(0)
+    this.events.on('clappr.any.event', this.callback)
+    this.callback.should.not.have.been.called
     this.events.off('clappr.any.event')
 
     this.events.trigger('clappr.any.event')
-    expect(counter).to.be.equal(0)
+    this.callback.should.not.have.been.called
   })
 
   it('can trigger multiple times', function(){
-    let counter = 1
-    const eventsCounter = function() {
-      counter += 1
-    }
-
-    this.events.on('clappr.any.event', eventsCounter)
-    expect(counter).to.be.equal(1)
+    this.events.on('clappr.any.event', this.callback)
+    this.callback.should.not.have.been.called
 
     this.events.trigger('clappr.any.event')
     this.events.trigger('clappr.any.event')
-    expect(counter).to.be.equal(3)
+    this.callback.should.have.been.calledTwice
   })
 
   it('restricts to trigger only once', function(){
-    let counter = 1
-    const eventsCounter = function() {
-      expect(this === eventsCounter).to.be.true
-      counter += 1
-    }
-
-    this.events.once('clappr.any.event', eventsCounter, eventsCounter)
-    expect(counter).to.be.equal(1)
+    this.events.once('clappr.any.event', this.callback)
+    this.callback.should.not.have.been.called
 
     this.events.trigger('clappr.any.event')
     this.events.trigger('clappr.any.event')
-    expect(counter).to.be.equal(2)
+    this.callback.should.have.been.calledOnce
   })
 
   it('permits to listen events in other objects', function(){
-    let counter = 0
-    const eventsCounter = function() {
-      counter += 1
-    }
     const myEvents = new Events()
-    const myCounter = function() {
-      counter += 2
-    }
 
-    this.events.on('clappr.any.event', eventsCounter)
-    myEvents.listenTo(this.events, 'clappr.any.event', myCounter)
+    this.events.on('clappr.any.event', this.callback)
+    myEvents.listenTo(this.events, 'clappr.any.event', this.callback)
 
-    expect(counter).to.be.equal(0)
+    this.callback.should.not.have.been.called
 
     this.events.trigger('clappr.any.event')
-    expect(counter).to.be.equal(3)
+    this.callback.should.have.been.calledTwice
   })
 
   it('permits to listen once events in other objects', function(){
-    let counter = 0
-    const eventsCounter = function() {
-      counter += 1
-    }
     const myEvents = new Events()
-    const myCounter = function() {
-      counter += 1
-    }
 
-    this.events.on('clappr.any.event', eventsCounter)
-    myEvents.listenToOnce(this.events, 'clappr.any.event', myCounter)
+    this.events.on('clappr.any.event', this.callback)
+    myEvents.listenToOnce(this.events, 'clappr.any.event', this.callback)
 
-    expect(counter).to.be.equal(0)
+    this.callback.should.not.have.been.called
 
     this.events.trigger('clappr.any.event')
     this.events.trigger('clappr.any.event')
-    expect(counter).to.be.equal(3)
+    this.callback.should.have.been.calledThrice
   })
 
   it('permits to stop listening events in other objects', function(){
-    let counter = 0
-    const eventsCounter = function() {
-      counter += 1
-    }
     const myEvents = new Events()
-    const myCounter = function() {
-      counter += 2
-    }
 
-    this.events.on('clappr.any.event', eventsCounter)
-    myEvents.listenTo(this.events, 'clappr.any.event', myCounter)
+    this.events.on('clappr.any.event', this.callback)
+    myEvents.listenTo(this.events, 'clappr.any.event', this.callback)
 
-    expect(counter).to.be.equal(0)
+    this.callback.should.not.have.been.called
 
-    myEvents.stopListening(this.events, 'clappr.any.event', myCounter)
+    myEvents.stopListening(this.events, 'clappr.any.event', this.callback)
     this.events.trigger('clappr.any.event')
-    expect(counter).to.be.equal(1)
+    this.callback.should.have.been.calledOnce
   })
 
   it('calls handlers in the order they were registered', function(){
@@ -173,18 +125,14 @@ describe('Events', function(){
   })
 
   it('still calls later handlers if one throws an exception', function(){
-    let secondHandlerCalled = false
     const handlerThatThrows = function() {
       throw new Error('Whoops')
     }
-    const handler = function() {
-      secondHandlerCalled = true
-    }
 
     this.events.on('clappr.any.event', handlerThatThrows)
-    this.events.on('clappr.any.event', handler)
+    this.events.on('clappr.any.event', this.callback)
     this.events.trigger('clappr.any.event')
 
-    expect(secondHandlerCalled).to.be.true
+    this.callback.should.have.been.calledOnce
   })
 })
