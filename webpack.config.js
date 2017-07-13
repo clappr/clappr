75bf689eb689cc0934b71843c6fa5f46d1dbfce3
Karim Slimani <karim@playmedia.fr>
2017-07-13 14:14:17 +0200

    chore(build): Upgrade Webpack to version 3
    Update webpack configuration.
    Upgrade babel to latest release.
    Restore 'build' & 'release' compilation behaviours.
    Adds 'dist' shortcut task.

--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,100 @@
-var path = require('path');
-var webpack = require('webpack');
+const path = require('path')
+const webpack = require('webpack')
 
-var ENTRY_POINT = 'src/clappr-dash-shaka-playback.js'
+var NPM_RUN = process.env.npm_lifecycle_event
 
-module.exports = [
-  {
-    devtool: 'source-maps',
-    entry: path.resolve(__dirname, ENTRY_POINT),
-    externals: {
-      "clappr": 'Clappr',
-      "shaka-player": 'shaka'
+const externals = () => {
+  // By default, only Clappr is defined as external library
+  return {
+    clappr: {
+      amd: 'clappr',
+      commonjs: 'clappr',
+      commonjs2: 'clappr',
+      root: 'Clappr'
+    }
+  }
+}
+
+const webpackConfig = (config) => {
+  return {
+    devServer: {
+      contentBase: [
+        path.resolve(__dirname, 'public'),
+      ],
+      disableHostCheck: true, // https://github.com/webpack/webpack-dev-server/issues/882
+      compress: true,
+      host: '0.0.0.0',
+      port: 8080
     },
+    devtool: 'source-maps',
+    entry: path.resolve(__dirname, 'src/clappr-dash-shaka-playback.js'),
+    externals: config.externals,
     module: {
-      loaders: [
+      rules: [
         {
           test: /\.js$/,
-          loader: 'babel'
-        }
+          loader: 'babel-loader',
+          include: [
+            path.resolve(__dirname, 'src')
+          ],
+          options: {
+            presets: ['es2015'],
+            plugins: ['add-module-exports'],
+          },
+        },
       ],
     },
-    resolve: {
-      extensions: ['', '.js'],
-    },
     output: {
-      filename: 'dash-shaka-playback.js',
+      filename: config.filename,
       library: 'DashShakaPlayback',
       libraryTarget: 'umd',
     },
-  },
+    plugins: config.plugins,
+  }
+}
 
-  {
-    devtool: 'source-maps',
-    entry: path.resolve(__dirname, ENTRY_POINT),
-    externals: {
-      "clappr": 'Clappr',
-      "shaka-player": 'shaka'
-    },
-    module: {
-      loaders: [
-        {
-          test: /\.js$/,
-          loader: 'babel'
-        }
-      ],
-    },
-    resolve: {
-      extensions: ['', '.js'],
-    },
-    output: {
-      filename: 'dash-shaka-playback.min.js',
-      library: 'DashShakaPlayback',
-      libraryTarget: 'umd',
-    },
+var configurations = []
+
+if (NPM_RUN === 'build' || NPM_RUN === 'start') {
+  // Unminified bundle with shaka-player
+  configurations.push(webpackConfig({
+    filename: 'dash-shaka-playback.js',
+    plugins: [],
+    externals: externals()
+  }))
+}
+
+if (NPM_RUN === 'release') {
+  // Minified bundle with shaka-player
+  configurations.push(webpackConfig({
+    filename: 'dash-shaka-playback.min.js',
     plugins: [
       new webpack.optimize.UglifyJsPlugin({
         compress: {
           warnings: false
-        }
+        },
+        sourceMap: true
       }),
-    ]
-  }
-];
+    ],
+    externals: externals()
+  }))
+
+  // Minified bundle without shaka-player
+  var customExt = externals()
+  customExt['shaka-player'] = 'shaka'
+  configurations.push(webpackConfig({
+    filename: 'dash-shaka-playback-external.min.js',
+    plugins: [
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        },
+        sourceMap: true
+      }),
+    ],
+    externals: customExt
+  }))
+}
+
+// https://webpack.js.org/configuration/configuration-types/#exporting-multiple-configurations
+module.exports = configurations
