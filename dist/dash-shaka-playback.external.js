3ee85b44360867e452124873b5b45d1301f2f26a
Leandro Moreira <leandro.ribeiro.moreira@gmail.com>
2017-09-01 16:25:19 -0300

    build new version

--- dist/dash-shaka-playback.external.js
+++ dist/dash-shaka-playback.external.js
@@ -67,7 +67,7 @@ return /******/ (function(modules) { // webpackBootstrap
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "";
+/******/ 	__webpack_require__.p = "dist/";
 /******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(__webpack_require__.s = 0);
