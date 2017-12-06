2bf6f47e8cd7183c57f598bb411ba65a70b4b2d7
tchakabam <tchakabam@gmail.com>
2017-12-06 19:10:23 +0100

    replace Flash/Hls plugins by void modules when respective build time var is set -> build size reduction by 50%

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,18 @@ if (process.env.npm_lifecycle_event === 'release') {
   webpackConfig.plugins.push(new Clean(['dist'], {verbose: false}))
 }
 
+
+const voidModulePath = path.resolve('./src/base/void');
+
+if (process.env.CLAPPR_PLAIN_HTML5_ONLY === 'yes') {
+    console.log('NOTE: Building only with plain HTML5 playback plugins, but will result in smaller build size');
+    webpackConfig.plugins.push(
+        new webpack.NormalModuleReplacementPlugin(/playbacks\/flash/, voidModulePath),
+        new webpack.NormalModuleReplacementPlugin(/playbacks\/base_flash_playback/, voidModulePath),
+        new webpack.NormalModuleReplacementPlugin(/playbacks\/flashls/, voidModulePath),
+        new webpack.NormalModuleReplacementPlugin(/playbacks\/hls/, voidModulePath)
+    );
+}
 webpackConfig.output = {
   path: path.resolve(__dirname, 'dist'),
   filename: 'clappr.js',
