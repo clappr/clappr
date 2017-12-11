2751126db9e3f052d60fad76e4594929ffba4a20
Flávio Ribeiro <flavio.ribeiro@nytimes.com>
2017-12-11 14:57:03 -0500

    add bundle analyzer

---
+++ webpack.config.bundle-analyzer.js
@@ -0,0 +1,10 @@
+const clone = require('clone');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const webpackConfigDev = require('./webpack.config.js');
+
+const config = clone(webpackConfigDev);
+config.plugins = config.plugins.concat([
+  new BundleAnalyzerPlugin()
+]);
+
+module.exports = config;
