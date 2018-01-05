850640e1e3ca5a52bc258e05a5ee3605c3fea065
Bruno Torres <me@brunotorr.es>
2018-01-05 11:53:40 -0200

    style(*): fix lint issues

--- webpack.config.bundle-analyzer.js
+++ webpack.config.bundle-analyzer.js
@@ -1,10 +1,10 @@
-const clone = require('clone');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-const webpackConfigDev = require('./webpack.config.js');
+const clone = require('clone')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const webpackConfigDev = require('./webpack.config.js')
 
-const config = clone(webpackConfigDev);
+const config = clone(webpackConfigDev)
 config.plugins = config.plugins.concat([
   new BundleAnalyzerPlugin()
-]);
+])
 
-module.exports = config;
+module.exports = config
