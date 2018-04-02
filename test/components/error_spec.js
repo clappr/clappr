9482c378794e923ffa2f42cee28ab0bcd9fe4d9b
Jhonatan Gomes <jhonatan.gomes93@gmail.com>
2018-04-02 15:27:48 -0300

    test(error): create tests for player component

---
+++ test/components/error_spec.js
@@ -0,0 +1,33 @@
+import PlayerError from '../../src/components/error'
+import Core from '../../src/components/core'
+import Events from '../../src/base/events'
+
+describe('PlayerError', function() {
+  beforeEach(function() {
+    this.core = new Core({})
+
+    this.errorData = {
+      code: 'test_01',
+      description: 'test error',
+      level: PlayerError.Levels.FATAL,
+      origin: 'test',
+      scope: 'it',
+      raw: {},
+    }
+  })
+
+  it('trigger ERROR with error data when error method is called', function() {
+    sinon.spy(this.core, 'trigger')
+    PlayerError.error(this.errorData)
+
+    assert.ok(this.core.trigger.calledWith(Events.ERROR, this.errorData))
+  })
+
+  it('does not trigger ERROR when core is not setted', function() {
+    sinon.spy(this.core, 'trigger')
+    PlayerError.core = undefined
+    PlayerError.error(this.errorData)
+
+    assert.notOk(this.core.trigger.calledWith(Events.ERROR, this.errorData))
+  })
+})
