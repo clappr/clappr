89b0807fd327684837be0b8ed2c3399b6b4396a6
Joao Paulo Vieira <vieira.joaopaulo@hotmail.com>
2018-04-09 00:40:42 -0300

    style(media_control): yarn lint --fix

--- src/plugins/media_control/media_control.js
+++ src/plugins/media_control/media_control.js
@@ -247,7 +247,7 @@ export default class MediaControl extends UICorePlugin {
     this.$fullscreenToggle.append(icon)
     this.applyButtonStyle(this.$fullscreenToggle)
     this.$el.find('.media-control').length !== 0 && this.$el.removeClass('w320')
-    (size.width <= 320 || this.options.hideVolumeBar) && this.$el.addClass('w320')
+    if (size.width <= 320 || this.options.hideVolumeBar) this.$el.addClass('w320')
   }
 
   togglePlayPause() {
@@ -646,7 +646,7 @@ export default class MediaControl extends UICorePlugin {
 
     this.changeTogglePlay()
 
-    if(this.container) {
+    if (this.container) {
       this.hideId = setTimeout(() => this.hide(), timeout)
       this.disabled && this.hide()
     }
