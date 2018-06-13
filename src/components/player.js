2287ba05337566407be4ed320a2a6eb16ce014e8
Bruno Torres <me@brunotorr.es>
2018-06-13 18:14:02 -0300

    fix(player): prevent event conflicts on configure
    There are some cases where calling configure may remove listeners added through `player.on()`. This change prevents this kind of conflict, as well as avoid removing listeners when there are no new events.

--- src/components/player.js
+++ src/components/player.js
@@ -308,9 +308,10 @@ export default class Player extends BaseObject {
   }
 
   _registerOptionEventListeners(newEvents = {}, events = {}) {
-    Object.keys(events).forEach((userEvent) => {
+    const hasNewEvents = Object.keys(newEvents).length > 0
+    hasNewEvents && Object.keys(events).forEach((userEvent) => {
       const eventType = this.eventsMapping[userEvent]
-      eventType && this.off(eventType)
+      eventType && this.off(eventType, events[userEvent])
     })
 
     Object.keys(newEvents).forEach((userEvent) => {
