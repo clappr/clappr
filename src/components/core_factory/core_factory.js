12bd804961b09b826985410d62e3a2012f1fd44e
Bruno Torres <me@brunotorr.es>
2018-11-12 19:04:01 -0200

    feat(core): expose externalInterface from plugins

--- src/components/core_factory/core_factory.js
+++ src/components/core_factory/core_factory.js
@@ -56,8 +56,10 @@ export default class CoreFactory extends BaseObject {
 
   setupExternalInterface(plugin) {
     const externalFunctions = plugin.getExternalInterface()
-    for (const key in externalFunctions)
+    for (const key in externalFunctions) {
       this.player[key] = externalFunctions[key].bind(plugin)
+      this.core[key] = externalFunctions[key].bind(plugin)
+    }
 
   }
 }
