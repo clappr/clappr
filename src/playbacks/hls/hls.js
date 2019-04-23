23cc4d4e29129c9832ec77ebcc7fa27064ea0a63
Bruno Torres <me@brunotorr.es>
2019-04-23 18:12:46 -0300

    fix(hls): change options backwards-compatible initialization
    This will allow using hlsjsConfig on options root, as with previous versions (0.2.x).
    Fixes #1783

--- src/playbacks/hls/hls.js
+++ src/playbacks/hls/hls.js
@@ -110,7 +110,7 @@ export default class HLS extends HTML5VideoPlayback {
   constructor(...args) {
     super(...args)
     // backwards compatibility (TODO: remove on 0.3.0)
-    this.options.playback || (this.options.playback = this.options)
+    this.options.playback = { ...this.options, ...this.options.playback }
     this._minDvrSize = typeof (this.options.hlsMinimumDvrSize) === 'undefined' ? 60 : this.options.hlsMinimumDvrSize
     // The size of the start time extrapolation window measured as a multiple of segments.
     // Should be 2 or higher, or 0 to disable. Should only need to be increased above 2 if more than one segment is
