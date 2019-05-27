8f219440226c088bfc1205d2d0397d27af6bd1c2
Bruno Torres <me@brunotorr.es>
2019-05-27 02:28:06 -0300

    chore(webpack): update directory-named-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -5,80 +5,13 @@ const webpack = require('webpack')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
-// const webpackConfig = require('./webpack-base-config')
+const webpackConfig = require('./webpack.config.base')
 const voidModulePath = path.resolve('./src/base/void')
 
 const minimize = !!process.env.MINIMIZE
 const analyzeBundle = !!process.env.ANALYZE_BUNDLE
 const forceInlineDebug = !!process.env.CLAPPR_INLINE_DEBUG
 
-const webpackConfig = (config) => {
-  return {
-    devServer: {
-      contentBase: [
-        path.resolve(__dirname, 'public'),
-      ],
-      disableHostCheck: true, // https://github.com/webpack/webpack-dev-server/issues/882
-      compress: true,
-      host: '0.0.0.0',
-      port: 8080
-    },
-    mode: config.mode,
-    devtool: config.devtool || 'source-maps',
-    optimization: config.optimization,
-    entry: path.resolve(__dirname, 'src/main.js'),
-    externals: config.externals,
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          loader: 'babel-loader',
-          exclude: [path.resolve(__dirname, './node_modules')]
-        },
-        {
-          test: /fonts\.css$/,
-          loaders: ['css-loader', 'postcss-loader'],
-          include: path.resolve(__dirname, 'src/components/core/public')
-        },
-        {
-          test: /\.scss$/,
-          loaders: ['style-loader?singleton=true', 'css-loader', 'postcss-loader', 'sass-loader?includePaths[]='
-            + path.resolve(__dirname, './src/base/scss')
-          ],
-          include: path.resolve(__dirname, 'src')
-        },
-        {
-          test: /\.(png|woff|eot|swf|cur|ttf)/,
-          loader: 'url-loader',
-          options: {
-            limit: 1,
-            publicPath: '<%=baseUrl%>/'
-          },
-        },
-        {
-          test: /\.svg/, loader: 'svg-inline-loader'
-        },
-        {
-          test: /\.html/, loader: 'html-loader?minimize=false'
-        }
-      ],
-    },
-    resolve: {
-      alias: {
-        'clappr-zepto': 'clappr-zepto/zepto.js'
-      },
-      modules: ['node_modules']
-    },
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      publicPath: 'dist/',
-      filename: config.filename,
-      library: 'Clappr',
-      libraryTarget: 'umd'
-    },
-    plugins: config.plugins,
-  }
-}
 
 const plainHtml5Plugins = [
   new webpack.NormalModuleReplacementPlugin(/playbacks\/flash/, voidModulePath),
