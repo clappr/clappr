21ceb5db421987f7ff0a6d865c98e76d528fa794
Bruno Torres <me@brunotorr.es>
2019-06-23 19:07:56 -0300

    feat: export all missing plugins

--- src/main.js
+++ src/main.js
@@ -2,28 +2,41 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-import MediaControl from './plugins/media_control'
-import ClickToPausePlugin from './plugins/click_to_pause'
+import ClickToPause from './plugins/click_to_pause'
+import ClosedCaptions from './plugins/closed_captions'
 import DVRControls from './plugins/dvr_controls'
+import EndVideo from './plugins/end_video'
 import ErrorScreen from './plugins/error_screen'
 import Favicon from './plugins/favicon'
+import GoogleAnalytics from './plugins/google_analytics'
+import MediaControl from './plugins/media_control'
 import Poster from './plugins/poster'
-import SpinnerThreeBouncePlugin from './plugins/spinner_three_bounce'
-import WaterMarkPlugin from './plugins/watermark'
+import SeekTime from './plugins/seek_time'
+import Sources from './plugins/sources'
+import SpinnerThreeBounce from './plugins/spinner_three_bounce'
+import Stats from './plugins/stats'
+import WaterMark from './plugins/watermark'
+
 import Vendor from './vendor'
 
 const version = VERSION
 
 export default {
   Plugins: {
-    MediaControl,
-    ClickToPausePlugin,
+    ClickToPause,
+    ClosedCaptions,
     DVRControls,
+    EndVideo,
     ErrorScreen,
     Favicon,
+    GoogleAnalytics,
+    MediaControl,
     Poster,
-    SpinnerThreeBouncePlugin,
-    WaterMarkPlugin,
+    SeekTime,
+    Sources,
+    SpinnerThreeBounce,
+    Stats,
+    WaterMark,
   },
   Vendor,
   version,
