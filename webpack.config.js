36c277059c963ea5b2024be726b5cfa95dd0fb6e
Bruno Torres <me@brunotorr.es>
2019-06-23 01:23:46 -0300

    chore(webpack): fix linter complaint

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 /* eslint-disable no-console */
-const path = require('path')
 const webpack = require('webpack')
 
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
