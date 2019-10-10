d6f1f4e1a4e7b9f750c57b84240ab82368304113
luanraithz <luanraithz@gmail.com>
2019-10-09 22:24:45 -0300

    fix(core): change way to check if a core instance is in fullscreen mode.

--- src/components/core/core.js
+++ src/components/core/core.js
@@ -138,7 +138,7 @@ export default class Core extends UIObject {
   }
 
   updateSize() {
-    Fullscreen.isFullscreen() ? this.setFullscreen() : this.setPlayerSize()
+    this.isFullscreen() ? this.setFullscreen() : this.setPlayerSize()
   }
 
   setFullscreen() {
@@ -236,7 +236,7 @@ export default class Core extends UIObject {
   }
 
   handleFullscreenChange() {
-    this.trigger(Events.CORE_FULLSCREEN, Fullscreen.isFullscreen())
+    this.trigger(Events.CORE_FULLSCREEN, this.isFullscreen())
     this.updateSize()
   }
 
@@ -304,13 +304,17 @@ export default class Core extends UIObject {
     return this.activeContainer && this.activeContainer.getPlaybackType()
   }
 
+  isFullscreen() {
+    return Fullscreen.getFullscreenElement() === (Browser.isiOS ? this.activeContainer.el : this.el)
+  }
+
   toggleFullscreen() {
-    if (!Fullscreen.isFullscreen()) {
-      Fullscreen.requestFullscreen(Browser.isiOS ? this.activeContainer.el : this.el)
-      !Browser.isiOS && this.$el.addClass('fullscreen')
-    } else {
+    if (this.isFullscreen()) {
       Fullscreen.cancelFullscreen()
       !Browser.isiOS && this.$el.removeClass('fullscreen nocursor')
+    } else {
+      Fullscreen.requestFullscreen(Browser.isiOS ? this.activeContainer.el : this.el)
+      !Browser.isiOS && this.$el.addClass('fullscreen')
     }
   }
 
