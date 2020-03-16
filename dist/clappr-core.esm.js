f82149cf1fead675a8d72ea5446b6d4b5991dd3c
Joao Paulo Vieira <vieira.joaopaulo@hotmail.com>
2020-03-16 15:46:37 -0300

    chore(package): bump to 0.4.11

--- dist/clappr-core.esm.js
+++ dist/clappr-core.esm.js
@@ -67,74 +67,6 @@ function _setPrototypeOf(o, p) {
   return _setPrototypeOf(o, p);
 }
 
-function isNativeReflectConstruct() {
-  if (typeof Reflect === "undefined" || !Reflect.construct) return false;
-  if (Reflect.construct.sham) return false;
-  if (typeof Proxy === "function") return true;
-
-  try {
-    Date.prototype.toString.call(Reflect.construct(Date, [], function () {}));
-    return true;
-  } catch (e) {
-    return false;
-  }
-}
-
-function _construct(Parent, args, Class) {
-  if (isNativeReflectConstruct()) {
-    _construct = Reflect.construct;
-  } else {
-    _construct = function _construct(Parent, args, Class) {
-      var a = [null];
-      a.push.apply(a, args);
-      var Constructor = Function.bind.apply(Parent, a);
-      var instance = new Constructor();
-      if (Class) _setPrototypeOf(instance, Class.prototype);
-      return instance;
-    };
-  }
-
-  return _construct.apply(null, arguments);
-}
-
-function _isNativeFunction(fn) {
-  return Function.toString.call(fn).indexOf("[native code]") !== -1;
-}
-
-function _wrapNativeSuper(Class) {
-  var _cache = typeof Map === "function" ? new Map() : undefined;
-
-  _wrapNativeSuper = function _wrapNativeSuper(Class) {
-    if (Class === null || !_isNativeFunction(Class)) return Class;
-
-    if (typeof Class !== "function") {
-      throw new TypeError("Super expression must either be null or a function");
-    }
-
-    if (typeof _cache !== "undefined") {
-      if (_cache.has(Class)) return _cache.get(Class);
-
-      _cache.set(Class, Wrapper);
-    }
-
-    function Wrapper() {
-      return _construct(Class, arguments, _getPrototypeOf(this).constructor);
-    }
-
-    Wrapper.prototype = Object.create(Class.prototype, {
-      constructor: {
-        value: Wrapper,
-        enumerable: false,
-        writable: true,
-        configurable: true
-      }
-    });
-    return _setPrototypeOf(Wrapper, Class);
-  };
-
-  return _wrapNativeSuper(Class);
-}
-
 function _assertThisInitialized(self) {
   if (self === void 0) {
     throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
@@ -241,71 +173,6 @@ function _nonIterableSpread() {
 
 function _nonIterableRest() {
   throw new TypeError("Invalid attempt to destructure non-iterable instance");
-}
-
-function _wrapRegExp(re, groups) {
-  _wrapRegExp = function (re, groups) {
-    return new BabelRegExp(re, undefined, groups);
-  };
-
-  var _RegExp = _wrapNativeSuper(RegExp);
-
-  var _super = RegExp.prototype;
-
-  var _groups = new WeakMap();
-
-  function BabelRegExp(re, flags, groups) {
-    var _this = _RegExp.call(this, re, flags);
-
-    _groups.set(_this, groups || _groups.get(re));
-
-    return _this;
-  }
-
-  _inherits(BabelRegExp, _RegExp);
-
-  BabelRegExp.prototype.exec = function (str) {
-    var result = _super.exec.call(this, str);
-
-    if (result) result.groups = buildGroups(result, this);
-    return result;
-  };
-
-  BabelRegExp.prototype[Symbol.replace] = function (str, substitution) {
-    if (typeof substitution === "string") {
-      var groups = _groups.get(this);
-
-      return _super[Symbol.replace].call(this, str, substitution.replace(/\$<([^>]+)>/g, function (_, name) {
-        return "$" + groups[name];
-      }));
-    } else if (typeof substitution === "function") {
-      var _this = this;
-
-      return _super[Symbol.replace].call(this, str, function () {
-        var args = [];
-        args.push.apply(args, arguments);
-
-        if (typeof args[args.length - 1] !== "object") {
-          args.push(buildGroups(args, _this));
-        }
-
-        return substitution.apply(this, args);
-      });
-    } else {
-      return _super[Symbol.replace].call(this, str, substitution);
-    }
-  };
-
-  function buildGroups(result, re) {
-    var g = _groups.get(re);
-
-    return Object.keys(g).reduce(function (groups, name) {
-      groups[name] = result[g[name]];
-      return groups;
-    }, Object.create(null));
-  }
-
-  return _wrapRegExp.apply(this, arguments);
 }
 
 // Copyright 2014 Globo.com Player authors. All rights reserved.
@@ -6539,25 +6406,19 @@ var CoreFactory = /*#__PURE__*/function (_BaseObject) {
   return CoreFactory;
 }(BaseObject);
 
-var VERSION_REGEX = /*#__PURE__*/_wrapRegExp(/([0-9]+)(\.([0-9]+)?(\.([0-9]+))?)?/, {
-  major: 1,
-  minor: 3,
-  patch: 5
-});
+var VERSION_REGEX = /(\d+)(?:\.(\d+))?(?:\.(\d+))?/;
 
 var Version = /*#__PURE__*/function () {
   _createClass(Version, null, [{
     key: "parse",
     value: function parse() {
       var str = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : '';
+      var matches = str.match(VERSION_REGEX) || [];
 
-      var _ref = str.match(VERSION_REGEX) || {},
-          groups = _ref.groups;
-
-      var _ref2 = groups || {},
-          major = _ref2.major,
-          minor = _ref2.minor,
-          patch = _ref2.patch;
+      var _matches = _slicedToArray(matches, 4),
+          major = _matches[1],
+          minor = _matches[2],
+          patch = _matches[3];
 
       if (typeof major === 'undefined') return null;
       return new Version(major, minor, patch);
@@ -6631,7 +6492,7 @@ var Loader = (function () {
     plugins: {},
     playbacks: []
   };
-  var currentVersion = "0.4.10";
+  var currentVersion = "0.4.11";
   return (/*#__PURE__*/function () {
       _createClass(Loader, null, [{
         key: "checkVersionSupport",
@@ -8029,7 +7890,7 @@ var HTML5Video = /*#__PURE__*/function (_Playback) {
     key: "supportedVersion",
     get: function get() {
       return {
-        min: "0.4.10"
+        min: "0.4.11"
       };
     }
   }, {
@@ -8782,7 +8643,7 @@ var HTML5Audio = /*#__PURE__*/function (_HTML5Video) {
     key: "supportedVersion",
     get: function get() {
       return {
-        min: "0.4.10"
+        min: "0.4.11"
       };
     }
   }, {
@@ -8830,7 +8691,7 @@ var HTMLImg = /*#__PURE__*/function (_Playback) {
     key: "supportedVersion",
     get: function get() {
       return {
-        min: "0.4.10"
+        min: "0.4.11"
       };
     }
   }, {
@@ -8911,7 +8772,7 @@ var NoOp = /*#__PURE__*/function (_Playback) {
     key: "supportedVersion",
     get: function get() {
       return {
-        min: "0.4.10"
+        min: "0.4.11"
       };
     }
   }, {
@@ -9067,7 +8928,7 @@ var Strings = /*#__PURE__*/function (_CorePlugin) {
     key: "supportedVersion",
     get: function get() {
       return {
-        min: "0.4.10"
+        min: "0.4.11"
       };
     }
   }]);
@@ -9216,7 +9077,7 @@ var SourcesPlugin = /*#__PURE__*/function (_CorePlugin) {
     key: "supportedVersion",
     get: function get() {
       return {
-        min: "0.4.10"
+        min: "0.4.11"
       };
     }
   }]);
@@ -9225,7 +9086,7 @@ var SourcesPlugin = /*#__PURE__*/function (_CorePlugin) {
 }(CorePlugin);
 
 // Copyright 2014 Globo.com Player authors. All rights reserved.
-var version = "0.4.10"; // Built-in Plugins/Playbacks
+var version = "0.4.11"; // Built-in Plugins/Playbacks
 
 Loader.registerPlugin(Strings);
 Loader.registerPlugin(SourcesPlugin);
--- dist/clappr-core.js
+++ dist/clappr-core.js
@@ -73,74 +73,6 @@
     return _setPrototypeOf(o, p);
   }
 
-  function isNativeReflectConstruct() {
-    if (typeof Reflect === "undefined" || !Reflect.construct) return false;
-    if (Reflect.construct.sham) return false;
-    if (typeof Proxy === "function") return true;
-
-    try {
-      Date.prototype.toString.call(Reflect.construct(Date, [], function () {}));
-      return true;
-    } catch (e) {
-      return false;
-    }
-  }
-
-  function _construct(Parent, args, Class) {
-    if (isNativeReflectConstruct()) {
-      _construct = Reflect.construct;
-    } else {
-      _construct = function _construct(Parent, args, Class) {
-        var a = [null];
-        a.push.apply(a, args);
-        var Constructor = Function.bind.apply(Parent, a);
-        var instance = new Constructor();
-        if (Class) _setPrototypeOf(instance, Class.prototype);
-        return instance;
-      };
-    }
-
-    return _construct.apply(null, arguments);
-  }
-
-  function _isNativeFunction(fn) {
-    return Function.toString.call(fn).indexOf("[native code]") !== -1;
-  }
-
-  function _wrapNativeSuper(Class) {
-    var _cache = typeof Map === "function" ? new Map() : undefined;
-
-    _wrapNativeSuper = function _wrapNativeSuper(Class) {
-      if (Class === null || !_isNativeFunction(Class)) return Class;
-
-      if (typeof Class !== "function") {
-        throw new TypeError("Super expression must either be null or a function");
-      }
-
-      if (typeof _cache !== "undefined") {
-        if (_cache.has(Class)) return _cache.get(Class);
-
-        _cache.set(Class, Wrapper);
-      }
-
-      function Wrapper() {
-        return _construct(Class, arguments, _getPrototypeOf(this).constructor);
-      }
-
-      Wrapper.prototype = Object.create(Class.prototype, {
-        constructor: {
-          value: Wrapper,
-          enumerable: false,
-          writable: true,
-          configurable: true
-        }
-      });
-      return _setPrototypeOf(Wrapper, Class);
-    };
-
-    return _wrapNativeSuper(Class);
-  }
-
   function _assertThisInitialized(self) {
     if (self === void 0) {
       throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
@@ -247,71 +179,6 @@
 
   function _nonIterableRest() {
     throw new TypeError("Invalid attempt to destructure non-iterable instance");
-  }
-
-  function _wrapRegExp(re, groups) {
-    _wrapRegExp = function (re, groups) {
-      return new BabelRegExp(re, undefined, groups);
-    };
-
-    var _RegExp = _wrapNativeSuper(RegExp);
-
-    var _super = RegExp.prototype;
-
-    var _groups = new WeakMap();
-
-    function BabelRegExp(re, flags, groups) {
-      var _this = _RegExp.call(this, re, flags);
-
-      _groups.set(_this, groups || _groups.get(re));
-
-      return _this;
-    }
-
-    _inherits(BabelRegExp, _RegExp);
-
-    BabelRegExp.prototype.exec = function (str) {
-      var result = _super.exec.call(this, str);
-
-      if (result) result.groups = buildGroups(result, this);
-      return result;
-    };
-
-    BabelRegExp.prototype[Symbol.replace] = function (str, substitution) {
-      if (typeof substitution === "string") {
-        var groups = _groups.get(this);
-
-        return _super[Symbol.replace].call(this, str, substitution.replace(/\$<([^>]+)>/g, function (_, name) {
-          return "$" + groups[name];
-        }));
-      } else if (typeof substitution === "function") {
-        var _this = this;
-
-        return _super[Symbol.replace].call(this, str, function () {
-          var args = [];
-          args.push.apply(args, arguments);
-
-          if (typeof args[args.length - 1] !== "object") {
-            args.push(buildGroups(args, _this));
-          }
-
-          return substitution.apply(this, args);
-        });
-      } else {
-        return _super[Symbol.replace].call(this, str, substitution);
-      }
-    };
-
-    function buildGroups(result, re) {
-      var g = _groups.get(re);
-
-      return Object.keys(g).reduce(function (groups, name) {
-        groups[name] = result[g[name]];
-        return groups;
-      }, Object.create(null));
-    }
-
-    return _wrapRegExp.apply(this, arguments);
   }
 
   // Copyright 2014 Globo.com Player authors. All rights reserved.
@@ -6545,25 +6412,19 @@
     return CoreFactory;
   }(BaseObject);
 
-  var VERSION_REGEX = /*#__PURE__*/_wrapRegExp(/([0-9]+)(\.([0-9]+)?(\.([0-9]+))?)?/, {
-    major: 1,
-    minor: 3,
-    patch: 5
-  });
+  var VERSION_REGEX = /(\d+)(?:\.(\d+))?(?:\.(\d+))?/;
 
   var Version = /*#__PURE__*/function () {
     _createClass(Version, null, [{
       key: "parse",
       value: function parse() {
         var str = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : '';
+        var matches = str.match(VERSION_REGEX) || [];
 
-        var _ref = str.match(VERSION_REGEX) || {},
-            groups = _ref.groups;
-
-        var _ref2 = groups || {},
-            major = _ref2.major,
-            minor = _ref2.minor,
-            patch = _ref2.patch;
+        var _matches = _slicedToArray(matches, 4),
+            major = _matches[1],
+            minor = _matches[2],
+            patch = _matches[3];
 
         if (typeof major === 'undefined') return null;
         return new Version(major, minor, patch);
@@ -6637,7 +6498,7 @@
       plugins: {},
       playbacks: []
     };
-    var currentVersion = "0.4.10";
+    var currentVersion = "0.4.11";
     return (/*#__PURE__*/function () {
         _createClass(Loader, null, [{
           key: "checkVersionSupport",
@@ -8035,7 +7896,7 @@
       key: "supportedVersion",
       get: function get() {
         return {
-          min: "0.4.10"
+          min: "0.4.11"
         };
       }
     }, {
@@ -8788,7 +8649,7 @@
       key: "supportedVersion",
       get: function get() {
         return {
-          min: "0.4.10"
+          min: "0.4.11"
         };
       }
     }, {
@@ -8836,7 +8697,7 @@
       key: "supportedVersion",
       get: function get() {
         return {
-          min: "0.4.10"
+          min: "0.4.11"
         };
       }
     }, {
@@ -8917,7 +8778,7 @@
       key: "supportedVersion",
       get: function get() {
         return {
-          min: "0.4.10"
+          min: "0.4.11"
         };
       }
     }, {
@@ -9073,7 +8934,7 @@
       key: "supportedVersion",
       get: function get() {
         return {
-          min: "0.4.10"
+          min: "0.4.11"
         };
       }
     }]);
@@ -9222,7 +9083,7 @@
       key: "supportedVersion",
       get: function get() {
         return {
-          min: "0.4.10"
+          min: "0.4.11"
         };
       }
     }]);
@@ -9231,7 +9092,7 @@
   }(CorePlugin);
 
   // Copyright 2014 Globo.com Player authors. All rights reserved.
-  var version = "0.4.10"; // Built-in Plugins/Playbacks
+  var version = "0.4.11"; // Built-in Plugins/Playbacks
 
   Loader.registerPlugin(Strings);
   Loader.registerPlugin(SourcesPlugin);
