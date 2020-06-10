7477eb5ffff2d857243bef4265aa24c86f227990
João Paulo <vieira.joaopaulo.91@gmail.com>
2020-06-09 22:29:39 -0300

    refactor: use rollup as bundler tool instead webpack

---
+++ rollup.config.js
@@ -0,0 +1,111 @@
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import replace from '@rollup/plugin-replace'
+import babel from '@rollup/plugin-babel'
+import filesize from 'rollup-plugin-filesize'
+import livereload from 'rollup-plugin-livereload'
+import serve from 'rollup-plugin-serve'
+import size from 'rollup-plugin-sizes'
+import { terser } from 'rollup-plugin-terser'
+import visualize from 'rollup-plugin-visualizer'
+import { version as clapprCoreVersion } from '@clappr/core/package.json'
+import pkg from './package.json'
+
+let rollupConfig
+
+const serveLocal = !!process.env.SERVE
+const reloadEnabled = !!process.env.RELOAD
+const analyzeBundle = !!process.env.ANALYZE_BUNDLE
+const minimize = !!process.env.MINIMIZE
+
+const babelOptionsPlugins = { exclude: 'node_modules/**', babelHelpers: 'bundled' }
+const servePluginOptions = { contentBase: ['dist', 'public'], host: '0.0.0.0', port: '8080' }
+const livereloadPluginOptions = { watch: ['dist', 'public'] }
+const baseReplacePluginOptions = { CLAPPR_CORE_VERSION: JSON.stringify(clapprCoreVersion) }
+const replacePluginOptions = { ...baseReplacePluginOptions, 'process.env.NODE_ENV': JSON.stringify('development') }
+
+let plugins = [
+  replace(baseReplacePluginOptions),
+  resolve(),
+  commonjs(),
+  babel(babelOptionsPlugins),
+  size(),
+  filesize(),
+]
+
+serveLocal && (plugins = [...plugins, replace(replacePluginOptions), serve(servePluginOptions)])
+reloadEnabled && (plugins = [...plugins, livereload(livereloadPluginOptions)])
+analyzeBundle && plugins.push(visualize({ open: true }))
+
+const mainBundle = {
+  external: ['@clappr/core'],
+  input: 'src/hls.js',
+  output: {
+    exports: 'named',
+    name: 'HlsjsPlayback',
+    file: pkg.main,
+    format: 'umd',
+    globals: { '@clappr/core': 'Clappr' },
+  },
+  plugins,
+}
+
+const mainBundleWithoutHLS = {
+  external: ['@clappr/core', 'hls.js'],
+  input: 'src/hls.js',
+  output: {
+    exports: 'named',
+    name: 'HlsjsPlayback',
+    file: 'dist/hlsjs-playback.external.js',
+    format: 'umd',
+    globals: { '@clappr/core': 'Clappr', 'hls.js': 'Hls' },
+  },
+  plugins,
+}
+
+const mainBundleMinified = {
+  input: 'src/hls.js',
+  output: {
+    exports: 'named',
+    name: 'HlsjsPlayback',
+    file: 'dist/hlsjs-playback.min.js',
+    format: 'iife',
+    sourcemap: true,
+    plugins: terser(),
+  },
+  plugins,
+}
+
+const mainBundleWithoutHLSMinified = {
+  external: ['@clappr/core', 'hls.js'],
+  input: 'src/hls.js',
+  output: {
+    exports: 'named',
+    name: 'HlsjsPlayback',
+    file: 'dist/hlsjs-playback.external.min.js',
+    globals: { '@clappr/core': 'Clappr', 'hls.js': 'Hls' },
+    format: 'iife',
+    sourcemap: true,
+    plugins: terser(),
+  },
+  plugins,
+}
+
+const moduleBundle = {
+  external: ['@clappr/core'],
+  input: 'src/hls.js',
+  output: {
+    exports: 'named',
+    name: 'HlsjsPlayback',
+    file: pkg.module,
+    format: 'esm',
+    globals: { '@clappr/core': 'Clappr' },
+  },
+  plugins,
+}
+
+rollupConfig = [mainBundle, mainBundleWithoutHLS, moduleBundle]
+serveLocal && (rollupConfig = [mainBundle])
+minimize && rollupConfig.push(mainBundleMinified, mainBundleWithoutHLSMinified)
+
+export default rollupConfig
