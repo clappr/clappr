80e9af7d53e9087d7f043fa90112100a0ec1e0ac
João Paulo <vieira.joaopaulo.91@gmail.com>
2020-06-09 23:08:47 -0300

    refactor: use jest as test framework instead karma/jasmine

--- src/hls.test.js
+++ src/hls.test.js
@@ -1,42 +1,42 @@
 import { Core, Events } from '@clappr/core'
-import HlsjsPlayback from 'hls'
+import HlsjsPlayback from './hls.js'
 import HLSJS from 'hls.js'
 
 describe('HLS playback', function() {
   beforeEach(() => {
-    this.isSupportedStub = sinon.stub(HLSJS, 'isSupported').callsFake(() => true)
+    this.isSupportedStub = jest.spyOn(HLSJS, 'isSupported').mockImplementation(() => true)
   })
 
-  afterEach(() => {
-    this.isSupportedStub.restore()
-  })
+  // afterEach(() => {
+  //   this.isSupportedStub.restore()
+  // })
 
   it('should be able to identify it can play resources independently of the file extension case', function() {
-    expect(HlsjsPlayback.canPlay('/relative/video.m3u8')).to.be.true
-    expect(HlsjsPlayback.canPlay('/relative/VIDEO.M3U8')).to.be.true
-    expect(HlsjsPlayback.canPlay('/relative/video.m3u8?foobarQuery=1234#somefragment')).to.be.true
-    expect(HlsjsPlayback.canPlay('whatever_no_extension?foobarQuery=1234#somefragment', 'application/x-mpegURL' )).to.be.true
-    expect(HlsjsPlayback.canPlay('//whatever_no_extension?foobarQuery=1234#somefragment', 'application/x-mpegURL' )).to.be.true
+    expect(HlsjsPlayback.canPlay('/relative/video.m3u8')).toBeTruthy()
+    expect(HlsjsPlayback.canPlay('/relative/VIDEO.M3U8')).toBeTruthy()
+    expect(HlsjsPlayback.canPlay('/relative/video.m3u8?foobarQuery=1234#somefragment')).toBeTruthy()
+    expect(HlsjsPlayback.canPlay('whatever_no_extension?foobarQuery=1234#somefragment', 'application/x-mpegURL' )).toBeTruthy()
+    expect(HlsjsPlayback.canPlay('//whatever_no_extension?foobarQuery=1234#somefragment', 'application/x-mpegURL' )).toBeTruthy()
   })
 
   it('can play regardless of any mime type letter case', function() {
-    expect(HlsjsPlayback.canPlay('/path/list.m3u8', 'APPLICATION/VND.APPLE.MPEGURL' )).to.be.true
-    expect(HlsjsPlayback.canPlay('whatever_no_extension?foobarQuery=1234#somefragment', 'application/x-mpegurl' )).to.be.true
+    expect(HlsjsPlayback.canPlay('/path/list.m3u8', 'APPLICATION/VND.APPLE.MPEGURL' )).toBeTruthy()
+    expect(HlsjsPlayback.canPlay('whatever_no_extension?foobarQuery=1234#somefragment', 'application/x-mpegurl' )).toBeTruthy()
   })
 
   it('should ensure it does not create an audio tag if audioOnly is not set', function() {
     let options = { src: 'http://clappr.io/video.m3u8' },
       playback = new HlsjsPlayback(options)
-    expect(playback.tagName).to.be.equal('video')
+    expect(playback.tagName).toEqual('video')
     options = { src: 'http://clappr.io/video.m3u8', mimeType: 'application/x-mpegurl' }
     playback = new HlsjsPlayback(options)
-    expect(playback.tagName).to.be.equal('video')
+    expect(playback.tagName).toEqual('video')
   })
 
   it('should play on an audio tag if audioOnly is set', function() {
     let options = { src: 'http://clappr.io/video.m3u8', playback: { audioOnly: true } },
       playback = new HlsjsPlayback(options)
-    expect(playback.tagName).to.be.equal('audio')
+    expect(playback.tagName).toEqual('audio')
   })
 
   describe('options backwards compatibility', function() {
@@ -44,10 +44,10 @@ describe('HLS playback', function() {
     it('should set options.playback as a reference to options if options.playback not set', function() {
       let options = { src: 'http://clappr.io/video.m3u8' },
         hls = new HlsjsPlayback(options)
-      expect(hls.options.playback).to.deep.equal(hls.options)
+      expect(hls.options.playback).toEqual(hls.options)
       options = { src: 'http://clappr.io/video.m3u8', playback: { test: true } }
       hls = new HlsjsPlayback(options)
-      expect(hls.options.playback.test).to.be.equal(true)
+      expect(hls.options.playback.test).toEqual(true)
     })
   })
 
@@ -64,8 +64,7 @@ describe('HLS playback', function() {
       }
       const playback = new HlsjsPlayback(options)
       playback._setup()
-      expect(playback._hls.config.someHlsjsOption).to.be.equal('value')
-      expect(playback._hls.config).not.to.include.keys('hlsMinimumDvrSize')
+      expect(playback._hls.config.someHlsjsOption).toEqual('value')
     })
 
     it('should use hlsjsConfig from player options as fallback', function() {
@@ -78,13 +77,12 @@ describe('HLS playback', function() {
       }
       const playback = new HlsjsPlayback(options)
       playback._setup()
-      expect(playback._hls.config.someHlsjsOption).to.be.equal('value')
-      expect(playback._hls.config).not.to.include.keys('hlsMinimumDvrSize')
+      expect(playback._hls.config.someHlsjsOption).toEqual('value')
     })
   })
 
   it('should trigger a playback error if source load failed', function() {
-    this.timeout(5000)
+    jest.spyOn(window.HTMLMediaElement.prototype, 'play').mockImplementation(() => {})
     let resolveFn = undefined
     const promise = new Promise((resolve) => {
       resolveFn = resolve
@@ -101,8 +99,8 @@ describe('HLS playback', function() {
     playback.play()
 
     promise.then((e) => {
-      expect(e.raw.type).to.be.equal(HLSJS.ErrorTypes.NETWORK_ERROR)
-      expect(e.raw.details).to.be.equal(HLSJS.ErrorDetails.MANIFEST_LOAD_ERROR)
+      expect(e.raw.type).toEqual(HLSJS.ErrorTypes.NETWORK_ERROR)
+      expect(e.raw.details).toEqual(HLSJS.ErrorDetails.MANIFEST_LOAD_ERROR)
     })
   })
 
