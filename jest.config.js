d8f1bf39652239044191ac22bac9f1fc88ee6d88
Joao Paulo Vieira <vieira.joaopaulo@hotmail.com>
2020-06-25 07:58:04 -0300

    test(jest): create jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  "verbose": true,
+  "resolver": "jest-directory-named-resolver",
+  "transform": {
+    "^.+\\.js$": "babel-jest",
+    "^.+\\.html$": "<rootDir>/src/__mocks__/htmlMock.js"
+  },
+  "moduleNameMapper": {
+    "^@/(.*)$": "<rootDir>/src/$1",
+    "^clappr-zepto$": "clappr-zepto/zepto.js",
+    "\\.(scss)$": "<rootDir>/src/__mocks__/styleMock.js"
+  },
+  "collectCoverageFrom": [
+    "src/*.js",
+    "src/**/*.js",
+    "src/**/**/*.js"
+  ]
+}
