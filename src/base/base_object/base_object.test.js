b9374d026c3017406860f7ed0a5e8785a767950e
João Paulo <vieira.joaopaulo.91@gmail.com>
2020-08-01 16:52:23 -0300

    test(base_object): cover options getter

--- src/base/base_object/base_object.test.js
+++ src/base/base_object/base_object.test.js
@@ -1,6 +1,13 @@
 import BaseObject from './base_object'
 
 describe('BaseObject', () => {
+  test('has a getter that returns the set of options', () => {
+    const options = { clappr: 'is awesome!' }
+    const baseObject = new BaseObject(options)
+
+    expect(baseObject.options).toEqual(options)
+  })
+
   test('has unique id', () => {
     const baseObject = new BaseObject()
     const baseObject2 = new BaseObject()
