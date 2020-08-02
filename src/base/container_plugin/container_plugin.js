4f8cc4204dcfaf820186d08ca78486299c5c50ee
João Paulo <vieira.joaopaulo.91@gmail.com>
2020-08-01 23:08:56 -0300

    style(container_plugin): use alias path to import modules

--- src/base/container_plugin/container_plugin.js
+++ src/base/container_plugin/container_plugin.js
@@ -1,6 +1,6 @@
-import BaseObject from '../base_object'
-import ErrorMixin from '../error_mixin'
-import { extend } from '../../utils'
+import BaseObject from '@/base/base_object'
+import ErrorMixin from '@/base/error_mixin'
+import { extend } from '@/utils'
 
 /**
  * The base class for a container plugin
