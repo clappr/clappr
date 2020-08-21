6d8f9c944d61e3c7e1bb0e0f980092b574b6ca19
João Paulo <vieira.joaopaulo.91@gmail.com>
2020-08-21 16:53:24 -0300

    test(log): cover level getter/setter with tests

--- src/components/log/log.test.js
+++ src/components/log/log.test.js
@@ -13,4 +13,23 @@ describe('Log', () => {
     expect(logger.offLevel).toEqual(Log.LEVEL_ERROR)
   })
 
+  test('have a getter and a setter called level', () => {
+    const logger = new Log()
+
+    expect(Object.getOwnPropertyDescriptor(Object.getPrototypeOf(logger), 'level').get).toBeTruthy()
+    expect(Object.getOwnPropertyDescriptor(Object.getPrototypeOf(logger), 'level').set).toBeTruthy()
+  })
+
+  test('level getter returns current level', () => {
+    const logger = new Log(Log.LEVEL_DEBUG)
+
+    expect(logger.level).toEqual(Log.LEVEL_DEBUG)
+  })
+
+  test('can configure level after the creation', () => {
+    const logger = new Log()
+    logger.level = Log.LEVEL_WARN
+
+    expect(logger.level).toEqual(Log.LEVEL_WARN)
+  })
 })
