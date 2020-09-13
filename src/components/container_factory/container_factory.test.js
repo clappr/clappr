91339ddb3d2988f64aeba64d8fcdfc9109cbc0eb
João Paulo <vieira.joaopaulo.91@gmail.com>
2020-09-13 00:02:46 -0300

    test(container_factory): cover createContainer method with tests

--- src/components/container_factory/container_factory.test.js
+++ src/components/container_factory/container_factory.test.js
@@ -1,6 +1,7 @@
 import ContainerFactory from './container_factory'
 import Loader from '@/components/loader'
 import ContainerPlugin from '@/base/container_plugin'
+import Playback from '@/base/playback'
 
 describe('ContainerFactory', function() {
   beforeEach(() => {
@@ -39,5 +40,54 @@ describe('ContainerFactory', function() {
     const pluginInstance = container.getPlugin('test_plugin')
 
     expect(pluginInstance.container).toEqual(container)
+  })
+
+  describe('createContainer method', () => {
+    test('creates a container for a given source', () => {
+      const source = 'http://some.url/for/video.mp4'
+      const containerFactory =  new ContainerFactory({}, new Loader(), {})
+      const container = containerFactory.createContainer(source)
+
+      expect(container.options.src).toEqual(source)
+    })
+
+    test('creates a playback instance based on existent playback plugins and a given source', () => {
+      class CustomPlayback extends Playback {
+        get name() { return 'custom-playback' }
+        get supportedVersion() { return { min: VERSION } }
+      }
+      CustomPlayback.canPlay = () => true
+      Loader.registerPlayback(CustomPlayback)
+
+      const source = 'http://some.url/for/video.mp4'
+      const containerFactory =  new ContainerFactory({}, new Loader(), {})
+      const container = containerFactory.createContainer(source)
+
+      expect(container.playback.name).toEqual('custom-playback')
+    })
+
+    test('creates a container for a given set of options that includes a source', () => {
+      const options = { source: 'http://some.url/for/video.mp4' }
+      const containerFactory =  new ContainerFactory({}, new Loader(), {})
+      const container = containerFactory.createContainer(options)
+
+      expect(container.options.src).toEqual(options.source)
+    })
+
+    test('creates a container for a given set of options that includes a source and a mimeType', () => {
+      const options = { source: 'http://some.url/for/video', mimeType: 'mp4' }
+      const containerFactory =  new ContainerFactory({}, new Loader(), {})
+      const container = containerFactory.createContainer(options)
+
+      expect(container.options.src).toEqual(options.source)
+    })
+
+    test('uses current domain protocol to set source on the container instance', () => {
+      const source = '//some.url/for/video.mp4'
+      const containerFactory =  new ContainerFactory({}, new Loader(), {})
+      const container = containerFactory.createContainer(source)
+
+      expect(container.options.src).toEqual(`http:${source}`)
+    })
   })
 })
