519d993793715c23cb83ccb5383639d2e6d31196
João Paulo <vieira.joaopaulo.91@gmail.com>
2020-10-22 00:43:27 -0300

    test(core_factory): cover create method with tests

--- src/components/core_factory/core_factory.test.js
+++ src/components/core_factory/core_factory.test.js
@@ -1,4 +1,5 @@
 import CoreFactory from './core_factory'
+import Core from '@/components/core'
 import Player from '@/components/player'
 
 describe('CoreFactory', () => {
@@ -16,5 +17,32 @@ describe('CoreFactory', () => {
 
   test('loader getter returns current player loader reference', () => {
     expect(bareFactory.loader).toEqual(barePlayer.loader)
+  })
+
+  describe('create method', () => {
+    const factory = new CoreFactory(barePlayer)
+    jest.spyOn(factory, 'addCorePlugins')
+    const coreInstance = factory.create()
+
+    test('sets a loader instance into options reference', () => {
+      expect(factory.options.loader).toEqual(barePlayer.loader)
+    })
+
+    test('sets a core instance into internal reference', () => {
+      expect(factory.core instanceof Core).toBeTruthy()
+    })
+
+    test('calls addCorePlugins method', () => {
+      expect(factory.addCorePlugins).toHaveBeenCalledTimes(1)
+    })
+
+    test('trigger container creation for the core instance', () => {
+      expect(factory.core.activeContainer).not.toBeUndefined()
+    })
+
+    test('returns the internal core instance', () => {
+      expect(coreInstance).toEqual(factory.core)
+      expect(coreInstance instanceof Core).toBeTruthy()
+    })
   })
 })
