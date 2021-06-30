294ccdeb2ce43fa98150eae1297a36ebf4c2848b
João Paulo <vieira.joaopaulo.91@gmail.com>
2021-06-29 23:57:45 -0300

    feat(src): creat drm_handler module

---
+++ packages/html5-tvs-playback/src/drm/drm_handler.js
@@ -0,0 +1,128 @@
+import { Log } from '@clappr/core'
+
+const MESSAGE_TYPE = 'application/vnd.ms-playready.initiator+xml'
+const DRM_SYSTEM_ID = 'urn:dvb:casystemid:19219'
+
+export const getLicenseOverrideMessageTemplate = licenseServerURL => {
+  const template = '<?xml version="1.0" encoding="utf-8"?>'
+  + '<PlayReadyInitiator xmlns="http://schemas.microsoft.com/DRM/2007/03/protocols/">'
+  + '<LicenseServerUriOverride>'
+  + `<LA_URL>${licenseServerURL}</LA_URL>`
+  + '</LicenseServerUriOverride>'
+  + '</PlayReadyInitiator>'
+  return template
+}
+
+export const getClearMessageTemplate = () => {
+  const template = '<?xml version="1.0" encoding="utf-8"?>'
+  + '<PlayReadyInitiator xmlns="http://schemas.microsoft.com/DRM/2007/03/protocols/">'
+  + '</PlayReadyInitiator>'
+  return template
+}
+
+export const createDrmAgent = () => {
+  const drmElement = document.createElement('object')
+
+  drmElement.id = 'oipfdrmagent'
+  drmElement.type = 'application/oipfdrmagent'
+  drmElement.style.visibility = 'hidden'
+  drmElement.style.position = 'absolute'
+  drmElement.style.top = '0'
+  drmElement.style.left = '0'
+  drmElement.style.width = '0'
+  drmElement.style.height = '0'
+
+  return drmElement
+}
+
+/* eslint-disable-next-line func-style */
+export function sendLicenseRequest(config = {}, callback = () => {}) {
+  const successCallback = callback.bind(this)
+  let oipfdrmagent = document.getElementById('oipfdrmagent')
+
+  if (!oipfdrmagent) {
+    oipfdrmagent = createDrmAgent()
+    this && this.el
+      ? this.el.appendChild(oipfdrmagent)
+      : document.body.appendChild(oipfdrmagent)
+  }
+
+  !config.licenseServerURL
+    && Log.warn('DRMHandler', 'No one license server is found. The expected result for this behavior is clear the current license.')
+  const xmlLicenceAcquisition = getLicenseOverrideMessageTemplate(config.licenseServerURL)
+
+  const drmRightsErrorHandler = resultCode => {
+    const errorMessage = {
+      0: 'DRM: No license error',
+      1: 'DRM: Invalid license error',
+      2: 'license valid',
+    }
+
+    if (resultCode < 2) {
+      Log.error('DRMHandler', 'Error at onDRMRightsError call', errorMessage[resultCode])
+      return errorMessage[resultCode]
+    }
+  }
+
+  const drmMessageResultHandler = (messageID, resultMessage, resultCode) => {
+    Log.info('DRMHandler', 'onDRMMessageResult messageID', messageID)
+    Log.info('DRMHandler', 'onDRMMessageResult resultMessage', resultMessage)
+    Log.info('DRMHandler', 'onDRMMessageResult resultCode', resultCode)
+
+    const errorMessage = {
+      1: 'DRM: Unspecified error',
+      2: 'DRM: Cannot process request',
+      3: 'DRM: Wrong format',
+      4: 'DRM: User Consent Needed',
+      5: 'DRM: Unknown DRM system',
+    }
+
+    if (resultCode > 0) {
+      Log.error('DRMHandler', 'Error at onDRMMessageResult call', errorMessage[resultCode])
+      return errorMessage[resultCode]
+    }
+
+    successCallback()
+  }
+
+  try {
+    oipfdrmagent.onDRMRightsError = drmRightsErrorHandler
+    oipfdrmagent.onDRMMessageResult = drmMessageResultHandler
+    oipfdrmagent.sendDRMMessage(MESSAGE_TYPE, xmlLicenceAcquisition, DRM_SYSTEM_ID)
+  } catch (error) {
+    Log.error('DRMHandler', 'Error at sendDRMMessage call', error.message)
+  }
+}
+
+/* eslint-disable-next-line func-style */
+export function clearLicenseRequest(callback = () => {}) {
+  const successCallback = callback.bind(this)
+  const oipfdrmagent = document.getElementById('oipfdrmagent')
+
+  if (!oipfdrmagent) {
+    Log.warn('DRMHandler', 'No one DRM license has been configured before. It\'s not necessary to clear any license server.')
+    return
+  }
+
+  const xmlLicenceAcquisition = getClearMessageTemplate()
+
+  try {
+    oipfdrmagent.onDRMRightsError = successCallback
+
+    oipfdrmagent.onDRMMessageResult = () => {
+      oipfdrmagent.remove()
+      successCallback()
+    }
+
+    oipfdrmagent.sendDRMMessage(MESSAGE_TYPE, xmlLicenceAcquisition, DRM_SYSTEM_ID)
+  } catch (error) {
+    Log.error('DRMHandler', 'Error at sendDRMMessage call', error.message)
+  }
+}
+
+const DRMHandler = {
+  sendLicenseRequest,
+  clearLicenseRequest,
+}
+
+export default DRMHandler
---
+++ packages/html5-tvs-playback/src/drm/drm_handler.test.js
@@ -0,0 +1,244 @@
+import mockConsole from 'jest-mock-console'
+
+import DRMHandler, {
+  getLicenseOverrideMessageTemplate,
+  getClearMessageTemplate,
+  createDrmAgent,
+  sendLicenseRequest,
+  clearLicenseRequest,
+} from './drm_handler'
+
+const LOG_WARN_STYLE = 'color: #ff8000;font-weight: bold; font-size: 13px;'
+const LOG_WARN_HEAD_MESSAGE = '%c[warn][DRMHandler]'
+
+describe('DRMHandler', function() {
+  beforeEach(() => {
+    this.restoreConsole = mockConsole()
+    jest.clearAllMocks()
+  })
+  afterEach(() => this.restoreConsole())
+
+  test('only exports methods to handle with license request', () => {
+    expect(DRMHandler.sendLicenseRequest).toBeDefined()
+    expect(DRMHandler.clearLicenseRequest).toBeDefined()
+    expect(DRMHandler.getLicenseOverrideMessageTemplate).toBeUndefined()
+    expect(DRMHandler.getClearMessageTemplate).toBeUndefined()
+    expect(DRMHandler.createDrmAgent).toBeUndefined()
+  })
+
+  describe('getLicenseOverrideMessageTemplate method', () => {
+    test('returns XML template with received license server URL', () => {
+      const fakeLicenseServerURL = 'http://fake-domain/fake_folder/playready?deviceId=FAKEID'
+      const result = '<?xml version="1.0" encoding="utf-8"?>'
+      + '<PlayReadyInitiator xmlns="http://schemas.microsoft.com/DRM/2007/03/protocols/">'
+      + '<LicenseServerUriOverride>'
+      + `<LA_URL>${fakeLicenseServerURL}</LA_URL>`
+      + '</LicenseServerUriOverride>'
+      + '</PlayReadyInitiator>'
+      const response = getLicenseOverrideMessageTemplate(fakeLicenseServerURL)
+
+      expect(response).toEqual(result)
+    })
+  })
+
+  describe('getClearMessageTemplate method', () => {
+    test('returns XML template without any licenseServerURL', () => {
+      const result = '<?xml version="1.0" encoding="utf-8"?>'
+      + '<PlayReadyInitiator xmlns="http://schemas.microsoft.com/DRM/2007/03/protocols/">'
+      + '</PlayReadyInitiator>'
+
+      expect(getClearMessageTemplate()).toEqual(result)
+    })
+  })
+
+  describe('createDrmAgent method', () => {
+    test('returns a DOM element with a specific type to handle with the DRM license request', () => {
+      const drmElement = createDrmAgent()
+
+      expect(drmElement.tagName).toEqual('OBJECT')
+      expect(drmElement.type).toEqual('application/oipfdrmagent')
+    })
+
+    test('returns a DOM element with properties that avoids showing it on the screen', () => {
+      const drmElement = createDrmAgent()
+
+      expect(drmElement.id).toEqual('oipfdrmagent')
+      expect(drmElement.style.visibility).toEqual('hidden')
+      expect(drmElement.style.position).toEqual('absolute')
+      expect(drmElement.style.top).toEqual('0px')
+      expect(drmElement.style.left).toEqual('0px')
+      expect(drmElement.style.width).toEqual('0px')
+      expect(drmElement.style.height).toEqual('0px')
+    })
+  })
+
+  describe('sendLicenseRequest method', () => {
+    beforeEach(() => {
+      this.config = { licenseServerURL: 'http://fake-domain/fake_folder/playready?deviceId=FAKEID' }
+    })
+    afterEach(() => {
+      const drmAgentElement = document.getElementById('oipfdrmagent')
+      drmAgentElement && drmAgentElement.remove()
+    })
+
+    test('reuses drmAgent element if already exists', () => {
+      const cb = jest.fn()
+
+      document.body.appendChild(createDrmAgent())
+      sendLicenseRequest(this.config, cb)
+
+      const duplicateIds = document.querySelectorAll('[id=\'oipfdrmagent\']')
+
+      expect(duplicateIds.length).toEqual(1)
+    })
+
+    test('prefer to append drmAgent on parent element of received scope if exits', () => {
+      const cb = jest.fn()
+      const wantedScope = { el: document.createElement('div') }
+      sendLicenseRequest.call(wantedScope, this.config, cb)
+
+      expect(wantedScope.el.firstChild.id).toEqual('oipfdrmagent')
+    })
+
+    test('appends drmAgent on document.body if any external scope is received', () => {
+      const cb = jest.fn()
+      sendLicenseRequest(this.config, cb)
+
+      expect(document.body.firstChild.id).toEqual('oipfdrmagent')
+    })
+
+    test('logs warn message if no one licenser server URL is received', () => {
+      sendLicenseRequest()
+
+      /* eslint-disable-next-line no-console */
+      expect(console.log).toHaveBeenNthCalledWith(1,
+        LOG_WARN_HEAD_MESSAGE,
+        LOG_WARN_STYLE,
+        'No one license server is found. The expected result for this behavior is clear the current license.',
+      )
+    })
+
+    test('logs a error message if the sendDRMMessage call fails', () => {
+      const cb = jest.fn()
+      sendLicenseRequest(this.config, cb)
+
+      /* eslint-disable-next-line no-console */
+      expect(console.log).toHaveBeenCalledWith(
+        '%c[error][DRMHandler]',
+        'color: #ff0000;font-weight: bold; font-size: 13px;',
+        'Error at sendDRMMessage call',
+        'oipfdrmagent.sendDRMMessage is not a function',
+      )
+    })
+
+    describe('at onDRMRightsError callback', () => {
+      test('don\'t return one error if the received code number is greater o equal 2', () => {
+        document.body.appendChild(createDrmAgent())
+        const drmAgentElement = document.getElementById('oipfdrmagent')
+        drmAgentElement.sendDRMMessage = () => {}
+
+        const cb = jest.fn()
+        sendLicenseRequest(this.config, cb)
+        const errorResponse = drmAgentElement.onDRMRightsError(2)
+
+        expect(errorResponse).toBeUndefined()
+      })
+
+      test('returns one error if the received code number is below 2', () => {
+        document.body.appendChild(createDrmAgent())
+        const drmAgentElement = document.getElementById('oipfdrmagent')
+        drmAgentElement.sendDRMMessage = () => {}
+
+        const cb = jest.fn()
+        sendLicenseRequest(this.config, cb)
+        const errorResponse = drmAgentElement.onDRMRightsError(1)
+
+        expect(errorResponse).toEqual('DRM: Invalid license error')
+      })
+    })
+
+    describe('at onDRMMessageResult callback', () => {
+      test('returns one error if the received result code is greater than 0', () => {
+        document.body.appendChild(createDrmAgent())
+        const drmAgentElement = document.getElementById('oipfdrmagent')
+        drmAgentElement.sendDRMMessage = () => {}
+
+        const cb = jest.fn()
+        sendLicenseRequest(this.config, cb)
+        const resultResponse = drmAgentElement.onDRMMessageResult(0, 'a error message', 1)
+
+        expect(resultResponse).toEqual('DRM: Unspecified error')
+      })
+
+      test('calls the successCallback if the received result code is 0', () => {
+        document.body.appendChild(createDrmAgent())
+        const drmAgentElement = document.getElementById('oipfdrmagent')
+        drmAgentElement.sendDRMMessage = () => {}
+
+        const cb = jest.fn()
+        sendLicenseRequest(this.config, cb)
+        drmAgentElement.onDRMMessageResult(0, 'a success message', 0)
+
+        expect(cb).toHaveBeenCalled()
+      })
+    })
+  })
+
+  describe('clearLicenseRequest method', () => {
+    afterEach(() => {
+      const drmAgentElement = document.getElementById('oipfdrmagent')
+      drmAgentElement && drmAgentElement.remove()
+    })
+
+    test('only clear license server if one drmAgent element already exists', () => {
+      clearLicenseRequest()
+
+      /* eslint-disable-next-line no-console */
+      expect(console.log).toHaveBeenCalledWith(
+        LOG_WARN_HEAD_MESSAGE,
+        LOG_WARN_STYLE,
+        'No one DRM license has been configured before. It\'s not necessary to clear any license server.',
+      )
+    })
+
+    test('logs a error message if the sendDRMMessage call fails', () => {
+      const cb = jest.fn()
+      document.body.appendChild(createDrmAgent())
+      clearLicenseRequest(cb)
+
+      /* eslint-disable-next-line no-console */
+      expect(console.log).toHaveBeenCalledWith(
+        '%c[error][DRMHandler]',
+        'color: #ff0000;font-weight: bold; font-size: 13px;',
+        'Error at sendDRMMessage call',
+        'oipfdrmagent.sendDRMMessage is not a function',
+      )
+    })
+
+    describe('at onDRMMessageResult callback', () => {
+      test('removes drmAgentElement from DOM', () => {
+        document.body.appendChild(createDrmAgent())
+        const drmAgentElement = document.getElementById('oipfdrmagent')
+        drmAgentElement.sendDRMMessage = () => {}
+
+        const cb = jest.fn()
+        clearLicenseRequest(cb)
+        drmAgentElement.onDRMMessageResult()
+
+        expect(document.getElementById('oipfdrmagent')).toBeNull()
+      })
+
+      test('calls the successCallback', () => {
+        document.body.appendChild(createDrmAgent())
+        const drmAgentElement = document.getElementById('oipfdrmagent')
+        drmAgentElement.sendDRMMessage = () => {}
+
+        const cb = jest.fn()
+        clearLicenseRequest(cb)
+        drmAgentElement.onDRMMessageResult()
+
+        expect(cb).toHaveBeenCalled()
+      })
+    })
+  })
+})
