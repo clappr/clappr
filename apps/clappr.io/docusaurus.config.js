d8b63bfbc468846baa4f2ae5c4c03a1b9657dc9f
Felipe Cesar <felipecesr@gmail.com>
2023-10-18 17:01:08 -0300

    docs: add clappr-docs website

---
+++ apps/clappr.io/docusaurus.config.js
@@ -0,0 +1,119 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'Clappr',
+  tagline: 'An extensible media player for web applications',
+  url: 'https://clappr.io',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+
+  // GitHub pages deployment config.
+  organizationName: 'clappr',
+  projectName: 'clappr',
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'Clappr',
+        logo: {
+          alt: 'Clappr Logo',
+          // src: 'img/clappr_logo_black.png',
+          src: 'img/clappr_icon_black.png',
+          // srcDark: 'img/clappr_logo_white.png',
+          srcDark: 'img/clappr_icon_white.png',
+        },
+        items: [
+          {
+            to: '/docs/intro', 
+            label: 'Docs', 
+            position: 
+            'left'
+          },
+          {
+            to: 'http://clappr.github.io/', 
+            label: 'API', 
+            position: 
+            'left'
+          },
+          {
+            href: 'https://github.com/clappr/clappr',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Learn',
+            items: [
+              {
+                label: 'Getting Started',
+                to: '/getting-started/intro',
+              },
+              {
+                label: 'Docs',
+                to: '/docs/intro',
+              },
+              {
+                label: 'API',
+                to: 'http://clappr.github.io/',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/clappr',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/clappr/clappr',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Clappr, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+};
+
+module.exports = config;
---
+++ apps/clappr.io/src/pages/index.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+
+import styles from './index.module.css';
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary"
+            to="/docs/intro">
+            Get Started
+          </Link>
+          <Link
+            className="button button--info"
+            href="http://clappr.io/demo">
+            Try a Demo
+          </Link>
+          <span className={styles.indexCtasGitHubButtonWrapper}>
+            <iframe
+              className={styles.indexCtasGitHubButton}
+              src="https://ghbtns.com/github-btn.html?user=clappr&amp;repo=clappr&amp;type=star&amp;count=true&amp;size=large"
+              width={160}
+              height={60}
+              title="GitHub Stars"
+            />
+          </span>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default function Home() {
+  return (
+    <Layout
+      description="Description will go into a meta tag in <head />">
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+}
