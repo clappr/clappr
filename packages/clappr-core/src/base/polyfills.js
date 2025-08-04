e54a8ba347fe3f4efe9791eb381d8c21c2abd4de
Lucio Ariel <lucio.furtado@g.globo>
2025-08-04 10:51:56 -0300

    chore: format code for eslint consistency

--- packages/clappr-core/src/base/polyfills.js
+++ packages/clappr-core/src/base/polyfills.js
@@ -106,9 +106,9 @@ if (typeof Object.assign != 'function') {
         if (nextSource != null) { // Skip over if undefined or null
           for (let nextKey in nextSource) {
             // Avoid bugs when hasOwnProperty is shadowed
-            if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+            if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) 
               to[nextKey] = nextSource[nextKey]
-            }
+            
           }
         }
       }
@@ -123,26 +123,26 @@ if (typeof Object.assign != 'function') {
 if (!Array.prototype.findIndex) {
   Object.defineProperty(Array.prototype, 'findIndex', {
     value: function(predicate) {
-     // 1. Let O be ? ToObject(this value).
-      if (this == null) {
-        throw new TypeError('"this" is null or not defined');
-      }
+      // 1. Let O be ? ToObject(this value).
+      if (this == null) 
+        throw new TypeError('"this" is null or not defined')
+      
 
-      var o = Object(this);
+      let o = Object(this)
 
       // 2. Let len be ? ToLength(? Get(O, "length")).
-      var len = o.length >>> 0;
+      let len = o.length >>> 0
 
       // 3. If IsCallable(predicate) is false, throw a TypeError exception.
-      if (typeof predicate !== 'function') {
-        throw new TypeError('predicate must be a function');
-      }
+      if (typeof predicate !== 'function') 
+        throw new TypeError('predicate must be a function')
+      
 
       // 4. If thisArg was supplied, let T be thisArg; else let T be undefined.
-      var thisArg = arguments[1];
+      let thisArg = arguments[1]
 
       // 5. Let k be 0.
-      var k = 0;
+      let k = 0
 
       // 6. Repeat, while k < len
       while (k < len) {
@@ -150,18 +150,18 @@ if (!Array.prototype.findIndex) {
         // b. Let kValue be ? Get(O, Pk).
         // c. Let testResult be ToBoolean(? Call(predicate, T, « kValue, k, O »)).
         // d. If testResult is true, return k.
-        var kValue = o[k];
-        if (predicate.call(thisArg, kValue, k, o)) {
-          return k;
-        }
+        let kValue = o[k]
+        if (predicate.call(thisArg, kValue, k, o)) 
+          return k
+        
         // e. Increase k by 1.
-        k++;
+        k++
       }
 
       // 7. Return -1.
-      return -1;
+      return -1
     },
     configurable: true,
     writable: true
-  });
+  })
 }
