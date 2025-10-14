4f3dd380ba557f599efe68cb6d582752d57bd0ce
Amanda <ammelo@id.uff.br>
2025-10-14 14:41:25 -0300

    feat(clappr-plugins): migrate testing framework from Karma to Jest

---
+++ packages/clappr-plugins/jest.config.js
@@ -0,0 +1,18 @@
+const ClapprCorePkg = require('@clappr/core/package.json')
+
+module.exports = {
+  testEnvironment: 'jsdom',
+  verbose: true,
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+    '^.+\\.html$': '<rootDir>/src/__mocks__/htmlMock.js'
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^clappr$': '<rootDir>/node_modules/@clappr/core/dist/clappr-core.js',
+    '\\.(scss|svg)$': '<rootDir>/src/__mocks__/styleMock.js',
+    '^clappr-zepto$': 'clappr-zepto/zepto.js'
+  },
+  collectCoverageFrom: ['src/*.js', 'src/**/*.js', 'src/**/**/*.js'],
+  globals: { CLAPPR_CORE_VERSION: ClapprCorePkg.version }
+}
