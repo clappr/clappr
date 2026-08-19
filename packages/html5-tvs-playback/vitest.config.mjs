b9fb3848e85b17753880533d2a394172264ba83d
Felipe Leão <felipe@felipeleao.com>
2026-08-19 18:16:24 -0300

    test(html5-tvs): migrate test runner to vitest

---
+++ packages/html5-tvs-playback/vitest.config.mjs
@@ -0,0 +1,17 @@
+import { createRequire } from 'node:module'
+import { viteDefine } from '../../vite.config.base.mjs'
+import { defineClapprVitest } from '../../vitest.config.base.mjs'
+
+const require = createRequire(import.meta.url)
+const { version: clapprCoreVersion } = require('@clappr/core/package.json')
+
+export default defineClapprVitest({
+  define: viteDefine({ VERSION: clapprCoreVersion }),
+  coverageInclude: ['src/**/*.js'],
+  coverageThresholds: {
+    statements: 90,
+    branches: 90,
+    functions: 90,
+    lines: 90
+  }
+})
