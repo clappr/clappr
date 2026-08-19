b661c76877a519d4d3bf86c8b7c96f8db030e3b1
Felipe Leão <felipe@felipeleao.com>
2026-08-19 17:52:57 -0300

    refactor(plugins): import modules by explicit file path
    Unit tests 61 passed before and after. Rollup build still succeeds.

--- packages/clappr-plugins/src/main.js
+++ packages/clappr-plugins/src/main.js
@@ -2,21 +2,21 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-import ClickToPause from './plugins/click_to_pause'
-import ClosedCaptions from './plugins/closed_captions'
-import DVRControls from './plugins/dvr_controls'
+import ClickToPause from './plugins/click_to_pause/click_to_pause'
+import ClosedCaptions from './plugins/closed_captions/closed_captions'
+import DVRControls from './plugins/dvr_controls/dvr_controls'
 import EndVideo from './plugins/end_video'
-import ErrorScreen from './plugins/error_screen'
+import ErrorScreen from './plugins/error_screen/error_screen'
 import Favicon from './plugins/favicon'
-import GoogleAnalytics from './plugins/google_analytics'
-import MediaControl from './plugins/media_control'
-import Poster from './plugins/poster'
-import SeekTime from './plugins/seek_time'
-import SpinnerThreeBounce from './plugins/spinner_three_bounce'
-import Stats from './plugins/stats'
-import WaterMark from './plugins/watermark'
+import GoogleAnalytics from './plugins/google_analytics/google_analytics'
+import MediaControl from './plugins/media_control/media_control'
+import Poster from './plugins/poster/poster'
+import SeekTime from './plugins/seek_time/seek_time'
+import SpinnerThreeBounce from './plugins/spinner_three_bounce/spinner_three_bounce'
+import Stats from './plugins/stats/stats'
+import WaterMark from './plugins/watermark/watermark'
 
-import Vendor from './vendor'
+import Vendor from './vendor/index'
 
 const version = VERSION
 
